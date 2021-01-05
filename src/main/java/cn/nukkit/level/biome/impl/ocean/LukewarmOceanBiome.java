cc85ba6b86dc46aadfe341bdb8eb4d173a31a3fe
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-05 09:29:39 +0900

    Create LukewarmOceanBiome.java

---
+++ src/main/java/cn/nukkit/level/biome/impl/ocean/LukewarmOceanBiome.java
@@ -0,0 +1,57 @@
+package cn.nukkit.level.biome.impl.ocean;
+
+import cn.nukkit.blockstate.BlockState;
+import cn.nukkit.level.generator.populator.impl.PopulatorDisk;
+import cn.nukkit.level.generator.populator.impl.PopulatorKelp;
+import cn.nukkit.level.generator.populator.impl.PopulatorSeagrass;
+
+import java.util.Arrays;
+
+/**
+ * @author GoodLucky777
+ */
+public class LukewarmOceanBiome extends OceanBiome {
+
+    private static final BlockState STATE_SAND = BlockState.of(SAND);
+    
+    public LukewarmOceanBiome() {
+        PopulatorDisk populatorDiskSand = new PopulatorDisk(1.0, BlockState.of(SAND), 2, 4, 2, Arrays.asList(BlockState.of(GRASS), BlockState.of(DIRT)));
+        populatorDiskSand.setBaseAmount(3);
+        this.addPopulator(populatorDiskSand);
+        
+        PopulatorDisk populatorDiskClay = new PopulatorDisk(1.0, BlockState.of(CLAY_BLOCK), 1, 2, 1, Arrays.asList(BlockState.of(DIRT), BlockState.of(CLAY_BLOCK)));
+        populatorDiskClay.setBaseAmount(1);
+        this.addPopulator(populatorDiskClay);
+        
+        PopulatorDisk populatorDiskGravel = new PopulatorDisk(1.0, BlockState.of(GRAVEL), 2, 3, 2, Arrays.asList(BlockState.of(GRASS), BlockState.of(DIRT)));
+        populatorDiskGravel.setBaseAmount(1);
+        this.addPopulator(populatorDiskGravel);
+        
+        PopulatorKelp populatorKelp = new PopulatorKelp();
+        populatorKelp.setBaseAmount(-175);
+        populatorKelp.setRandomAmount(210);
+        this.addPopulator(populatorKelp);
+        
+        PopulatorSeagrass populatorSeagrass = new PopulatorSeagrass(0.3);
+        populatorSeagrass.setBaseAmount(40);
+        populatorSeagrass.setBaseAmount(40);
+        this.addPopulator(populatorSeagrass);
+        
+        this.setBaseHeight(-1f);
+        this.setHeightVariation(0.1f);
+    }
+    
+    @Override
+    public String getName() {
+        return "Lukewarm Ocean";
+    }
+    
+    public BlockState getGroundState(int x, int y, int z) {
+        return STATE_SAND;
+    }
+    
+    @Override
+    public int getGroundDepth(int x, int y, int z) {
+        return 1;
+    }
+}
