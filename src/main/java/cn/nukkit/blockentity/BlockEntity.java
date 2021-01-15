865e101196bb400e704e28b7dffd5fbc1131bacd
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-15 11:34:07 +0900

    Add END_PORTAL, END_GATEWAY

--- src/main/java/cn/nukkit/blockentity/BlockEntity.java
+++ src/main/java/cn/nukkit/blockentity/BlockEntity.java
@@ -67,11 +67,16 @@ public abstract class BlockEntity extends Position {
     @PowerNukkitOnly
     @Since("1.4.0.0-PN")
     public static final String TARGET = "Target";
-
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public static final String END_PORTAL = "EndPortal";
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public static final String END_GATEWAY = "EndGateway";
 
     public static long count = 1;
 
-    private static final BiMap<String, Class<? extends BlockEntity>> knownBlockEntities = HashBiMap.create(21);
+    private static final BiMap<String, Class<? extends BlockEntity>> knownBlockEntities = HashBiMap.create(35);
 
     public FullChunk chunk;
     public String name;
