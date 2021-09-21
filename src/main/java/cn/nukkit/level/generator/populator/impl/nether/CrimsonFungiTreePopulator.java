6f72be693157361a68455f0056e6385d5a9e41d6
Flaming Knight <asherncc@gmail.com>
2021-09-21 12:51:01 -0700

    Fixed Crimson Biome Populators

--- src/main/java/cn/nukkit/level/generator/populator/impl/nether/CrimsonFungiTreePopulator.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/nether/CrimsonFungiTreePopulator.java
@@ -9,34 +9,37 @@ import cn.nukkit.math.NukkitMath;
 import cn.nukkit.math.NukkitRandom;
 import cn.nukkit.math.Vector3;
 
+import java.util.ArrayList;
+
 public class CrimsonFungiTreePopulator extends Populator {
     private ChunkManager level;
 
     @Override
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
         this.level = level;
-        int amount = random.nextBoundedInt(4) + 3;
+        int amount = random.nextBoundedInt(6) + 4;
 
         for (int i = 0; i < amount; ++i) {
             int x = NukkitMath.randomRange(random, chunkX << 4, (chunkX << 4) + 15);
             int z = NukkitMath.randomRange(random, chunkZ << 4, (chunkZ << 4) + 15);
-            int y = this.getHighestWorkableBlock(x, z);
-            if (y <= 1) {
-                continue;
+            ArrayList<Integer> ys = this.getHighestWorkableBlocks(x, z);
+            for(int y : ys) {
+                if(y <= 1) continue;
+                if(random.nextBoundedInt(4) == 1) continue;
+                new ObjectCrimsonTree().placeObject(this.level, x, y, z, random);
             }
-            new ObjectCrimsonTree().placeObject(this.level, x, y, z, random);
         }
     }
 
-    private int getHighestWorkableBlock(int x, int z) {
+    private ArrayList<Integer> getHighestWorkableBlocks(int x, int z) {
         int y;
+        ArrayList<Integer> blockYs = new ArrayList<>();
         for (y = 128; y > 0; --y) {
             int b = this.level.getBlockIdAt(x, y, z);
-            if (b == Block.CRIMSON_NYLIUM && this.level.getBlockIdAt(x, y+1, z) == 0) {
-                break;
+            if ((b == Block.BASALT || b == Block.BLACKSTONE) && this.level.getBlockIdAt(x, y+1, z) == 0) {
+                blockYs.add(y+1);
             }
         }
-
-        return ++y;
+        return blockYs;
     }
 }
--- src/main/java/cn/nukkit/level/generator/populator/impl/nether/CrimsonWeepingVinesPopulator.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/nether/CrimsonWeepingVinesPopulator.java
@@ -1,10 +1,14 @@
 package cn.nukkit.level.generator.populator.impl.nether;
 
+import cn.nukkit.block.Block;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.level.generator.object.tree.ObjectCrimsonTree;
 import cn.nukkit.level.generator.populator.type.Populator;
 import cn.nukkit.math.NukkitMath;
 import cn.nukkit.math.NukkitRandom;
+
+import java.util.ArrayList;
 
 public class CrimsonWeepingVinesPopulator extends Populator {
     private ChunkManager level;
@@ -17,14 +21,14 @@ public class CrimsonWeepingVinesPopulator extends Populator {
         for (int i = 0; i < amount; ++i) {
             int x = NukkitMath.randomRange(random, chunkX << 4, (chunkX << 4) + 15);
             int z = NukkitMath.randomRange(random, chunkZ << 4, (chunkZ << 4) + 15);
-            int y = this.getHighestWorkableBlock(x, z);
-            if (y <= 1) {
-                continue;
-            }
-            int endY = this.getHighestEndingBlock(x, y, z);
-            int amountToDecrease = random.nextBoundedInt(y-endY);
-            for(int yPos = y; yPos > y-amountToDecrease; yPos--) {
-                this.level.setBlockAt(x, yPos, z, WEEPING_VINES);
+            ArrayList<Integer> ys = this.getHighestWorkableBlocks(x, z);
+            for(int y : ys) {
+                if(y <= 1) continue;
+                int endY = this.getHighestEndingBlock(x, y, z);
+                int amountToDecrease = random.nextBoundedInt(y-endY);
+                for(int yPos = y; yPos > y-amountToDecrease; yPos--) {
+                    this.level.setBlockAt(x, yPos, z, WEEPING_VINES);
+                }
             }
         }
     }
@@ -45,19 +49,15 @@ public class CrimsonWeepingVinesPopulator extends Populator {
         return ++y;
     }
 
-    private int getHighestWorkableBlock(int x, int z) {
+    private ArrayList<Integer> getHighestWorkableBlocks(int x, int z) {
         int y;
+        ArrayList<Integer> blockYs = new ArrayList<>();
         for (y = 128; y > 0; --y) {
             int b = this.level.getBlockIdAt(x, y, z);
-            if ((b == NETHERRACK || b == CRIMSON_NYLIUM || b == BLOCK_NETHER_WART_BLOCK ||
-                 b == STILL_LAVA || b == LAVA ||
-                 b == CRIMSON_FUNGUS || b == CRIMSON_ROOTS ||
-                 b == QUARTZ_ORE || b == NETHER_GOLD_ORE || b == ANCIENT_DERBRIS)
-                 && this.level.getBlockIdAt(x, y-1, z) == 0) {
-                break;
+            if ((b == Block.BASALT || b == Block.BLACKSTONE) && this.level.getBlockIdAt(x, y+1, z) == 0) {
+                blockYs.add(y+1);
             }
         }
-
-        return --y;
+        return blockYs;
     }
 }
