91a383173cb5c45c680dca485509d852ea202629
helloworldSB <65109494+helloworldSB@users.noreply.github.com>
2022-02-03 13:11:38 +0800

    Revert "Update PopulatorEndSpike.java"
    This reverts commit e4aa2b7053f98acf0294092586d5b43365e382fd.

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndSpike.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndSpike.java
@@ -19,15 +19,13 @@ public class PopulatorEndSpike extends Populator {
     @Override
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
         return;
-        /*
-         * final int r = 3 + random.nextBoundedInt(4);
-         * final int h = 49 + 9 * r;
-         * final int randH = random.nextBoundedInt(3);
-         * ObjectEndSpike objectEndSpike = new ObjectEndSpike(new Vector3(chunkX << 4,
-         * chunkZ << 4), r, h + randH * 3,
-         * r == 3 && randH != 0);
-         * objectEndSpike.generate(level, random);
-         */
+/*
+        final int r = 3 + random.nextBoundedInt(4);
+        final int h = 49 + 9 * r;
+        final int randH = random.nextBoundedInt(3);
+        ObjectEndSpike objectEndSpike = new ObjectEndSpike(new Vector3(chunkX << 4, chunkZ << 4), r, h + randH * 3,
+                r == 3 && randH != 0);
+        objectEndSpike.generate(level, random);
     }
-
+*/
 }
