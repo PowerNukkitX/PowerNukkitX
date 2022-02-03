c25b8804743c0f25f648d16340472a5f799e65bd
helloworldSB <65109494+helloworldSB@users.noreply.github.com>
2022-02-03 22:34:39 +0800

    Let Populator more comfortable

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndSpike.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorEndSpike.java
@@ -18,11 +18,15 @@ public class PopulatorEndSpike extends Populator {
 
     @Override
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
-
+        if (chunkX > 200 || chunkY > 200)
+            return;
         final int r = 3 + random.nextBoundedInt(4);
         final int h = 49 + 9 * r;
         final int randH = random.nextBoundedInt(3);
-        ObjectEndSpike objectEndSpike = new ObjectEndSpike(new Vector3(chunk.getX(), chunk.getZ()), r,
+        ObjectEndSpike objectEndSpike = new ObjectEndSpike(
+                new Vector3(chunk.getX() - 32 + 32 * random.nextBoundedInt(3),
+                        chunk.getZ() - 32 + 32 * random.nextBoundedInt(3)),
+                r,
                 h + randH * 3,
                 r == 3 && randH != 0); // Emulate Vanilla Behavior
 
