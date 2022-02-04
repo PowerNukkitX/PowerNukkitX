a08d162a8cd1f39ef97c0dcaadff62f34cf69f9e
LT_Name <572413378@qq.com>
2022-02-04 12:54:09 +0800

    Stuff

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorCactus.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorCactus.java
@@ -3,6 +3,7 @@ package cn.nukkit.level.generator.populator.impl;
 import cn.nukkit.block.Block;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.level.generator.populator.helper.EnsureAround;
 import cn.nukkit.level.generator.populator.helper.EnsureBelow;
 import cn.nukkit.level.generator.populator.helper.EnsureCover;
 import cn.nukkit.level.generator.populator.type.PopulatorSurfaceBlock;
@@ -14,11 +15,11 @@ import cn.nukkit.math.NukkitRandom;
 public class PopulatorCactus extends PopulatorSurfaceBlock {
     @Override
     protected void populateCount(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
-        int x = random.nextBoundedInt(16);
-        int z = random.nextBoundedInt(16);
+        int x = random.nextBoundedInt(14) + 1;
+        int z = random.nextBoundedInt(14) + 1;
         int y = getHighestWorkableBlock(level, x, z, chunk);
         int height = 0;
-        int range = random.nextRange(0, 17);
+        int range = random.nextBoundedInt(18);
         if (range >= 16) {
             height = 2;
         }else if (range >= 11) {
@@ -36,7 +37,9 @@ public class PopulatorCactus extends PopulatorSurfaceBlock {
 
     @Override
     protected boolean canStay(int x, int y, int z, FullChunk chunk) {
-        return EnsureCover.ensureCover(x, y, z, chunk) && (EnsureBelow.ensureBelow(x, y, z, SAND, chunk) || EnsureBelow.ensureBelow(x, y, z, CACTUS, chunk));
+        return EnsureCover.ensureCover(x, y, z, chunk) &&
+                (EnsureBelow.ensureBelow(x, y, z, SAND, chunk) || EnsureBelow.ensureBelow(x, y, z, CACTUS, chunk)) &&
+                EnsureAround.ensureAroundAir(x, y, z, chunk);
     }
 
     @Override
