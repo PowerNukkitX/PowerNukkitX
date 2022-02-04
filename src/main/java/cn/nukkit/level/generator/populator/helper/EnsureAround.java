b28b8bf51bbe42a7f16d7156bd286976df80255c
LT_Name <572413378@qq.com>
2022-02-04 12:39:53 +0800

    仙人掌旁不应有方块

---
+++ src/main/java/cn/nukkit/level/generator/populator/helper/EnsureAround.java
@@ -0,0 +1,18 @@
+package cn.nukkit.level.generator.populator.helper;
+
+import cn.nukkit.level.format.FullChunk;
+
+import static cn.nukkit.block.BlockID.AIR;
+
+
+/**
+ * @author LT_Name
+ */
+public interface EnsureAround {
+    static boolean ensureAroundAir(int x, int y, int z, FullChunk chunk)  {
+        return chunk.getBlockId(x + 1, y, z) == AIR &&
+                chunk.getBlockId(x - 1, y, z) == AIR &&
+                chunk.getBlockId(x, y, z + 1) == AIR &&
+                chunk.getBlockId(x, y, z - 1) == AIR;
+    }
+}
