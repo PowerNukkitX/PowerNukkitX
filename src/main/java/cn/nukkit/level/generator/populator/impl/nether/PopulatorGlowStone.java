a7cec07892a640cb75eb8006a03e5b18a17b2587
CoolLoong <1542536763@qq.com>
2022-02-09 12:12:52 +0800

    优化萤石填充算法

--- src/main/java/cn/nukkit/level/generator/populator/impl/nether/PopulatorGlowStone.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/nether/PopulatorGlowStone.java
@@ -1,9 +1,11 @@
 package cn.nukkit.level.generator.populator.impl.nether;
 
 import cn.nukkit.block.Block;
+import cn.nukkit.block.BlockID;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.level.generator.populator.type.Populator;
+import cn.nukkit.math.BlockFace;
 import cn.nukkit.math.NukkitMath;
 import cn.nukkit.math.NukkitRandom;
 
@@ -16,8 +18,22 @@ public class PopulatorGlowStone extends Populator {
             int y = this.getHighestWorkableBlock(chunk, x & 0xF, z & 0xF);
             if (y != -1 && level.getBlockIdAt(x, y, z) != NETHERRACK) {
                 int count = NukkitMath.randomRange(random, 40, 60);
-                for (int i = 0; i < count; i++) {
-                    level.setBlockAt(x + (random.nextBoundedInt(7) - 3), y + (random.nextBoundedInt(9) - 4), z + (random.nextBoundedInt(7) - 3), GLOWSTONE);
+                level.setBlockAt(x,y,z,GLOWSTONE);
+                int cyclesNum=0;
+                while(!(count==0)){
+                    if (cyclesNum==1500) break;
+                    int spawnX = x + random.nextBoundedInt(8) - random.nextBoundedInt(8);
+                    int spawnY = y + random.nextBoundedInt(8) - 4;
+                    int spawnZ = z + random.nextBoundedInt(8) - random.nextBoundedInt(8);
+                    if (cyclesNum%100==0 && cyclesNum!=0){
+                        level.setBlockAt(x+random.nextRange(-2,2),y+random.nextBoundedInt(3),z+random.nextRange(-2,2),GLOWSTONE);
+                        count--;
+                    }
+                    if(checkAroundBlock(spawnX,spawnY,spawnZ,level)){
+                        level.setBlockAt(spawnX,spawnY,spawnZ,GLOWSTONE);
+                        count--;
+                    }
+                    cyclesNum++;
                 }
             }
         }
@@ -33,5 +49,14 @@ public class PopulatorGlowStone extends Populator {
             }
         }
         return y == 0 ? -1 : y;
+    }
+
+    private boolean checkAroundBlock(int x,int y,int z,ChunkManager level){
+        for (BlockFace i : BlockFace.values()){
+            if(level.getBlockIdAt(x+i.getXOffset(),y+i.getYOffset(),z+i.getZOffset())==BlockID.GLOWSTONE){
+                return true;
+            }
+        }
+        return false;
     }
 }
