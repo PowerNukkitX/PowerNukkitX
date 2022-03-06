327ff7559dd046e34f5a6e1de210ac89d7f406c6
smartcmd <60499296+smartcmd@users.noreply.github.com>
2022-03-06 11:55:04 +0800

    合并更改 (#138)
    * 实现发光墨囊功能
    
    * 初步实现地图 (#107)
    
    * 初步实现地图
    
    * 修复构建
    
    * 修复BaseFullChunk#getHighestBlockAt方法的最小y值
    
    * 修复活塞的一个bug(还有个)
    为Position/Level添加getBlockAround()方法
    
    * 修复发光物品展示框显示错误的问题; (#118)
    
    修复两个无法被点燃的方块错误点燃的问题;
    
    * 修复活塞的另一个bug
    
    * 完全修复活塞问题
    
    * 修复一处手残
    
    * 修复几处np,修复一处并发异常
    
    * 补上紫水晶的onBreak()里面写漏的东西
    
    * 补上紫水晶的onBreak()里面写漏的东西
    
    * 补上紫水晶的onBreak()里面写漏的东西
    
    * 更新Watchdog中的issues链接 (#131)
    
    * 增加部分事件，修复石锥掉落不触发事件的bug (#133)
    
    * 修复白名单命令提示信息 (#132)
    
    * 更新WhitelistCommand
    
    * 更多修改
    
    * 尝试更新语言文件
    
    * 添加分号
    
    * 尝试修复LevelProvider#getDimension多线程NPE问题 (#137)
    
    Co-authored-by: LT_Name <572413378@qq.com>
    Co-authored-by: LT_Name <45508179+lt-name@users.noreply.github.com>
    Co-authored-by: CoolLoong <69153398+CoolLoong@users.noreply.github.com>
    Co-authored-by: Superice666 <51087818+Superice666@users.noreply.github.com>

--- src/main/java/cn/nukkit/block/BlockAmethystBud.java
+++ src/main/java/cn/nukkit/block/BlockAmethystBud.java
@@ -110,6 +110,8 @@ public abstract class BlockAmethystBud extends BlockTransparentMeta implements F
         if (item.isPickaxe()){
             Arrays.stream(this.getDrops(item)).forEach(item1 -> this.level.dropItem(this.add(0.5,0,0.5), item1));
             this.getLevel().setBlock(this, layer, Block.get(BlockID.AIR), true, true);
+        }else {
+            this.getLevel().setBlock(this, layer, Block.get(BlockID.AIR), true, true);
         }
         return true;
     }
--- src/main/java/cn/nukkit/command/defaults/WhitelistCommand.java
+++ src/main/java/cn/nukkit/command/defaults/WhitelistCommand.java
@@ -15,21 +15,28 @@ import cn.nukkit.utils.TextFormat;
 public class WhitelistCommand extends VanillaCommand {
 
     public WhitelistCommand(String name) {
-        super(name, "%nukkit.command.whitelist.description", "%commands.whitelist.usage");
+        super(name, "%nukkit.command.whitelist.description", "%commands.whitelist.usage", new String[]{"allowlist"}); // In Minecraft Bedrock v1.18.10 the whitelist was renamed to allowlist
         this.setPermission(
                 "nukkit.command.whitelist.reload;" +
                         "nukkit.command.whitelist.enable;" +
                         "nukkit.command.whitelist.disable;" +
                         "nukkit.command.whitelist.list;" +
                         "nukkit.command.whitelist.add;" +
-                        "nukkit.command.whitelist.remove"
+                        "nukkit.command.whitelist.remove;"+
+                        //v1.18.10+
+                        "nukkit.command.allowlist.reload;" +
+                        "nukkit.command.allowlist.enable;" +
+                        "nukkit.command.allowlist.disable;" +
+                        "nukkit.command.allowlist.list;" +
+                        "nukkit.command.allowlist.add;" +
+                        "nukkit.command.allowlist.remove"
         );
         this.commandParameters.clear();
         this.commandParameters.put("1arg", new CommandParameter[]{
-                CommandParameter.newEnum("action", new CommandEnum("WhitelistAction", "on", "off", "list", "reload"))
+                CommandParameter.newEnum("action", new CommandEnum("AllowlistAction", "on", "off", "list", "reload"))
         });
         this.commandParameters.put("2args", new CommandParameter[]{
-                CommandParameter.newEnum("action", new CommandEnum("WhitelistPlayerAction", "add", "remove")),
+                CommandParameter.newEnum("action", new CommandEnum("AllowlistPlayerAction", "add", "remove")),
                 CommandParameter.newType("player", CommandParamType.TARGET)
         });
     }
@@ -52,17 +59,17 @@ public class WhitelistCommand extends VanillaCommand {
             switch (args[0].toLowerCase()) {
                 case "reload":
                     sender.getServer().reloadWhitelist();
-                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.whitelist.reloaded"));
+                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.allowlist.reloaded"));
 
                     return true;
                 case "on":
                     sender.getServer().setPropertyBoolean("white-list", true);
-                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.whitelist.enabled"));
+                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.allowlist.enabled"));
 
                     return true;
                 case "off":
                     sender.getServer().setPropertyBoolean("white-list", false);
-                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.whitelist.disabled"));
+                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.allowlist.disabled"));
 
                     return true;
                 case "list":
@@ -72,17 +79,17 @@ public class WhitelistCommand extends VanillaCommand {
                         result.append(player).append(", ");
                         ++count;
                     }
-                    sender.sendMessage(new TranslationContainer("commands.whitelist.list", String.valueOf(count), String.valueOf(count)));
+                    sender.sendMessage(new TranslationContainer("commands.allowlist.list", String.valueOf(count), String.valueOf(count)));
                     sender.sendMessage(result.length() > 0 ? result.substring(0, result.length() - 2) : "");
 
                     return true;
 
                 case "add":
-                    sender.sendMessage(new TranslationContainer("commands.generic.usage", "%commands.whitelist.add.usage"));
+                    sender.sendMessage(new TranslationContainer("commands.generic.usage", this.usageMessage));
                     return true;
 
                 case "remove":
-                    sender.sendMessage(new TranslationContainer("commands.generic.usage", "%commands.whitelist.remove.usage"));
+                    sender.sendMessage(new TranslationContainer("commands.generic.usage", this.usageMessage));
                     return true;
             }
         } else if (args.length == 2) {
@@ -92,12 +99,12 @@ public class WhitelistCommand extends VanillaCommand {
             switch (args[0].toLowerCase()) {
                 case "add":
                     sender.getServer().getOfflinePlayer(args[1]).setWhitelisted(true);
-                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.whitelist.add.success", args[1]));
+                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.allowlist.add.success", args[1]));
 
                     return true;
                 case "remove":
                     sender.getServer().getOfflinePlayer(args[1]).setWhitelisted(false);
-                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.whitelist.remove.success", args[1]));
+                    Command.broadcastCommandMessage(sender, new TranslationContainer("commands.allowlist.remove.success", args[1]));
 
                     return true;
             }
@@ -107,7 +114,7 @@ public class WhitelistCommand extends VanillaCommand {
     }
 
     private boolean badPerm(CommandSender sender, String perm) {
-        if (!sender.hasPermission("nukkit.command.whitelist." + perm)) {
+        if (!sender.hasPermission("nukkit.command.whitelist." + perm) && !sender.hasPermission("nukkit.command.allowlist." + perm)) {
             sender.sendMessage(new TranslationContainer(TextFormat.RED + "%commands.generic.permission"));
 
             return true;
