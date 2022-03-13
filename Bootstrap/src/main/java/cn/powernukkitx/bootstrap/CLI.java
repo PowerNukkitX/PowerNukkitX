39a113c7a9442e693501634ea910151d1198fe84
超神的冰凉 <2801621420@qq.com>
2022-03-13 09:34:36 +0800

    组件化版本检查

--- Bootstrap/src/main/java/cn/powernukkitx/bootstrap/CLI.java
+++ Bootstrap/src/main/java/cn/powernukkitx/bootstrap/CLI.java
@@ -1,13 +1,8 @@
 package cn.powernukkitx.bootstrap;
 
-import cn.powernukkitx.bootstrap.cli.AdoptOpenJDKInstall;
-import cn.powernukkitx.bootstrap.cli.Component;
-import cn.powernukkitx.bootstrap.cli.GraalVMInstall;
-import cn.powernukkitx.bootstrap.cli.PrintHelp;
-import cn.powernukkitx.bootstrap.info.locator.JarLocator;
+import cn.powernukkitx.bootstrap.cli.*;
 import cn.powernukkitx.bootstrap.info.locator.JavaLocator;
 import cn.powernukkitx.bootstrap.info.locator.Location;
-import cn.powernukkitx.bootstrap.util.GitUtils;
 import cn.powernukkitx.bootstrap.util.LanguageUtils;
 import cn.powernukkitx.bootstrap.util.Logger;
 import cn.powernukkitx.bootstrap.util.URLUtils;
@@ -17,8 +12,6 @@ import joptsimple.OptionSpec;
 
 import java.net.URL;
 import java.util.*;
-
-import static cn.powernukkitx.bootstrap.Bootstrap.workingDir;
 
 public final class CLI implements Program {
     public final Timer timer = new Timer();
@@ -30,6 +23,7 @@ public final class CLI implements Program {
         components.put("GraalVMInstall", new GraalVMInstall());
         components.put("AdoptOpenJDKInstall", new AdoptOpenJDKInstall());
         components.put("PrintHelp", new PrintHelp());
+        components.put("CheckVersion", new CheckVersion());
     }
 
     @Override
@@ -46,58 +40,7 @@ public final class CLI implements Program {
             exec("PrintHelp", parser);
         }
         if (options.has(versionSpec)) {
-            String arg = options.valueOf(versionSpec);
-            if (arg == null) arg = "bootstrap,pnx,java";
-            if (arg.contains("bootstrap")) {
-                    Optional<GitUtils.FullGitInfo> optGitInfo = GitUtils.getSelfGitInfo();
-                    if (optGitInfo.isPresent()) {
-                        GitUtils.FullGitInfo gitInfo = optGitInfo.get();
-                        Logger.trInfo("display.bootstrap-version");
-                        Logger.info("");
-                        Logger.trInfo("display.version.major", gitInfo.getMainVersion());
-                        Logger.trInfo("display.version.full", "git-" + gitInfo.getBranchID() + "-" + gitInfo.getCommitID());
-                        Logger.trInfo("display.version.time", gitInfo.getTime());
-                        Logger.info("");
-                    } else {
-                        Logger.trWarn("display.bootstrap-break");
-                    }
-            }
-            if (arg.contains("pnx")) {
-                JarLocator pnxLocator = new JarLocator(workingDir, "cn.nukkit.api.PowerNukkitOnly");
-                List<Location<JarLocator.JarInfo>> result = pnxLocator.locate();
-                if (result.size() == 0) {
-                    Logger.trWarn("display.server-not-found");
-                } else if (result.size() > 1) {
-                    Logger.trWarn("display.server-multi-conflict");
-                } else {
-                    Location<JarLocator.JarInfo> location = result.get(0);
-                    if (location.getInfo().getGitInfo().isPresent()) {
-                        GitUtils.FullGitInfo gitInfo = location.getInfo().getGitInfo().get();
-                        Logger.trInfo("display.server-version");
-                        Logger.info("");
-                        Logger.trInfo("display.version.path", location.getFile().getAbsolutePath());
-                        Logger.trInfo("display.version.major", gitInfo.getMainVersion());
-                        Logger.trInfo("display.version.full", "git-" + gitInfo.getBranchID() + "-" + gitInfo.getCommitID());
-                        Logger.trInfo("display.version.time", gitInfo.getTime());
-                        Logger.info("");
-                    } else {
-                        Logger.trWarn("display.server-break");
-                    }
-                }
-            }
-            if (arg.contains("java")) {
-                JavaLocator javaLocator = new JavaLocator(null);
-                Logger.trInfo("display.java-version");
-                for (final Location<JavaLocator.JavaInfo> each : javaLocator.locate()) {
-                    Logger.info("");
-                    Logger.trInfo("display.version.path", each.getFile().getAbsolutePath());
-                    Logger.trInfo("display.version.major", each.getInfo().getMajorVersion());
-                    Logger.trInfo("display.version.full", each.getInfo().getFullVersion());
-                    Logger.trInfo("display.version.vendor", each.getInfo().getVendor());
-                }
-                Logger.info("");
-            }
-            startPNX = false;
+            exec("CheckVersion", options.valueOf(versionSpec));
         }
 
         if (startPNX) {
