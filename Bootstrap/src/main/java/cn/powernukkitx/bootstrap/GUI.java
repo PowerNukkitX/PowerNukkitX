5dddd5e643d0188fc1a4f9f7b016e7d507d8b6f6
超神的冰凉 <2801621420@qq.com>
2022-03-16 14:02:52 +0800

    引入swing开发者工具

--- Bootstrap/src/main/java/cn/powernukkitx/bootstrap/GUI.java
+++ Bootstrap/src/main/java/cn/powernukkitx/bootstrap/GUI.java
@@ -2,12 +2,16 @@ package cn.powernukkitx.bootstrap;
 
 import cn.powernukkitx.bootstrap.gui.controller.MainWindowController;
 import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.extras.FlatInspector;
+import com.formdev.flatlaf.extras.FlatUIDefaultsInspector;
 
 public final class GUI implements Program {
     @Override
     public void exec(String... args) {
         // TODO: 2022/3/12 完成GUI
         FlatDarculaLaf.setup();
+        FlatInspector.install( "ctrl shift alt X" );
+        FlatUIDefaultsInspector.install( "ctrl shift alt Y" );
         // 创建主窗口
         final MainWindowController mainWindowController = new MainWindowController();
         mainWindowController.start();
