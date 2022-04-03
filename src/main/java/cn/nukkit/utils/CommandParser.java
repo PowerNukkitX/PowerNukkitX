2fdfd6cef9951fc22b24d8dd882083fae865056b
smart_cmd <3523206925@qq.com>
2022-04-03 21:23:16 +0800

    更换目标选择器的正则表达式

--- src/main/java/cn/nukkit/utils/CommandParser.java
+++ src/main/java/cn/nukkit/utils/CommandParser.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 public class CommandParser {
 
     private static final String STRING_PATTERN = "(\\S+)";
-    private static final String TARGET_PATTERN = "(@\\S+|[A-Za-z][A-Za-z0-9\\s]+)";
+    private static final String TARGET_PATTERN = "(@[aeprs](?:\\[[^ ]*])?|[A-Za-z][A-Za-z0-9\\s]+)";
     private static final String MULTIPLE_STRING_PATTERN = "(.+)";
     private static final String INT_PATTERN = "(~-?\\d+|-?\\d+|~)";//only int
     private static final String FLOAT_PATTERN = "(~-?\\d+(?:\\.\\d+)?|-?\\d+(?:\\.\\d+)?|~)";//float or int
