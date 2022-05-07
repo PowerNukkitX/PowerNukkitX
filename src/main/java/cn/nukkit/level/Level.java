c83875d8d64ea1320377d54609cf6dc05689ebb0
超神的冰凉 <2801621420@qq.com>
2022-05-07 17:31:13 +0800

    优化并行化计算运动逻辑

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -491,7 +491,7 @@ public class Level implements ChunkManager, Metadatable {
     }
 
     public int recalcTickOptDelay() {
-        if (tickRateTime > 50) {
+        if (tickRateTime > 40) {
             return Math.min(tickRateOptDelay << 1, 8);
         } else if (tickRateOptDelay == 1) {
             return 1;
@@ -996,20 +996,18 @@ public class Level implements ChunkManager, Metadatable {
         this.timings.entityTick.startTiming();
 
         if (!this.updateEntities.isEmpty()) {
-            var start = System.currentTimeMillis();
-            CompletableFuture.anyOf(CompletableFuture.runAsync(() -> updateEntities.keySet()
-                            .longParallelStream().mapToObj(id -> {
-                                var entity = this.updateEntities.get(id);
-                                if (entity instanceof EntityAsyncPrepare entityAsyncPrepare) {
-                                    return entityAsyncPrepare;
-                                } else {
-                                    return null;
-                                }
-                            }).forEach(entityAsyncPrepare -> {
-                                if (entityAsyncPrepare != null)
-                                    entityAsyncPrepare.asyncPrepare(currentTick);
-                            }), Server.getInstance().computeThreadPool));
-            System.out.println(System.currentTimeMillis() - start);
+            CompletableFuture.runAsync(() -> updateEntities.keySet()
+                    .longParallelStream().mapToObj(id -> {
+                        var entity = this.updateEntities.get(id);
+                        if (entity instanceof EntityAsyncPrepare entityAsyncPrepare) {
+                            return entityAsyncPrepare;
+                        } else {
+                            return null;
+                        }
+                    }).forEach(entityAsyncPrepare -> {
+                        if (entityAsyncPrepare != null)
+                            entityAsyncPrepare.asyncPrepare(currentTick);
+                    }), Server.getInstance().computeThreadPool).join();
             for (long id : new ArrayList<>(this.updateEntities.keySet())) {
                 Entity entity = this.updateEntities.get(id);
                 if (entity == null) {
@@ -2409,7 +2407,7 @@ public class Level implements ChunkManager, Metadatable {
                 BlockBreakEvent ev = new BlockBreakEvent(player, target, face, item, eventDrops, player.isCreative(),
                         fastBreak);
 
-                if (player.isSurvival() && !target.isBreakable(item) ) {
+                if (player.isSurvival() && !target.isBreakable(item)) {
                     ev.setCancelled();
                 } else if (!player.isOp() && isInSpawnRadius(target)) {
                     ev.setCancelled();
