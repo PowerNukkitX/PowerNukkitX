3360c994156a3578e5a95a3a9367ec59ff6f6fb9
zimzaza4 <1875162740@qq.com>
2022-05-15 01:44:50 +0800

    Mob equipment support

--- src/main/java/cn/nukkit/entity/mob/EntityMob.java
+++ src/main/java/cn/nukkit/entity/mob/EntityMob.java
@@ -1,15 +1,69 @@
 package cn.nukkit.entity.mob;
 
 import cn.nukkit.entity.EntityPhysical;
+import cn.nukkit.inventory.EntityArmorInventory;
+import cn.nukkit.inventory.EntityEquipmentInventory;
+import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.NBTIO;
 import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.ListTag;
+import lombok.Getter;
 
 /**
  * @author MagicDroidX (Nukkit Project)
  */
 public abstract class EntityMob extends EntityPhysical {
+
+    private static final String TAG_MAINHAND = "Mainhand";
+    private static final String TAG_OFFHAND = "Offhand";
+    private static final String TAG_ARMOR = "Armor";
+
+    @Getter
+    private EntityEquipmentInventory equipmentInventory;
+
+    @Getter
+    private EntityArmorInventory armorInventory;
+
     public EntityMob(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
     }
 
+    @Override
+    protected void initEntity() {
+        super.initEntity();
+
+        this.equipmentInventory = new EntityEquipmentInventory(this);
+        this.armorInventory = new EntityArmorInventory(this);
+
+        if (this.namedTag.contains(TAG_MAINHAND)) {
+            this.equipmentInventory.setItemInHand(NBTIO.getItemHelper(this.namedTag.getCompound(TAG_MAINHAND)), true);
+        }
+
+        if (this.namedTag.contains(TAG_OFFHAND)) {
+            this.equipmentInventory.setItemInOffhand(NBTIO.getItemHelper(this.namedTag.getCompound(TAG_OFFHAND)), true);
+        }
+
+        if (this.namedTag.contains(TAG_ARMOR)) {
+            ListTag<CompoundTag> armorList = this.namedTag.getList(TAG_ARMOR, CompoundTag.class);
+            for (CompoundTag armorTag : armorList.getAll()) {
+                this.armorInventory.setItem(armorTag.getByte("Slot"), NBTIO.getItemHelper(armorTag));
+            }
+        }
+    }
+
+    @Override
+    public void saveNBT() {
+        super.saveNBT();
+        this.namedTag.put(TAG_MAINHAND, NBTIO.putItemHelper(this.equipmentInventory.getItemInHand()));
+        this.namedTag.put(TAG_OFFHAND, NBTIO.putItemHelper(this.equipmentInventory.getItemInOffhand()));
+
+        if (this.armorInventory != null) {
+            ListTag<CompoundTag> armorTag = new ListTag<>(TAG_ARMOR);
+            for (int i = 0; i < 4; i++) {
+                armorTag.add(NBTIO.putItemHelper(this.armorInventory.getItem(i), i));
+            }
+            this.namedTag.putList(armorTag);
+        }
+    }
 }
