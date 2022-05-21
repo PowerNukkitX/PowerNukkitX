fc25e7a5ba6d5a176a7a7605386635e981121a55
daoge <60499296+smartcmd@users.noreply.github.com>
2022-05-21 14:40:48 +0800

    修复NPC实体删除问题

--- src/main/java/cn/nukkit/entity/passive/EntityNPCEntity.java
+++ src/main/java/cn/nukkit/entity/passive/EntityNPCEntity.java
@@ -1,11 +1,21 @@
 package cn.nukkit.entity.passive;
 
+import cn.nukkit.Player;
+import cn.nukkit.api.PowerNukkitDifference;
 import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
+import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.EntityInteractable;
 import cn.nukkit.entity.EntityLiving;
+import cn.nukkit.entity.data.ByteEntityData;
+import cn.nukkit.entity.data.StringEntityData;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
+import cn.nukkit.event.entity.EntityDamageEvent;
+import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.math.Vector3;
 import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.network.protocol.NPCDialoguePacket;
 
 /**
  * @author good777LUCKY
@@ -63,5 +73,30 @@ public class EntityNPCEntity extends EntityLiving implements EntityNPC, EntityIn
         this.setHealth(20);
         this.setNameTagVisible(true);
         this.setNameTagAlwaysVisible(true);
+        this.setDataProperty(new ByteEntityData(Entity.DATA_HAS_NPC_COMPONENT,1));
+        this.setDataProperty(new StringEntityData(Entity.DATA_NPC_DATA,"{\"picker_offsets\":{\"scale\":[1.70,1.70,1.70],\"translate\":[0,20,0]},\"portrait_offsets\":{\"scale\":[1.750,1.750,1.750],\"translate\":[-7,50,0]},\"skin_list\":[{\"variant\":0},{\"variant\":1},{\"variant\":2},{\"variant\":3},{\"variant\":4},{\"variant\":5},{\"variant\":6},{\"variant\":7},{\"variant\":8},{\"variant\":9},{\"variant\":10},{\"variant\":11},{\"variant\":12},{\"variant\":13},{\"variant\":14},{\"variant\":15},{\"variant\":16},{\"variant\":17},{\"variant\":18},{\"variant\":19},{\"variant\":20},{\"variant\":21},{\"variant\":22},{\"variant\":23},{\"variant\":24}]}"));
+        this.setDataProperty(new StringEntityData(Entity.DATA_URL_TAG,""));
+        this.setDataProperty(new StringEntityData(Entity.DATA_NAME_RAW_TEXT,"NPC"));
+        this.setDataProperty(new StringEntityData(Entity.DATA_INTERACTIVE_TAG,""));
+    }
+
+    //todo: remove this DEBUG
+    @Override
+    public boolean onInteract(Player player, Item item, Vector3 clickedPos) {
+        long uniqueId = this.getId();
+        NPCDialoguePacket packet = new NPCDialoguePacket();
+        packet.setRuntimeEntityId(uniqueId);
+        packet.setAction(NPCDialoguePacket.NPCDialogAction.OPEN);
+        player.dataPacket(packet);
+        return true;
+    }
+
+    @PowerNukkitDifference(info = "Using new method to play sounds", since = "1.4.0.0-PN")
+    @Override
+    public boolean attack(EntityDamageEvent source) {
+        if (source instanceof EntityDamageByEntityEvent event && event.getDamager() instanceof Player damager && damager.isCreative()) {
+            this.kill();
+        }
+        return false;
     }
 }
