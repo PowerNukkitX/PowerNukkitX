a3e589b0397b24440eca3ea0d947018365c00f4b
CoolLoong <1542536763@qq.com>
2022-06-15 23:12:47 +0800

    fix #401

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -2636,10 +2636,10 @@ public class Level implements ChunkManager, Metadatable {
 
 //        cause bug (eg: frog_spawn) (and I don't know what this is for)
 //        2022/6/11 @daoge_cmd
-//        if (target.canBeReplaced()) {
-//            block = target;
-//            hand.position(block);
-//        }
+        if (!(hand instanceof BlockFrogSpawn) && target.canBeReplaced()) {
+            block = target;
+            hand.position(block);
+        }
 
         if (!hand.canPassThrough() && hand.getBoundingBox() != null) {
             Entity[] entities = this.getCollidingEntities(hand.getBoundingBox());
