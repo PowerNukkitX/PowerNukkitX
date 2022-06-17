35cf98625d8ece18f931c566bb19494f4c2d7f66
CoolLoong <1542536763@qq.com>
2022-06-17 16:13:49 +0800

    fix

--- src/main/java/cn/nukkit/item/food/FoodChorusFruit.java
+++ src/main/java/cn/nukkit/item/food/FoodChorusFruit.java
@@ -34,7 +34,7 @@ public class FoodChorusFruit extends FoodNormal {
 
         Level level = player.getLevel();
         if (level == null) return false;
-        if (level.getBlock(player.up()) instanceof BlockLiquid) return false;
+        if (player.isInsideOfWater()) return false;
         NukkitRandom random = new NukkitRandom();
         for (int attempts = 0; attempts < 128; attempts++) {
             int x = random.nextRange(minX, maxX);
