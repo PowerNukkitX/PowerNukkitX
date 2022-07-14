ed6624e971baa88eb45b31a884c5c2bbdb0035fd
CoolLoong <1542536763@qq.com>
2022-07-15 00:07:49 +0800

    修改为接口实现 (#459)

--- src/main/java/cn/nukkit/entity/custom/CustomEntity.java
+++ src/main/java/cn/nukkit/entity/custom/CustomEntity.java
@@ -1,19 +1,5 @@
 package cn.nukkit.entity.custom;
 
-import cn.nukkit.entity.Entity;
-import cn.nukkit.level.format.FullChunk;
-import cn.nukkit.nbt.tag.CompoundTag;
-
-public abstract class CustomEntity extends Entity {
-    public CustomEntity(FullChunk chunk, CompoundTag nbt) {
-        super(chunk, nbt);
-    }
-
-    @Override
-    public int getNetworkId() {
-        return 0;
-    }
-
-    public abstract CustomEntityDefinition getDefinition();
-
+public interface CustomEntity {
+    CustomEntityDefinition getDefinition();
 }
