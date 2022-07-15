b82724886c93724ded0a2aee58c9849e1cab5c85
daoge <60499296+smartcmd@users.noreply.github.com>
2022-07-15 13:28:35 +0800

    补全todo

--- src/main/java/cn/nukkit/entity/ai/route/SimpleAStarRouteFinder.java
+++ src/main/java/cn/nukkit/entity/ai/route/SimpleAStarRouteFinder.java
@@ -357,6 +357,7 @@ public class SimpleAStarRouteFinder extends SimpleRouteFinder {
         if (limit > 0) {
             for (int y = vector3.getFloorY(); y >= vector3.getFloorY() - limit; y--) {
                 Block block = this.level.getBlock(vector3.getFloorX(), y, vector3.getFloorZ(), false);
+                //todo: 实现方块权重
                 if ((evalBlock(block) != -1)/* && level.getBlock(block.add(0, 1, 0), false).getId() == Block.AIR*/)
                     return block;
             }
@@ -364,6 +365,7 @@ public class SimpleAStarRouteFinder extends SimpleRouteFinder {
         }
         for (int y = vector3.getFloorY(); y >= -64; y--) {
             Block block = this.level.getBlock(vector3.getFloorX(), y, vector3.getFloorZ(), false);
+            //todo: 实现方块权重
             if ((evalBlock(block) != -1)/* && level.getBlock(block.add(0, 1, 0), false).getId() == Block.AIR*/)
                 return block;
         }
