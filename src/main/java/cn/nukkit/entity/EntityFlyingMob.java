7546520f19fcc6ce0390da439331092221f9d0be
超神的冰凉 <2801621420@qq.com>
2022-07-20 11:38:26 +0800

    修复飞行实体运动

--- src/main/java/cn/nukkit/entity/EntityFlyingMob.java
+++ src/main/java/cn/nukkit/entity/EntityFlyingMob.java
@@ -15,4 +15,35 @@ public abstract class EntityFlyingMob extends EntityMob {
     public EntityFlyingMob(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
     }
+
+
+    @Override
+    protected void handleGravity() {
+        resetFallDistance();
+    }
+
+    @Override
+    public float getGravity() {
+        return 0;
+    }
+
+    @Override
+    protected void handleFrictionMovement() {
+
+    }
+
+    @Override
+    protected void handleFloatingMovement() {
+        if (this.isTouchingWater()) {
+            this.motionY += getMovementSpeed() * 0.3;
+        }
+    }
+
+    @Override
+    public void updateMovement() {
+        super.updateMovement();
+        this.motionX = 0;
+        this.motionY = 0;
+        this.motionZ = 0;
+    }
 }
