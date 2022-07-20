9fdac4faef00b29d0a14fdf20f9d2c52d34f6d6b
超神的冰凉 <2801621420@qq.com>
2022-07-20 16:45:49 +0800

    修复三维寻路路径平滑

--- src/main/java/cn/nukkit/entity/ai/route/SimpleSpaceAStarRouteFinder.java
+++ src/main/java/cn/nukkit/entity/ai/route/SimpleSpaceAStarRouteFinder.java
@@ -7,7 +7,10 @@ import cn.nukkit.entity.ai.route.blockevaluator.IBlockEvaluator;
 import cn.nukkit.entity.ai.route.data.Node;
 import cn.nukkit.level.Level;
 import cn.nukkit.level.Position;
+import cn.nukkit.math.AxisAlignedBB;
+import cn.nukkit.math.SimpleAxisAlignedBB;
 import cn.nukkit.math.Vector3;
+import cn.nukkit.utils.Utils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -41,9 +44,6 @@ public class SimpleSpaceAStarRouteFinder extends SimpleFlatAStarRouteFinder {
                 for (int dy = -1; dy <= 1; dy++) {
                     var vec = node.getVector3().add(dx, dy, dz);
                     if (!existInCloseList(vec) && evalBlock(entity.level.getTickCachedBlock(vec))) {
-//                        if (vec.y % 1 + entity.getHeight() > 1 && evalBlock(entity.level.getTickCachedBlock(vec.add(0, 1,0)))) {
-//                            continue;
-//                        }
                         // 计算移动1格的开销
                         var cost = switch (Math.abs(dx) + Math.abs(dy) + Math.abs(dz)) {
                             case 1 -> DIRECT_MOVE_COST;
@@ -71,5 +71,51 @@ public class SimpleSpaceAStarRouteFinder extends SimpleFlatAStarRouteFinder {
     @Override
     protected ArrayList<Node> FloydSmooth(ArrayList<Node> array) {
         return super.FloydSmooth(array);
+    }
+
+//    @Override
+//    protected boolean hasBarrier(Vector3 pos1, Vector3 pos2) {
+//        var list = new ArrayList<Vector3>();
+//        var rx = pos1.x;
+//        var rz = pos1.z;
+//        var ry = pos1.y;
+//        var dx = pos2.x - rx;
+//        var dz = pos2.z - rz;
+//        var dy = pos2.y - ry;
+//        double px;
+//        double pz;
+//        double py;
+//        int cnt;
+//        if (Math.abs(dx) > Math.abs(dz)) {
+//            px = 1;
+//            cnt = (int) Math.abs(dx);
+//            pz = dz / cnt;
+//        } else {
+//            pz = 1;
+//            cnt = (int) Math.abs(dz);
+//            px = dx / cnt;
+//        }
+//        py = dy / cnt;
+//        rx -= px;
+//        rz -= pz;
+//        ry -= py;
+//        for (int i = 0; i < cnt; i++) {
+//            rx += px;
+//            rz += pz;
+//            ry += py;
+//            list.add(new Vector3(rx, ry, rz));
+//        }
+//        var tmp = hasBlocksAround(list);
+//        return tmp;
+//    }
+
+    protected boolean hasBlocksAround(ArrayList<Vector3> list) {
+        double radius = (this.entity.getWidth() * this.entity.getScale()) / 2 + 0.1;
+        double height = this.entity.getHeight() * this.entity.getScale();
+        for (Vector3 vector3 : list) {
+            AxisAlignedBB bb = new SimpleAxisAlignedBB(vector3.getX() - radius, vector3.getY(), vector3.getZ() - radius, vector3.getX() + radius, vector3.getY() + height, vector3.getZ() + radius);
+            if (Utils.hasCollisionTickCachedBlocks(level, bb)) return true;
+        }
+        return false;
     }
 }
