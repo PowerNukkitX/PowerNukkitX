d098b922e82e18b424495ff1f189bb38832c41a0
刀哥 <3523206925@qq.com>
2022-07-21 22:56:04 +0800

    接口化 EntityTameable.java

--- src/main/java/cn/nukkit/entity/passive/EntityAnimal.java
+++ src/main/java/cn/nukkit/entity/passive/EntityAnimal.java
@@ -1,11 +1,19 @@
 package cn.nukkit.entity.passive;
 
+import cn.nukkit.Player;
+import cn.nukkit.api.PowerNukkitXOnly;
+import cn.nukkit.api.Since;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.EntityAgeable;
 import cn.nukkit.entity.EntityIntelligent;
+import cn.nukkit.entity.data.ByteEntityData;
+import cn.nukkit.entity.data.StringEntityData;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
+
+import static cn.nukkit.entity.passive.EntityTamable.DATA_OWNER_NAME;
+import static cn.nukkit.entity.passive.EntityTamable.DATA_TAMED_FLAG;
 
 /**
  * @author MagicDroidX (Nukkit Project)
@@ -13,6 +21,103 @@ import cn.nukkit.nbt.tag.CompoundTag;
 public abstract class EntityAnimal extends EntityIntelligent implements EntityAgeable {
     public EntityAnimal(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
+    }
+
+    @Override
+    protected void initEntity() {
+        super.initEntity();
+        if (this instanceof EntityTamable){
+            if (getDataProperty(DATA_TAMED_FLAG) == null) {
+                setDataProperty(new ByteEntityData(DATA_TAMED_FLAG, (byte) 0));
+            }
+
+            if (getDataProperty(DATA_OWNER_NAME) == null) {
+                setDataProperty(new StringEntityData(DATA_OWNER_NAME, ""));
+            }
+
+            String ownerName = "";
+
+            if (namedTag != null) {
+                if (namedTag.contains("Owner")) {
+                    ownerName = namedTag.getString("Owner");
+                }
+
+                if (ownerName.length() > 0) {
+                    this.setOwnerName(ownerName);
+                    this.setTamed(true);
+                }
+
+                this.setSitting(namedTag.getBoolean("Sitting"));
+            }
+        }
+    }
+
+    @Override
+    public void saveNBT() {
+        super.saveNBT();
+        if (this instanceof EntityTamable){
+            if (this.getOwnerName() == null) {
+                namedTag.putString("Owner", "");
+            } else {
+                namedTag.putString("Owner", getOwnerName());
+            }
+
+            namedTag.putBoolean("Sitting", isSitting());
+        }
+    }
+
+    @PowerNukkitXOnly
+    @Since("1.6.0.0-PNX")
+    public String getOwnerName() {
+        return getDataPropertyString(DATA_OWNER_NAME);
+    }
+
+    @PowerNukkitXOnly
+    @Since("1.6.0.0-PNX")
+    public void setOwnerName(String playerName) {
+        setDataProperty(new StringEntityData(DATA_OWNER_NAME, playerName));
+    }
+
+    @PowerNukkitXOnly
+    @Since("1.6.0.0-PNX")
+    public Player getOwner() {
+        return getServer().getPlayer(getOwnerName());
+    }
+
+    @PowerNukkitXOnly
+    @Since("1.6.0.0-PNX")
+    public boolean isTamed() {
+        return (getDataPropertyByte(DATA_TAMED_FLAG) & 4) != 0;
+    }
+
+    @PowerNukkitXOnly
+    @Since("1.6.0.0-PNX")
+    public void setTamed(boolean flag) {
+        int var = getDataPropertyByte(DATA_TAMED_FLAG); // ?
+
+        if (flag) {
+            setDataProperty(new ByteEntityData(DATA_TAMED_FLAG, (byte) (var | 4)));
+        } else {
+            setDataProperty(new ByteEntityData(DATA_TAMED_FLAG, (byte) (var & -5)));
+        }
+    }
+
+    @PowerNukkitXOnly
+    @Since("1.6.0.0-PNX")
+    public boolean isSitting() {
+        return (getDataPropertyByte(DATA_TAMED_FLAG) & 1) != 0;
+    }
+
+    @PowerNukkitXOnly
+    @Since("1.6.0.0-PNX")
+    public void setSitting(boolean flag) {
+        int var = getDataPropertyByte(DATA_TAMED_FLAG); // ?
+
+        if (flag) {
+            setDataProperty(new ByteEntityData(DATA_TAMED_FLAG, (byte) (var | 1)));
+        } else {
+            setDataProperty(new ByteEntityData(DATA_TAMED_FLAG, (byte) (var & -2)));
+        }
     }
 
     @Override
