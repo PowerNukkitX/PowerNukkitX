104965343f572dcf13f4ce9e98db30566787046a
超神的冰凉 <2801621420@qq.com>
2022-07-21 17:03:13 +0800

    优化IPosEvaluator中的注释

--- src/main/java/cn/nukkit/entity/ai/route/posevaluator/IPosEvaluator.java
+++ src/main/java/cn/nukkit/entity/ai/route/posevaluator/IPosEvaluator.java
@@ -15,20 +15,26 @@ import org.jetbrains.annotations.NotNull;
 @Since("1.6.0.0-PNX")
 public interface IPosEvaluator {
     /**
-     * 返回目标坐标是否可以作为路径点，通常用于返回非整数坐标点（飞行和游泳）的实体
+     * 返回目标坐标是否可以作为路径点，通常用于返回非整数坐标点（飞行和游泳）的实体 <br>
+     * 如果此使用此评估器的寻路器返回非整数坐标点，才需要实现此方法。
      *
      * @param entity 目标实体
      * @param pos  评估坐标
      * @return 是否可以作为路径点
      */
-    boolean evalPos(@NotNull EntityIntelligent entity, @NotNull Vector3 pos);
+    default boolean evalPos(@NotNull EntityIntelligent entity, @NotNull Vector3 pos) {
+        return true;
+    }
 
     /**
-     * 返回此方块是否可以作为脚下站立的方块，通常用于返回整数坐标点（行走）的实体
+     * 返回此方块是否可以作为脚下站立的方块，通常用于返回整数坐标点（行走）的实体 <br>
+     * 如果此使用此评估器的寻路器只返回整数坐标点，才需要实现此方块。
      *
      * @param entity 目标实体
      * @param block 评估方块
      * @return 是否可以作为脚下站立的方块
      */
-    boolean evalStandingBlock(@NotNull EntityIntelligent entity, @NotNull Block block);
+    default boolean evalStandingBlock(@NotNull EntityIntelligent entity, @NotNull Block block) {
+        return true;
+    }
 }
