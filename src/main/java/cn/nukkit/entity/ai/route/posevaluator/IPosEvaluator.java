3b8a38074095e85cfd7aea29762090b2fc33c616
刀哥 <3523206925@qq.com>
2022-07-21 14:16:57 +0800

    修复冰凉漏写的 "!" 导致的寻路器错误

--- src/main/java/cn/nukkit/entity/ai/route/posevaluator/IPosEvaluator.java
+++ src/main/java/cn/nukkit/entity/ai/route/posevaluator/IPosEvaluator.java
@@ -2,13 +2,12 @@ package cn.nukkit.entity.ai.route.posevaluator;
 
 import cn.nukkit.api.PowerNukkitXOnly;
 import cn.nukkit.api.Since;
-import cn.nukkit.block.Block;
 import cn.nukkit.entity.EntityIntelligent;
 import cn.nukkit.math.Vector3;
 
 /**
- * 方块评估器用于寻路器评估方块 <br/>
- * 通过编写特定的方块评估器，可以自定义寻路器的寻路策略
+ * 坐标评估器用于寻路器评估坐标 <br/>
+ * 通过编写特定的坐标评估器，可以自定义寻路器的寻路策略
  */
 @PowerNukkitXOnly
 @Since("1.6.0.0-PNX")
