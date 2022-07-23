7ce128847e15b588f34957aadba76264c30618ae
刀哥 <3523206925@qq.com>
2022-07-23 18:41:52 +0800

    now sheep will avoid walk into water

--- src/main/java/cn/nukkit/entity/ai/executor/RandomRoamExecutor.java
+++ src/main/java/cn/nukkit/entity/ai/executor/RandomRoamExecutor.java
@@ -1,5 +1,6 @@
 package cn.nukkit.entity.ai.executor;
 
+import cn.nukkit.block.Block;
 import cn.nukkit.entity.EntityIntelligent;
 import cn.nukkit.entity.ai.memory.LookTargetMemory;
 import cn.nukkit.entity.ai.memory.MoveTargetMemory;
@@ -17,14 +18,26 @@ public class RandomRoamExecutor implements IBehaviorExecutor{
     protected int currentTargetCalTick = 0;
     protected boolean calNextTargetImmediately = false;
     protected boolean keepRunning;
+    protected boolean avoidWater;
+    protected int maxRetryTime;
 
-    public RandomRoamExecutor(float speed,int maxRoamRange, int frequency, boolean calNextTargetImmediately, boolean keepRunning) {
+    public RandomRoamExecutor(float speed,int maxRoamRange, int frequency){
+        this(speed,maxRoamRange,frequency,false,false);
+    }
+
+    public RandomRoamExecutor(float speed,int maxRoamRange, int frequency, boolean calNextTargetImmediately, boolean keepRunning){
+        this(speed,maxRoamRange,frequency,calNextTargetImmediately,keepRunning,false,10);
+    }
+
+    public RandomRoamExecutor(float speed,int maxRoamRange, int frequency, boolean calNextTargetImmediately, boolean keepRunning, boolean avoidWater, int maxRetryTime) {
         this.speed = speed;
         this.maxRoamRange = maxRoamRange;
         this.frequency = frequency;
         this.currentTargetCalTick = this.frequency;
         this.calNextTargetImmediately = calNextTargetImmediately;
         this.keepRunning = keepRunning;
+        this.avoidWater = avoidWater;
+        this.maxRetryTime = maxRetryTime;
     }
 
     @Override
@@ -33,7 +46,15 @@ public class RandomRoamExecutor implements IBehaviorExecutor{
         if (currentTargetCalTick >= frequency || (calNextTargetImmediately && needUpdateTarget(entity))) {
             //roam to a random location
             var random = ThreadLocalRandom.current();
-            Vector3 target = entity.getPosition().add(random.nextInt(maxRoamRange * 2) - maxRoamRange, 0, random.nextInt(maxRoamRange * 2) - maxRoamRange);
+            Vector3 target = next(entity);
+            if (avoidWater) {
+                int blockId = -1;
+                int time = 0;
+                while (time <= maxRetryTime && ((blockId = entity.level.getBlock(target.add(0, -1, 0)).getId()) == Block.FLOWING_WATER || blockId == Block.STILL_WATER)) {
+                    target = next(entity);
+                    time++;
+                }
+            }
             if (entity.getMovementSpeed() != speed)
                 entity.setMovementSpeed(speed);
             //更新寻路target
--- src/main/java/cn/nukkit/entity/passive/EntitySheep.java
+++ src/main/java/cn/nukkit/entity/passive/EntitySheep.java
@@ -41,11 +41,11 @@ public class EntitySheep extends EntityWalkingAnimal {
     public static final int NETWORK_ID = 13;
     private final IBehaviorGroup behaviorGroup = new BehaviorGroup(
             Set.of(
-                    new Behavior(new RandomRoamExecutor(0.5f, 8, 40, true,false), new AttackTimeCheckEvaluator(100), 5, 1),
+                    new Behavior(new RandomRoamExecutor(0.5f, 8, 40, true,false,true,10), new AttackTimeCheckEvaluator(100), 5, 1),
                     new Behavior(new MoveToTargetExecutor(NearestBeggingPlayerMemory.class, 0.3f), new MemoryCheckEvaluator(NearestBeggingPlayerMemory.class), 4, 1),
                     new Behavior(new EatGrassExecutor(40), new AllMatchEvaluator(new ProbabilityEvaluator(1),new AnyMatchEvaluator(new BlockCheckEvaluator(Block.GRASS,new Vector3(0,-1,0)),new BlockCheckEvaluator(Block.TALL_GRASS,Vector3.ZERO))),3,1),
                     new Behavior(new LookAtTargetExecutor(NearestPlayerMemory.class,100), new ProbabilityEvaluator(10), 2, 1),
-                    new Behavior(new RandomRoamExecutor(0.1f, 8, 100, false,true), (entity -> true), 1, 1)
+                    new Behavior(new RandomRoamExecutor(0.1f, 8, 100, false,true,true,10), (entity -> true), 1, 1)
             ),
             Set.of(new NearestBeggingPlayerSensor(8, 0), new NearestPlayerSensor(8, 0)),
             Set.of(new WalkController(), new LookController(true, true)),
