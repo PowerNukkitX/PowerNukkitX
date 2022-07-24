2609cd56793b443cd004f9255732d897f7f72583
刀哥 <3523206925@qq.com>
2022-07-24 19:00:50 +0800

    增加/tps命令

--- src/main/java/cn/nukkit/command/SimpleCommandMap.java
+++ src/main/java/cn/nukkit/command/SimpleCommandMap.java
@@ -31,6 +31,7 @@ public class SimpleCommandMap implements CommandMap {
 
     private void setDefaultCommands() {
         this.register("nukkit",new ExecuteCommand("execute"));
+        this.register("nukkit",new TpsCommand("tps"));
         this.register("nukkit",new TickingAreaCommand("tickingarea"));
         this.register("nukkit",new TellrawCommand("tellraw"));
         this.register("nukkit",new TitlerawCommand("titleraw"));
