9dfddbc02e87b352a9295abb2d58857be96ab071
刀哥 <3523206925@qq.com>
2022-07-26 15:14:44 +0800

    通过延长部分行为的评估周期进一步优化性能

--- src/main/java/cn/nukkit/entity/passive/EntitySheep.java
+++ src/main/java/cn/nukkit/entity/passive/EntitySheep.java
@@ -36,20 +36,27 @@ public class EntitySheep extends EntityWalkingAnimal {
     public static final int NETWORK_ID = 13;
     private final IBehaviorGroup behaviorGroup = new BehaviorGroup(
             Set.of(
+                    //用于刷新InLove状态的核心行为
                     new Behavior(
                             new InLoveExecutor(400),
                             new AllMatchEvaluator(
                                     new PassByTimeEvaluator<>(PlayerBreedingMemory.class,0,400),
                                     new PassByTimeEvaluator<>(InLoveMemory.class,6000,Integer.MAX_VALUE,true)
                             ),
-                            1,1,20
+                            1,1
                     )
             ),
             Set.of(
                     new Behavior(new RandomRoamExecutor(0.5f, 8, 40, true,false,true,10), new PassByTimeEvaluator<>(AttackMemory.class,0,100), 5, 1),
                     new Behavior(new MoveToTargetExecutor(NearestBeggingPlayerMemory.class, 0.3f), new MemoryCheckNotEmptyEvaluator(NearestBeggingPlayerMemory.class), 4, 1),
-                    new Behavior(new EatGrassExecutor(40), new AllMatchEvaluator(new ProbabilityEvaluator(1,1000),new AnyMatchEvaluator(new BlockCheckEvaluator(Block.GRASS,new Vector3(0,-1,0)),new BlockCheckEvaluator(Block.TALL_GRASS,Vector3.ZERO))),3,1),
-                    new Behavior(new LookAtTargetExecutor(NearestPlayerMemory.class,100), new ProbabilityEvaluator(5,1000), 2, 1),
+                    new Behavior(new EatGrassExecutor(40), new AllMatchEvaluator(
+                            new ProbabilityEvaluator(1,10),
+                            new AnyMatchEvaluator(
+                                    new BlockCheckEvaluator(Block.GRASS,new Vector3(0,-1,0)),
+                                    new BlockCheckEvaluator(Block.TALL_GRASS,Vector3.ZERO))),
+                            3,1,100
+                    ),
+                    new Behavior(new LookAtTargetExecutor(NearestPlayerMemory.class,100), new ProbabilityEvaluator(5,10), 2, 1,100),
                     new Behavior(new RandomRoamExecutor(0.1f, 8, 100, false,true,true,10), (entity -> true), 1, 1)
             ),
             Set.of(new NearestBeggingPlayerSensor(8, 0), new NearestPlayerSensor(8, 0)),
