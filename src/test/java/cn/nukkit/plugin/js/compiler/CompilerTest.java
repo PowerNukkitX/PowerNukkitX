5b74c60f6b3967254923267a54fb7b3fb5db94af
超神的冰凉 <2801621420@qq.com>
2022-07-28 22:59:13 +0800

    添加简单构造函数的单元测试

--- src/test/java/cn/nukkit/plugin/js/compiler/CompilerTest.java
+++ src/test/java/cn/nukkit/plugin/js/compiler/CompilerTest.java
@@ -1,10 +1,13 @@
 package cn.nukkit.plugin.js.compiler;
 
-import cn.nukkit.plugin.CommonJSPlugin;
 import lombok.SneakyThrows;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class CompilerTest {
@@ -19,12 +22,43 @@ public class CompilerTest {
 
     @SneakyThrows
     @Test
-    public void constructorCompile() {
-        var builder = new JClassBuilder((Context) null)
-                .setClassName("ConstructorTestClass");
-        builder.addConstructor(new JConstructor(builder, "super_0_js", new JType[0], JType.of(String.class)));
-        builder.compileToFile(Path.of("./target/ConstructorTestClass.class"));
+    public void simpleConstructorCompile() {
+        var ctx = makeContext();
+        var builder = new JClassBuilder(ctx)
+                .setDelegate(execJS(ctx, "simpleConstructorCompile.js", "var a = {testSuper: () => {print('Hello world!');return [];}};a"))
+                .setClassName("SimpleConstructorTestClass");
+        System.out.println(builder.getDelegate().getMember("testSuper"));
+        builder.addConstructor(new JConstructor(builder, "testSuper", new JType[0]));
+        builder.compileToFile(Path.of("./target/SimpleConstructorTestClass.class"));
         var clazz = builder.compileToClass();
         System.out.println(clazz);
+        var obj = clazz.getConstructor().newInstance();
+        System.out.println(obj);
+    }
+
+    private Context makeContext() {
+        return Context.newBuilder("js")
+                .allowAllAccess(true)
+                .allowHostAccess(HostAccess.ALL)
+                .allowHostClassLoading(true)
+                .allowHostClassLookup(className -> true)
+                .allowIO(true)
+                .allowExperimentalOptions(true)
+                .option("js.esm-eval-returns-exports", "true")
+                .option("js.shared-array-buffer", "true")
+                .option("js.foreign-object-prototype", "true")
+                .option("js.nashorn-compat", "true")
+                .option("js.ecmascript-version", "13")
+                .option("engine.WarnInterpreterOnly", "false")
+                .build();
+    }
+
+    public Value execJS(Context context, String name, String jsCode) {
+        try {
+            return context.eval(Source.newBuilder("js", jsCode, name).build());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
