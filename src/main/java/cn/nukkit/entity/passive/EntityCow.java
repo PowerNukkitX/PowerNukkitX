e751c8664bce934e7bee8c0bd06ccc435b0c4544
daoge <60499296+smartcmd@users.noreply.github.com>
2022-07-30 10:00:48 +0800

    Feature/zombie (#483)
    * init
    
    * 初步僵尸AI实现

--- src/main/java/cn/nukkit/entity/passive/EntityCow.java
+++ src/main/java/cn/nukkit/entity/passive/EntityCow.java
@@ -30,30 +30,7 @@ public class EntityCow extends EntityWalkingAnimal {
 
     public static final int NETWORK_ID = 11;
 
-    private final IBehaviorGroup behaviorGroup = new BehaviorGroup(
-            this.tickSpread,
-            Set.of(
-                    //用于刷新InLove状态的核心行为
-                    new Behavior(
-                            new InLoveExecutor(400),
-                            new AllMatchEvaluator(
-                                    new PassByTimeEvaluator<>(PlayerBreedingMemory.class,0,400),
-                                    new PassByTimeEvaluator<>(InLoveMemory.class,6000,Integer.MAX_VALUE,true)
-                            ),
-                            1,1
-                    )
-            ),
-            Set.of(
-                    new Behavior(new RandomRoamExecutor(0.5f, 12, 40, true,100,true,10), new PassByTimeEvaluator<>(AttackMemory.class,0,100), 4, 1),
-                    new Behavior(new EntityBreedingExecutor<>(EntityCow.class,16,100,0.5f), entity -> entity.getMemoryStorage().get(InLoveMemory.class).isInLove(),3,1),
-                    new Behavior(new MoveToTargetExecutor(NearestBeggingPlayerMemory.class, 0.3f), new MemoryCheckNotEmptyEvaluator(NearestBeggingPlayerMemory.class), 2, 1),
-                    new Behavior(new LookAtTargetExecutor(NearestPlayerMemory.class,100), new ProbabilityEvaluator(4,10), 1, 1,100),
-                    new Behavior(new RandomRoamExecutor(0.15f, 12, 100, false,-1,true,10), (entity -> true), 1, 1)
-            ),
-            Set.of(new NearestBeggingPlayerSensor(8, 0), new NearestPlayerSensor(8, 0,20)),
-            Set.of(new WalkController(), new LookController(true, true)),
-            new SimpleFlatAStarRouteFinder(new WalkingPosEvaluator(), this)
-    );
+    private IBehaviorGroup behaviorGroup;
 
     public EntityCow(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
@@ -61,6 +38,32 @@ public class EntityCow extends EntityWalkingAnimal {
 
     @Override
     public IBehaviorGroup getBehaviorGroup() {
+        if (behaviorGroup == null){
+            behaviorGroup = new BehaviorGroup(
+                    this.tickSpread,
+                    Set.of(
+                            //用于刷新InLove状态的核心行为
+                            new Behavior(
+                                    new InLoveExecutor(400),
+                                    new AllMatchEvaluator(
+                                            new PassByTimeEvaluator<>(PlayerBreedingMemory.class,0,400),
+                                            new PassByTimeEvaluator<>(InLoveMemory.class,6000,Integer.MAX_VALUE,true)
+                                    ),
+                                    1,1
+                            )
+                    ),
+                    Set.of(
+                            new Behavior(new RandomRoamExecutor(0.5f, 12, 40, true,100,true,10), new PassByTimeEvaluator<>(AttackMemory.class,0,100), 4, 1),
+                            new Behavior(new EntityBreedingExecutor<>(EntityCow.class,16,100,0.5f), entity -> entity.getMemoryStorage().get(InLoveMemory.class).isInLove(),3,1),
+                            new Behavior(new MoveToTargetExecutor(NearestBeggingPlayerMemory.class, 0.3f), new MemoryCheckNotEmptyEvaluator(NearestBeggingPlayerMemory.class), 2, 1),
+                            new Behavior(new LookAtTargetExecutor(NearestPlayerMemory.class,100), new ProbabilityEvaluator(4,10), 1, 1,100),
+                            new Behavior(new RandomRoamExecutor(0.15f, 12, 100, false,-1,true,10), (entity -> true), 1, 1)
+                    ),
+                    Set.of(new NearestBeggingPlayerSensor(8, 0), new NearestPlayerSensor(8, 0,20)),
+                    Set.of(new WalkController(), new LookController(true, true)),
+                    new SimpleFlatAStarRouteFinder(new WalkingPosEvaluator(), this)
+            );
+        }
         return behaviorGroup;
     }
 
