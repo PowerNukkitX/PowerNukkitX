da8c8d159068b7f7ff9294c8cc9cb2f35e37bdab
超神的冰凉 <2801621420@qq.com>
2022-08-02 16:14:57 +0800

    尝试修复maven*3 +b

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -3551,7 +3551,8 @@ public class Level implements ChunkManager, Metadatable {
     }
 
     public synchronized boolean unloadChunk(int x, int z, boolean safe, boolean trySave) {
-        if (safe && (this.isChunkInUse(x, z) || getServer().getTickingAreaManager().getTickingAreaByChunk(this.getName(), new TickingArea.ChunkPos(x, z)) != null)) {
+        var tickingAreaManager = getServer().getTickingAreaManager();
+        if (safe && (this.isChunkInUse(x, z) || (tickingAreaManager != null && tickingAreaManager.getTickingAreaByChunk(this.getName(), new TickingArea.ChunkPos(x, z)) != null))) {
             return false;
         }
 
