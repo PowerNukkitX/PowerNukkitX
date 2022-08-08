39aadc9d65692afdd7f8b262659b99b61d6419b1
超神的冰凉 <2801621420@qq.com>
2022-08-08 18:42:02 +0800

    修复无法种植大型云杉木的问题

--- src/main/java/cn/nukkit/level/generator/object/tree/ObjectBigSpruceTree.java
+++ src/main/java/cn/nukkit/level/generator/object/tree/ObjectBigSpruceTree.java
@@ -1,5 +1,7 @@
 package cn.nukkit.level.generator.object.tree;
 
+import cn.nukkit.api.PowerNukkitXOnly;
+import cn.nukkit.api.Since;
 import cn.nukkit.block.Block;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.math.NukkitRandom;
@@ -16,9 +18,17 @@ public class ObjectBigSpruceTree extends ObjectSpruceTree {
         this.baseLeafRadius = baseLeafRadius;
     }
 
+    @PowerNukkitXOnly
+    @Since("1.19.10-r2")
+    public void setRandomTreeHeight(NukkitRandom random) {
+        this.treeHeight = random.nextBoundedInt(15) + 20;
+    }
+
     @Override
     public void placeObject(ChunkManager level, int x, int y, int z, NukkitRandom random) {
-        this.treeHeight = random.nextBoundedInt(15) + 20;
+        if (this.treeHeight == 0) {
+            this.setRandomTreeHeight(random);
+        }
 
         int topSize = this.treeHeight - (int) (this.treeHeight * leafStartHeightMultiplier);
         int lRadius = baseLeafRadius + random.nextBoundedInt(2);
@@ -42,6 +52,41 @@ public class ObjectBigSpruceTree extends ObjectSpruceTree {
                         level.setBlockAt(x + xx, y + yy, z + zz, this.getTrunkBlock(), this.getType());
                     }
                 }
+            }
+        }
+    }
+
+    @Override
+    public void placeLeaves(ChunkManager level, int topSize, int lRadius, int x, int y, int z, NukkitRandom random)   {
+        int radius = random.nextBoundedInt(2);
+        int maxR = 1;
+        int minR = 0;
+
+        for (int yy = 0; yy <= topSize; ++yy) {
+            int yyy = y + this.treeHeight - yy;
+
+            for (int xx = x - radius; xx <= x + radius; ++xx) {
+                int xOff = Math.abs(xx - x);
+                for (int zz = z - radius; zz <= z + radius; ++zz) {
+                    int zOff = Math.abs(zz - z);
+                    if (xOff == radius && zOff == radius && radius > 0) {
+                        continue;
+                    }
+
+                    if (!Block.isSolid(level.getBlockIdAt(xx, yyy, zz))) {
+                        level.setBlockAt(xx, yyy, zz, this.getLeafBlock(), this.getType());
+                    }
+                }
+            }
+
+            if (radius >= maxR) {
+                radius = minR;
+                minR = 1;
+                if (++maxR > lRadius) {
+                    maxR = lRadius;
+                }
+            } else {
+                ++radius;
             }
         }
     }
