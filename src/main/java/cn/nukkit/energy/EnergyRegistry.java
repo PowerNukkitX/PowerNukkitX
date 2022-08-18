0f2df5ce07363e00566692fb68e8932f4ba86304
超神的冰凉 <2801621420@qq.com>
2022-08-18 16:35:18 +0800

    注册能源时返回是否成功

--- src/main/java/cn/nukkit/energy/EnergyRegistry.java
+++ src/main/java/cn/nukkit/energy/EnergyRegistry.java
@@ -1,13 +1,24 @@
 package cn.nukkit.energy;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public final class EnergyRegistry {
     public static final Map<String, EnergyType> registeredEnergyTypes = new HashMap<>();
 
-    public static void registerEnergyType(EnergyType energyType) {
-        registeredEnergyTypes.put(energyType.getName(), energyType);
+    /**
+     * Register a new energy type.
+     * @param energyType The energy type to register.
+     * @return If the energy type was registered successfully.
+     */
+    public static boolean registerEnergyType(@NotNull EnergyType energyType) {
+        if (!registeredEnergyTypes.containsKey(energyType.getName())) {
+            registeredEnergyTypes.put(energyType.getName(), energyType);
+            return true;
+        }
+        return false;
     }
 
     public static EnergyType getEnergyType(String name) {
