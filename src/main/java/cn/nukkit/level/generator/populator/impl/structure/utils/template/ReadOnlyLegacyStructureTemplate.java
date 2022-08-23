c3d51526ceba27fe44c572ee867cfb8e6c449391
刀哥 <3523206925@qq.com>
2022-08-24 03:05:13 +0800

    fix loot bug

--- src/main/java/cn/nukkit/level/generator/populator/impl/structure/utils/template/ReadOnlyLegacyStructureTemplate.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/structure/utils/template/ReadOnlyLegacyStructureTemplate.java
@@ -183,15 +183,15 @@ public class ReadOnlyLegacyStructureTemplate extends AbstractLegacyStructureTemp
                 continue;
             }
 
-            BlockVector3 vec = blockInfo.pos.add(0, position.y);
+            BlockVector3 vec = blockInfo.pos.add(position);
 
             if (entry.getId() != BlockID.STRUCTURE_BLOCK) {
-                int BlockIDLayer0 = chunk.getBlockId(vec.getX(), vec.getY(), vec.getZ(), 0);
-                int BlockIDLayer1 = chunk.getBlockId(vec.getX(), vec.getY(), vec.getZ(), 1);
+                int BlockIDLayer0 = chunk.getBlockId(vec.getX() & 0xf, vec.getY(), vec.getZ() & 0xf, 0);
+                int BlockIDLayer1 = chunk.getBlockId(vec.getX() & 0xf, vec.getY(), vec.getZ() & 0xf, 1);
                 boolean shouldFillWater = BlockIDLayer0 == BlockID.FLOWING_WATER || BlockIDLayer0 == BlockID.STILL_WATER ||
                         BlockIDLayer1 == BlockID.FLOWING_WATER || BlockIDLayer1 == BlockID.STILL_WATER;
-                chunk.setBlockAtLayer(vec.getX(), vec.getY(), vec.getZ(), 0, entry.getId(), entry.getMeta());
-                if (shouldFillWater) chunk.setBlockAtLayer(vec.getX(), vec.getY(), vec.getZ(), 1, BlockID.STILL_WATER);
+                chunk.setBlockAtLayer(vec.getX() & 0xf, vec.getY(), vec.getZ() & 0xf, 0, entry.getId(), entry.getMeta());
+                if (shouldFillWater) chunk.setBlockAtLayer(vec.getX() & 0xf, vec.getY(), vec.getZ() & 0xf, 1, BlockID.STILL_WATER);
             } else if (!isIgnoreAir) {
                 chunk.setBlock(vec.getX() & 0xf, vec.getY(), vec.getZ() & 0xf, Block.AIR);
             }
--- src/main/java/cn/nukkit/level/generator/task/BlockActorSpawnTask.java
+++ src/main/java/cn/nukkit/level/generator/task/BlockActorSpawnTask.java
@@ -4,6 +4,7 @@ import cn.nukkit.api.PowerNukkitXOnly;
 import cn.nukkit.api.Since;
 import cn.nukkit.blockentity.BlockEntity;
 import cn.nukkit.level.Level;
+import cn.nukkit.math.Vector3;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.scheduler.Task;
 
