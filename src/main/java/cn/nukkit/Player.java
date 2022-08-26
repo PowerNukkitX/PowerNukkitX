a7406158a62a677769dcd25499f17fe49895b0d2
超神的冰凉 <2801621420@qq.com>
2022-08-26 09:20:49 +0800

    尝试修复反矿透导致的NPE

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3011,9 +3011,13 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                                             var tmpX = target.getFloorX() + each.getXOffset();
                                             var tmpY = target.getFloorY() + each.getYOffset();
                                             var tmpZ = target.getFloorZ() + each.getZOffset();
-                                            tmpVec = new Vector3WithRuntimeId(tmpX, tmpY, tmpZ, getLevel().getBlockRuntimeId(tmpX, tmpY, tmpZ, 0), getLevel().getBlockRuntimeId(tmpX, tmpY, tmpZ, 1));
-                                            if (getLevel().getRawFakeOreToPutRuntimeIdMap().containsKey(tmpVec.getRuntimeIdLayer0())) {
-                                                vecList.add(tmpVec);
+                                            try {
+                                                tmpVec = new Vector3WithRuntimeId(tmpX, tmpY, tmpZ, getLevel().getBlockRuntimeId(tmpX, tmpY, tmpZ, 0), getLevel().getBlockRuntimeId(tmpX, tmpY, tmpZ, 1));
+                                                if (getLevel().getRawFakeOreToPutRuntimeIdMap().containsKey(tmpVec.getRuntimeIdLayer0())) {
+                                                    vecList.add(tmpVec);
+                                                }
+                                            } catch (Exception ignore) {
+
                                             }
                                         }
                                         this.getLevel().sendBlocks(new Player[]{this}, vecList.toArray(Vector3[]::new), UpdateBlockPacket.FLAG_ALL);
