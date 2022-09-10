f1fa90fcfa25f5fae38d9f6f0dde886b708e404d
刀哥 <3523206925@qq.com>
2022-09-10 15:48:38 +0800

    修复PlayerDeathEvent的类型转换报错

--- src/main/java/cn/nukkit/event/player/PlayerDeathEvent.java
+++ src/main/java/cn/nukkit/event/player/PlayerDeathEvent.java
@@ -1,11 +1,15 @@
 package cn.nukkit.event.player;
 
 import cn.nukkit.Player;
+import cn.nukkit.api.DeprecationDetails;
+import cn.nukkit.api.PowerNukkitXOnly;
+import cn.nukkit.api.Since;
 import cn.nukkit.event.Cancellable;
 import cn.nukkit.event.HandlerList;
 import cn.nukkit.event.entity.EntityDeathEvent;
 import cn.nukkit.item.Item;
 import cn.nukkit.lang.TextContainer;
+import cn.nukkit.lang.TranslationContainer;
 
 public class PlayerDeathEvent extends EntityDeathEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
@@ -38,10 +42,28 @@ public class PlayerDeathEvent extends EntityDeathEvent implements Cancellable {
         return deathMessage;
     }
 
+    @PowerNukkitXOnly
+    @Since("1.19.21-r4")
+    public TranslationContainer getTranslationDeathMessage() {
+        return this.deathMessage instanceof TranslationContainer ?
+                (TranslationContainer) this.deathMessage :
+                new TranslationContainer(this.deathMessage.getText());
+    }
+
+    @PowerNukkitXOnly
+    @Since("1.19.21-r4")
+    public void setDeathMessage(TranslationContainer deathMessage) {
+        this.deathMessage = deathMessage;
+    }
+
+    @Deprecated
+    @DeprecationDetails(since = "1.19.21-r4", reason = "moved to TranslationContainer", replaceWith = "setDeathMessage(TranslationContainer deathMessage)")
     public void setDeathMessage(TextContainer deathMessage) {
         this.deathMessage = deathMessage;
     }
 
+    @Deprecated
+    @DeprecationDetails(since = "1.19.21-r4", reason = "moved to TranslationContainer")
     public void setDeathMessage(String deathMessage) {
         this.deathMessage = new TextContainer(deathMessage);
     }
