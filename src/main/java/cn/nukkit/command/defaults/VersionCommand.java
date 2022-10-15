5af959254e3b780d805b9d870410195a1b5a4af9
daoge <60499296+smartcmd@users.noreply.github.com>
2022-10-15 17:16:41 +0800

    Fix version check (#718)
    Fix version check

--- src/main/java/cn/nukkit/command/defaults/VersionCommand.java
+++ src/main/java/cn/nukkit/command/defaults/VersionCommand.java
@@ -19,8 +19,13 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -45,21 +50,45 @@ public class VersionCommand extends VanillaCommand {
                         String localCommitInfo = Server.getInstance().getGitCommit();
                         localCommitInfo = localCommitInfo.substring(4);
                         int versionMissed = -1;
+                        query.sender.sendMessage("####################");
+                        var matched = false;
                         for (int i = 0, len = cores.size(); i < len; i++) {
                             var entry = cores.get(i).getAsJsonObject();
                             var remoteCommitInfo = entry.get("name").getAsString().split("-")[1];
-                            if (remoteCommitInfo.equals(localCommitInfo)) {
+                            matched = remoteCommitInfo.equals(localCommitInfo);
+
+                            var infoBuilder = new StringBuilder();
+                            infoBuilder.append("[").append(i + 1).append("] ");
+                            if (i == 0) infoBuilder.append("Name: §e").append(entry.get("name").getAsString()).append("§f, Time: §e").append(utcToLocal(entry.get("lastModified").getAsString())).append(" §e(LATEST)").append(matched ? " §b(CURRENT)" : "");
+                            else if (matched) infoBuilder.append("Name: §b").append(entry.get("name").getAsString()).append("§f, Time: §b").append(utcToLocal(entry.get("lastModified").getAsString())).append(" §b(CURRENT)");
+                            else infoBuilder.append("Name: §a").append(entry.get("name").getAsString()).append("§f, Time: §a").append(utcToLocal(entry.get("lastModified").getAsString()));
+                            //打印相关信息
+                            query.sender.sendMessage(infoBuilder.toString());
+
+                            if (matched) {
                                 versionMissed = i;
                                 break;
                             }
                         }
+                        //too old
+                        if (!matched) {
+                            query.sender.sendMessage("....................");
+                            var localInfoBuilder = new StringBuilder();
+                            localInfoBuilder.append("[???] ").append("Name: §c").append(localCommitInfo).append("§f, Time: §c???").append(" §c(CURRENT)");
+                            query.sender.sendMessage(localInfoBuilder.toString());
+                        }
+                        query.sender.sendMessage("####################");
                         if (versionMissed == 0)
-                            query.sender.sendMessage(TextFormat.GREEN + "You are using the latest version of PowerNukkitX!");
+                            query.sender.sendMessage("§aYou are using the latest version of PowerNukkitX!");
                         else if (versionMissed > 0) {
-                            query.sender.sendMessage(TextFormat.YELLOW + "You are using an outdated version of PowerNukkitX!, " + versionMissed + " versions behind!");
-                            query.sender.sendMessage(TextFormat.YELLOW + "The latest version is " + cores.get(0).getAsJsonObject().get("name").getAsString());
+                            query.sender.sendMessage("§cYou are using an outdated version of PowerNukkitX!, §f" + versionMissed + " §aversions behind!");
                         } else {
-                            query.sender.sendMessage(TextFormat.RED + "Note that you may be using a custom build!");
+                            query.sender.sendMessage("§cCouldn't match your version number: §f" + localCommitInfo + "§c, maybe you are using a custom build or your version is too old!");
+                        }
+                        if (versionMissed != 0) {
+                            query.sender.sendMessage("Download the latest version at §a" + cores.get(0).getAsJsonObject().get("url").getAsString());
+                            query.sender.sendMessage("You can enter command §a \"pnx server update\"§f to automatically update your server if you are using PNX-CLI");
+                            query.sender.sendMessage("Download PNX-CLI at: §a" + "https://github.com/PowerNukkitX/PNX-CLI/releases");
                         }
                         queryQueue.remove(query);
                     }
@@ -98,7 +127,7 @@ public class VersionCommand extends VanillaCommand {
                     sender.getServer().getVersion(),
                     String.valueOf(ProtocolInfo.CURRENT_PROTOCOL)));
             if (sender.isOp()) {
-                sender.sendMessage("Checking version, please wait...");
+                sender.sendMessage(TextFormat.ITALIC + "Retrieving version information from remote server...");
                 queryQueue.add(new Query(sender, listVersion()));
             }
         } else {
@@ -155,7 +184,7 @@ public class VersionCommand extends VanillaCommand {
                 }
             }
             var client = HttpClient.newHttpClient();
-            var builder = HttpRequest.newBuilder(URI.create("https://api.powernukkitx.cn/get-core-manifest")).GET();
+            var builder = HttpRequest.newBuilder(URI.create("https://api.powernukkitx.cn/get-core-manifest?max=100")).GET();
             builder.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36");
             var request = builder.build();
             try {
@@ -172,6 +201,18 @@ public class VersionCommand extends VanillaCommand {
         });
     }
 
-    private record Query(CommandSender sender, CompletableFuture<JsonArray> jsonArrayFuture) {
+    protected String utcToLocal(String utcTime){
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date utcDate = null;
+        try {
+            utcDate = sdf.parse(utcTime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return df.format(utcDate);
     }
+
+    private record Query(CommandSender sender, CompletableFuture<JsonArray> jsonArrayFuture) { }
 }
