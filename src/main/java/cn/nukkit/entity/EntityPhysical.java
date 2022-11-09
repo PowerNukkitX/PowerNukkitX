701ef13a391c5091a26f61c4fe59a7d6373375dd
SoHugePenguin <2298566583@qq.com>
2022-11-09 20:15:04 +0800

    修复生物浮空漏洞

--- src/main/java/cn/nukkit/entity/EntityPhysical.java
+++ src/main/java/cn/nukkit/entity/EntityPhysical.java
@@ -78,8 +78,10 @@ public abstract class EntityPhysical extends EntityCreature implements EntityAsy
         }
 
         //修复生物在天上浮空而不能识别是否在地面的漏洞
-        if (this.motionY == 0d && this.motionX == 0d && this.motionZ == 0d && this.onGround && this.getLevel().getBlock(new Vector3(this.x, this.y - 0.001, this.z)).getId() == 0) {
-            this.onGround = false;
+        if (this.onGround) {
+            AxisAlignedBB bb = this.boundingBox.clone();
+            bb.setMinY(bb.getMinY() - 0.5);
+            this.onGround = this.level.getCollisionBlocks(bb).length > 0;
         }
         return super.onUpdate(currentTick);
     }
