af8aea1d983763699af465f1e3189efbbaaf04c2
CivicXFB <61540722+CivicXFB@users.noreply.github.com>
2022-12-01 20:40:41 +0800

    Add files via upload

--- src/main/java/cn/nukkit/entity/passive/EntityCat.java
+++ src/main/java/cn/nukkit/entity/passive/EntityCat.java
@@ -1,9 +1,30 @@
 package cn.nukkit.entity.passive;
 
+import cn.nukkit.Player;
 import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.ai.behavior.Behavior;
+import cn.nukkit.entity.ai.behaviorgroup.BehaviorGroup;
+import cn.nukkit.entity.ai.behaviorgroup.IBehaviorGroup;
+import cn.nukkit.entity.ai.controller.LookController;
+import cn.nukkit.entity.ai.controller.WalkController;
+import cn.nukkit.entity.ai.evaluator.AllMatchEvaluator;
+import cn.nukkit.entity.ai.evaluator.MemoryCheckNotEmptyEvaluator;
+import cn.nukkit.entity.ai.evaluator.PassByTimeEvaluator;
+import cn.nukkit.entity.ai.evaluator.ProbabilityEvaluator;
+import cn.nukkit.entity.ai.executor.*;
+import cn.nukkit.entity.ai.memory.CoreMemoryTypes;
+import cn.nukkit.entity.ai.route.finder.impl.SimpleFlatAStarRouteFinder;
+import cn.nukkit.entity.ai.route.posevaluator.WalkingPosEvaluator;
+import cn.nukkit.entity.ai.sensor.NearestFeedingPlayerSensor;
+import cn.nukkit.entity.ai.sensor.NearestPlayerSensor;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.utils.DyeColor;
+
+import java.util.Set;
 
 public class EntityCat extends EntityWalkingAnimal {
 
@@ -13,7 +34,8 @@ public class EntityCat extends EntityWalkingAnimal {
         super(chunk, nbt);
     }
 
-
+    private DyeColor collarColor = DyeColor.RED;//驯服后项圈为红色
+    private IBehaviorGroup behaviorGroup;
     @Override
     public void updateMovement() {
         //猫猫流线运动怎么可能会摔落造成伤害呢~
@@ -25,7 +47,36 @@ public class EntityCat extends EntityWalkingAnimal {
     public int getNetworkId() {
         return NETWORK_ID;
     }
-
+    @Override
+    public IBehaviorGroup getBehaviorGroup() {
+        if (behaviorGroup == null) {
+            behaviorGroup = new BehaviorGroup(
+                    this.tickSpread,
+                    Set.of(
+                            //用于刷新InLove状态的核心行为
+                            new Behavior(
+                                    new InLoveExecutor(400),
+                                    new AllMatchEvaluator(
+                                            new PassByTimeEvaluator(CoreMemoryTypes.LAST_BE_FED_TIME, 0, 400),
+                                            new PassByTimeEvaluator(CoreMemoryTypes.LAST_IN_LOVE_TIME, 6000, Integer.MAX_VALUE)
+                                    ),
+                                    1, 1
+                            )
+                    ),
+                    Set.of(
+                            new Behavior(new RandomRoamExecutor(0.25f, 12, 40, true, 100, true, 10), new PassByTimeEvaluator(CoreMemoryTypes.LAST_BE_ATTACKED_TIME, 0, 100), 4, 1),
+                            new Behavior(new EntityBreedingExecutor<>(EntityCat.class, 16, 100, 0.5f), entity -> entity.getMemoryStorage().get(CoreMemoryTypes.IS_IN_LOVE), 3, 1),
+                            new Behavior(new MoveToTargetExecutor(CoreMemoryTypes.NEAREST_FEEDING_PLAYER, 0.25f, true), new MemoryCheckNotEmptyEvaluator(CoreMemoryTypes.NEAREST_FEEDING_PLAYER), 2, 1),
+                            new Behavior(new LookAtTargetExecutor(CoreMemoryTypes.NEAREST_PLAYER, 100), new ProbabilityEvaluator(4, 10), 1, 1, 100),
+                            new Behavior(new RandomRoamExecutor(0.1f, 12, 100, false, -1, true, 10), (entity -> true), 1, 1)
+                    ),
+                    Set.of(new NearestFeedingPlayerSensor(8, 0), new NearestPlayerSensor(8, 0, 20)),
+                    Set.of(new WalkController(), new LookController(true, true)),
+                    new SimpleFlatAStarRouteFinder(new WalkingPosEvaluator(), this)
+            );
+        }
+        return behaviorGroup;
+    }
     @Override
     public float getWidth() {
         if (this.isBaby()) {
