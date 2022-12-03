441ba9600528b37873f42f4d63e347d5dc267109
CivicXFB <61540722+CivicXFB@users.noreply.github.com>
2022-12-03 20:26:37 +0800

    调整动作优先级

--- src/main/java/cn/nukkit/entity/ai/executor/CatLookFeedingPlayerExecutor.java
+++ src/main/java/cn/nukkit/entity/ai/executor/CatLookFeedingPlayerExecutor.java
@@ -5,7 +5,6 @@ import cn.nukkit.api.Since;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.EntityIntelligent;
 import cn.nukkit.entity.ai.memory.CoreMemoryTypes;
-import cn.nukkit.math.Vector3;
 
 /**
  * 猫看向携带食物的玩家.
@@ -15,7 +14,6 @@ import cn.nukkit.math.Vector3;
 @PowerNukkitXOnly
 @Since("1.19.30-r1")
 public class CatLookFeedingPlayerExecutor implements EntityControl, IBehaviorExecutor {
-    protected Vector3 oldTarget;
     @Override
     public boolean execute(EntityIntelligent entity) {
         if (!entity.isEnablePitch()) entity.setEnablePitch(true);
@@ -30,32 +28,18 @@ public class CatLookFeedingPlayerExecutor implements EntityControl, IBehaviorExe
     @Override
     public void onInterrupt(EntityIntelligent entity) {
         entity.setEnablePitch(false);
-        //目标丢失
-        removeRouteTarget(entity);
-        removeLookTarget(entity);
-        //重置速度
-        entity.setMovementSpeed(0.25f);
-        entity.setEnablePitch(false);
         if (entity.getMemoryStorage().isEmpty(CoreMemoryTypes.NEAREST_FEEDING_PLAYER)) {
             entity.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_INTERESTED, false);
         }
         removeLookTarget(entity);
-        oldTarget = null;
     }
 
     @Override
     public void onStop(EntityIntelligent entity) {
         entity.setEnablePitch(false);
-        //目标丢失
-        removeRouteTarget(entity);
-        removeLookTarget(entity);
-        //重置速度
-        entity.setMovementSpeed(0.25f);
-        entity.setEnablePitch(false);
         if (entity.getMemoryStorage().isEmpty(CoreMemoryTypes.NEAREST_FEEDING_PLAYER)) {
             entity.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_INTERESTED, false);
         }
         removeLookTarget(entity);
-        oldTarget = null;
     }
 }
