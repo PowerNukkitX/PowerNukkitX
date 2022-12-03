23ba764a83f315b28c9ff3a8c44aee38e06638ec
CivicXFB <61540722+CivicXFB@users.noreply.github.com>
2022-12-03 18:03:41 +0800

    偷腥的猫

--- src/main/java/cn/nukkit/entity/ai/executor/CatLookFeedingPlayerExecutor.java
+++ src/main/java/cn/nukkit/entity/ai/executor/CatLookFeedingPlayerExecutor.java
@@ -5,6 +5,7 @@ import cn.nukkit.api.Since;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.EntityIntelligent;
 import cn.nukkit.entity.ai.memory.CoreMemoryTypes;
+import cn.nukkit.math.Vector3;
 
 /**
  * 猫看向携带食物的玩家.
@@ -14,6 +15,7 @@ import cn.nukkit.entity.ai.memory.CoreMemoryTypes;
 @PowerNukkitXOnly
 @Since("1.19.30-r1")
 public class CatLookFeedingPlayerExecutor implements EntityControl, IBehaviorExecutor {
+    protected Vector3 oldTarget;
     @Override
     public boolean execute(EntityIntelligent entity) {
         if (!entity.isEnablePitch()) entity.setEnablePitch(true);
@@ -28,18 +30,32 @@ public class CatLookFeedingPlayerExecutor implements EntityControl, IBehaviorExe
     @Override
     public void onInterrupt(EntityIntelligent entity) {
         entity.setEnablePitch(false);
-        if (entity.getMemoryStorage().isEmpty(CoreMemoryTypes.NEAREST_FEEDING_PLAYER)) {
-            entity.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_INTERESTED, false);
-        }
+        //目标丢失
+        removeRouteTarget(entity);
         removeLookTarget(entity);
-    }
-
-    @Override
-    public void onStop(EntityIntelligent entity) {
+        //重置速度
+        entity.setMovementSpeed(0.25f);
         entity.setEnablePitch(false);
         if (entity.getMemoryStorage().isEmpty(CoreMemoryTypes.NEAREST_FEEDING_PLAYER)) {
             entity.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_INTERESTED, false);
         }
         removeLookTarget(entity);
+        oldTarget = null;
+    }
+
+    @Override
+    public void onStop(EntityIntelligent entity) {
+        entity.setEnablePitch(false);
+        //目标丢失
+        removeRouteTarget(entity);
+        removeLookTarget(entity);
+        //重置速度
+        entity.setMovementSpeed(0.25f);
+        entity.setEnablePitch(false);
+        if (entity.getMemoryStorage().isEmpty(CoreMemoryTypes.NEAREST_FEEDING_PLAYER)) {
+            entity.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_INTERESTED, false);
+        }
+        removeLookTarget(entity);
+        oldTarget = null;
     }
 }
