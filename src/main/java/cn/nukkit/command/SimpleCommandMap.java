7b533f4ae6f95e3d09bb342ddc3314f8272a2306
daoge <60499296+smartcmd@users.noreply.github.com>
2022-12-11 15:57:13 +0800

    Add some new commands (#851)
    * Add "world" command
    
    * reload命令现在可以指定插件

--- src/main/java/cn/nukkit/command/SimpleCommandMap.java
+++ src/main/java/cn/nukkit/command/SimpleCommandMap.java
@@ -32,6 +32,7 @@ public class SimpleCommandMap implements CommandMap {
     private void setDefaultCommands() {
         this.register("nukkit",new ExecuteCommand("execute"));
         this.register("nukkit",new ExecuteCommandOld("executeold"));
+        this.register("nukkit",new WorldCommand("world"));
         this.register("nukkit",new TpsCommand("tps"));
         this.register("nukkit",new TickingAreaCommand("tickingarea"));
         this.register("nukkit",new TellrawCommand("tellraw"));
