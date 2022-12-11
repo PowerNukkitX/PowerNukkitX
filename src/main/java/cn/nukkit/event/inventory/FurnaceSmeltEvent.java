53354b1c3c54a9c4ec651f36859044c138ca94b8
Superice666 <51087818+Superice666@users.noreply.github.com>
2022-12-11 11:00:11 +0800

    熔炉经验 (#841)
    * 基本完成熔炉经验
    
    * 添加熔炉配方经验表和计算公式

--- src/main/java/cn/nukkit/event/inventory/FurnaceSmeltEvent.java
+++ src/main/java/cn/nukkit/event/inventory/FurnaceSmeltEvent.java
@@ -1,5 +1,7 @@
 package cn.nukkit.event.inventory;
 
+import cn.nukkit.api.PowerNukkitXOnly;
+import cn.nukkit.api.Since;
 import cn.nukkit.blockentity.BlockEntityFurnace;
 import cn.nukkit.event.Cancellable;
 import cn.nukkit.event.HandlerList;
@@ -20,13 +22,17 @@ public class FurnaceSmeltEvent extends BlockEvent implements Cancellable {
     private final BlockEntityFurnace furnace;
     private final Item source;
     private Item result;
+    @Since("1.19.50-r3")
+    @PowerNukkitXOnly
+    private float xp;
 
-    public FurnaceSmeltEvent(BlockEntityFurnace furnace, Item source, Item result) {
+    public FurnaceSmeltEvent(BlockEntityFurnace furnace, Item source, Item result, float xp) {
         super(furnace.getBlock());
         this.source = source.clone();
         this.source.setCount(1);
         this.result = result;
         this.furnace = furnace;
+        this.xp = xp;
     }
 
     public BlockEntityFurnace getFurnace() {
@@ -43,5 +49,13 @@ public class FurnaceSmeltEvent extends BlockEvent implements Cancellable {
 
     public void setResult(Item result) {
         this.result = result;
+    }
+
+    public float getXp() {
+        return xp;
+    }
+
+    public void setXp(float xp) {
+        this.xp = xp;
     }
 }
