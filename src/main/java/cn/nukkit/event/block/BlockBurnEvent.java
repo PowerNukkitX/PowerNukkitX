fa4034ef991fff4e337aec52b9bcb46d002f23eb
CoolLoong <1542536763@qq.com>
2023-01-25 20:52:26 +0800

    revert 不明意义的更改

--- src/main/java/cn/nukkit/event/block/BlockBurnEvent.java
+++ src/main/java/cn/nukkit/event/block/BlockBurnEvent.java
@@ -11,12 +11,12 @@ public class BlockBurnEvent extends BlockEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
 
-    public static HandlerList getHandlers() {
-        return handlers;
-    }
-
     public BlockBurnEvent(Block block) {
         super(block);
+    }
+
+    public static HandlerList getHandlers() {
+        return handlers;
     }
 
 }
