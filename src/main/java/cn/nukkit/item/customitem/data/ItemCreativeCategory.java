26ea73f3be97f3e2a556652ef2d4ef3ffe4e4ad4
CoolLoong <1542536763@qq.com>
2023-01-30 10:34:31 +0800

    修改注释

--- src/main/java/cn/nukkit/item/customitem/data/ItemCreativeCategory.java
+++ src/main/java/cn/nukkit/item/customitem/data/ItemCreativeCategory.java
@@ -5,7 +5,7 @@ import cn.nukkit.api.Since;
 
 /**
  * 控制自定义物品在创造栏的大分类,例如建材栏,材料栏
- * <br>可选值:1 Unknown 2 nature 3 equipment 4 items
+ * <br>可选值:1 CONSTRUCTOR 2 NATURE 3 EQUIPMENT 4 ITEMS 5 NONE
  *
  * @return 自定义物品的在创造栏的大分类
  * @see <a href="https://wiki.bedrock.dev/documentation/creative-categories.html#list-of-creative-tabs">bedrock wiki</a>
