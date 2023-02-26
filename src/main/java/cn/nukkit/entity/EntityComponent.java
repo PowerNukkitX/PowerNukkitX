f21ff80701d1807b3d4f57fc33b3e30225e1e753
CoolLoong <1542536763@qq.com>
2023-02-26 12:08:00 +0800

    javadoc2

--- src/main/java/cn/nukkit/entity/EntityComponent.java
+++ src/main/java/cn/nukkit/entity/EntityComponent.java
@@ -5,8 +5,11 @@ import cn.nukkit.api.Since;
 import cn.nukkit.entity.ai.memory.IMemoryStorage;
 
 /**
- * 继承了此接口的接口为一个实体组件<p/>
+ * 继承了此接口的接口为一个实体组件<br>
  * 实体组件的实现使用default方法承载逻辑，相关值则使用记忆存储器存储
+ * <p>
+ * The interface that inherits this interface is an entity component<br>
+ * The implementation of the entity component uses the default method to carry the logic, and the related values are stored in memory
  */
 @PowerNukkitXOnly
 @Since("1.19.62-r2")
