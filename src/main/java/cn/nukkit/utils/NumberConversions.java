9f7f8e2bdc42445a044f5fe8bffb08f4e321c0dd
CoolLoong <1542536763@qq.com>
2023-03-04 10:05:05 +0800

    revert 移除NumberConversions

---
+++ src/main/java/cn/nukkit/utils/NumberConversions.java
@@ -0,0 +1,104 @@
+package cn.nukkit.utils;
+
+import cn.nukkit.api.PowerNukkitXOnly;
+import cn.nukkit.api.Since;
+
+/**
+ * Utils for casting number types to other number types
+ */
+@PowerNukkitXOnly
+@Since("1.19.63-r2")
+public final class NumberConversions {
+    private NumberConversions() {
+    }
+
+    public static int floor(double num) {
+        final int floor = (int) num;
+        return floor == num ? floor : floor - (int) (Double.doubleToRawLongBits(num) >>> 63);
+    }
+
+    public static int ceil(final double num) {
+        final int floor = (int) num;
+        return floor == num ? floor : floor + (int) (~Double.doubleToRawLongBits(num) >>> 63);
+    }
+
+    public static int round(double num) {
+        return floor(num + 0.5d);
+    }
+
+    public static double square(double num) {
+        return num * num;
+    }
+
+    public static int toInt(Object object) {
+        if (object instanceof Number) {
+            return ((Number) object).intValue();
+        }
+
+        try {
+            return Integer.parseInt(object.toString());
+        } catch (NumberFormatException | NullPointerException e) {
+        }
+        return 0;
+    }
+
+    public static float toFloat(Object object) {
+        if (object instanceof Number) {
+            return ((Number) object).floatValue();
+        }
+
+        try {
+            return Float.parseFloat(object.toString());
+        } catch (NumberFormatException | NullPointerException e) {
+        }
+        return 0;
+    }
+
+    public static double toDouble(Object object) {
+        if (object instanceof Number) {
+            return ((Number) object).doubleValue();
+        }
+
+        try {
+            return Double.parseDouble(object.toString());
+        } catch (NumberFormatException | NullPointerException e) {
+        }
+        return 0;
+    }
+
+    public static long toLong(Object object) {
+        if (object instanceof Number) {
+            return ((Number) object).longValue();
+        }
+
+        try {
+            return Long.parseLong(object.toString());
+        } catch (NumberFormatException | NullPointerException e) {
+        }
+        return 0;
+    }
+
+    public static short toShort(Object object) {
+        if (object instanceof Number) {
+            return ((Number) object).shortValue();
+        }
+
+        try {
+            return Short.parseShort(object.toString());
+        } catch (NumberFormatException | NullPointerException e) {
+        }
+        return 0;
+    }
+
+    public static byte toByte(Object object) {
+        if (object instanceof Number) {
+            return ((Number) object).byteValue();
+        }
+
+        try {
+            return Byte.parseByte(object.toString());
+        } catch (NumberFormatException | NullPointerException e) {
+        }
+        return 0;
+    }
+}
