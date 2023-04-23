57b0938dac61839322796c6ede3379c0f72a6afa
LT_Name <45508179+lt-name@users.noreply.github.com>
2023-04-23 21:24:25 +0800

    服务器加载完成后再开始监听控制台输入命令 (#1141)

--- src/main/java/cn/nukkit/Server.java
+++ src/main/java/cn/nukkit/Server.java
@@ -431,7 +431,7 @@ public class Server {
 
         this.console = new NukkitConsole(this);
         this.consoleThread = new ConsoleThread();
-        this.consoleThread.start();
+
         this.computeThreadPool = new ForkJoinPool(Math.min(0x7fff, Runtime.getRuntime().availableProcessors()), new ComputeThreadPoolThreadFactory(), null, false);
 
         this.playerDataSerializer = new DefaultPlayerDataSerializer(this);
@@ -950,6 +950,9 @@ public class Server {
             this.watchdog = new Watchdog(this, 60000);
             this.watchdog.start();
         }
+
+        //服务器加载完成后再开始监听控制台输入命令
+        this.consoleThread.start();
 
         System.runFinalization();
         this.start();
