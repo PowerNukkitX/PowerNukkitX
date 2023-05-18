d979967f185365eb16cdf8d39945494eae229cc7
CoolLoong <1542536763@qq.com>
2023-05-18 22:58:16 +0800

    change8

--- src/main/java/cn/nukkit/entity/passive/EntityHorse.java
+++ src/main/java/cn/nukkit/entity/passive/EntityHorse.java
@@ -318,7 +318,7 @@ public class EntityHorse extends EntityAnimal implements EntityWalkable, EntityV
         this.motionZ = 0;
         this.setMoveTarget(null);
         this.setLookTarget(null);
-        this.move(clientLoc.x - this.x, clientLoc.y - this.y, clientLoc.z - this.z);
+        this.move(clientLoc.x - this.x, (clientLoc.y - 0.5) - this.y, clientLoc.z - this.z);
         this.yaw = clientLoc.yaw;
         this.headYaw = clientLoc.headYaw;
     }
