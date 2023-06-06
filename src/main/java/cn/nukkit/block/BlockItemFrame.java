181ff18eb70d0233a176b6b5c588c9c0ef5d571c
CoolLoong <1542536763@qq.com>
2023-06-06 12:37:53 +0800

    change

--- src/main/java/cn/nukkit/block/BlockItemFrame.java
+++ src/main/java/cn/nukkit/block/BlockItemFrame.java
@@ -160,11 +160,7 @@ public class BlockItemFrame extends BlockTransparentMeta implements BlockEntityH
     public int onTouch(@Nullable Player player, Action action) {
         onUpdate(Level.BLOCK_UPDATE_TOUCH);
         if (player != null && action == Action.LEFT_CLICK_BLOCK) {
-            BlockEntityItemFrame entityItemFrame = getOrCreateBlockEntity();
-            if (entityItemFrame.getItem().getId() != Item.AIR) {
-                entityItemFrame.dropItem(player);
-                return 1;
-            } else return 0;
+            return getOrCreateBlockEntity().dropItem(player) ? 1 : 0;
         }
         return 0;
     }
