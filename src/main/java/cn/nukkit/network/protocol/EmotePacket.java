a9108fd18359a3ad3816f0e99aadaae817d98299
CoolLoong <1542536763@qq.com>
2023-06-08 13:47:18 +0800

    fix test

--- src/main/java/cn/nukkit/network/protocol/EmotePacket.java
+++ src/main/java/cn/nukkit/network/protocol/EmotePacket.java
@@ -9,7 +9,9 @@ public class EmotePacket extends DataPacket {
 
     @Since("1.3.0.0-PN")
     public long runtimeId;
+    @Since("1.20.0-r1")
     public String xuid;
+    @Since("1.20.0-r1")
     public String platformId;
     @Since("1.3.0.0-PN")
     public String emoteID;
