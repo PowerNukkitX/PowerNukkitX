6b33b7d07d3777212dc6e8b5c95c584825b49333
超神的冰凉 <2801621420@qq.com>
2023-06-10 21:42:34 +0800

    添加注解

--- src/main/java/cn/nukkit/event/block/SignWaxedEvent.java
+++ src/main/java/cn/nukkit/event/block/SignWaxedEvent.java
@@ -1,10 +1,15 @@
 package cn.nukkit.event.block;
 
 import cn.nukkit.Player;
+import cn.nukkit.api.PowerNukkitXOnly;
+import cn.nukkit.api.Since;
 import cn.nukkit.block.Block;
 import cn.nukkit.event.Cancellable;
 import cn.nukkit.event.HandlerList;
 
+
+@Since("1.20.0-r2")
+@PowerNukkitXOnly
 public class SignWaxedEvent extends BlockEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
