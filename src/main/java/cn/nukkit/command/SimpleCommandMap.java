11b47b17ab8e19a34e99f7c340d7134d1358cfaf
CoolLoong <1542536763@qq.com>
2023-06-10 19:39:59 +0800

    fix: the client crash when use command (#1218)
    * fix: Try to fix minecraft win10 edition's crash when trying to use command
    
    * fix: Try to fix minecraft win10 edition's crash when trying to use command x2
    
    * fix: Try to fix minecraft win10 edition's crash when trying to use command x3
    
    * fix: Try to fix minecraft win10 edition's crash when trying to use command x4
    
    * fix: Try to fix minecraft win10 edition's crash when trying to use command x5
    
    * fix: the client crash when use command
    
    * feat: add EDITOR command flag
    
    * add serversideonly
    
    * annotation
    
    ---------
    
    Co-authored-by: 刀哥 <3523206925@qq.com>

--- src/main/java/cn/nukkit/command/SimpleCommandMap.java
+++ src/main/java/cn/nukkit/command/SimpleCommandMap.java
@@ -255,10 +255,6 @@ public class SimpleCommandMap implements CommandMap {
         int start = 0;
 
         for (int i = 0; i < sb.length(); i++) {
-//            if (sb.charAt(i) == '\\') {
-//                sb.deleteCharAt(i);
-//                continue;
-//            }
             if ((sb.charAt(i) == '{' && curlyBraceCount >= 1) || (sb.charAt(i) == '{' && sb.charAt(i - 1) == ' ' && curlyBraceCount == 0)) {
                 curlyBraceCount++;
             } else if (sb.charAt(i) == '}' && curlyBraceCount > 0) {
