431adb8f9cb4a9dc6de01f63e4a44a7f0befd9aa
CoolLoong <1542536763@qq.com>
2023-06-11 19:29:43 +0800

    optimize BlockSuspiciousSand

--- src/main/java/cn/nukkit/block/BlockSuspiciousSand.java
+++ src/main/java/cn/nukkit/block/BlockSuspiciousSand.java
@@ -1,10 +1,14 @@
 package cn.nukkit.block;
 
+import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.PowerNukkitXOnly;
 import cn.nukkit.api.Since;
 import cn.nukkit.blockproperty.BlockProperties;
 import cn.nukkit.blockproperty.CommonBlockProperties;
 import cn.nukkit.blockproperty.IntBlockProperty;
+import cn.nukkit.entity.item.EntityFallingBlock;
+import cn.nukkit.item.Item;
+import cn.nukkit.nbt.tag.CompoundTag;
 
 @PowerNukkitXOnly
 @Since("1.20.0-r2")
@@ -36,5 +40,17 @@ public class BlockSuspiciousSand extends BlockFallableMeta {
     @Override
     public double getResistance() {
         return 1.25;
+    }
+
+    @PowerNukkitOnly
+    @Override
+    protected EntityFallingBlock createFallingEntity(CompoundTag customNbt) {
+        customNbt.putBoolean("BreakOnGround", true);
+        return super.createFallingEntity(customNbt);
+    }
+
+    @Override
+    public Item[] getDrops(Item item) {
+        return new Item[]{Item.AIR_ITEM};
     }
 }
