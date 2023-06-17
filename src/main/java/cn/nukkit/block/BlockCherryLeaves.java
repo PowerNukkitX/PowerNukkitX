d58cc6ea5bd634be9872c1f93dfa6e410ef2fffe
超神的冰凉 <2801621420@qq.com>
2023-06-17 20:01:32 +0800

    修复樱花树叶腐烂后报错问题

--- src/main/java/cn/nukkit/block/BlockCherryLeaves.java
+++ src/main/java/cn/nukkit/block/BlockCherryLeaves.java
@@ -4,6 +4,8 @@ import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.PowerNukkitXOnly;
 import cn.nukkit.api.Since;
 import cn.nukkit.blockproperty.BlockProperties;
+import cn.nukkit.blockproperty.value.WoodType;
+import cn.nukkit.item.Item;
 import org.jetbrains.annotations.NotNull;
 
 @PowerNukkitXOnly
@@ -20,6 +22,17 @@ public class BlockCherryLeaves extends BlockLeaves {
     @Override
     public int getId() {
         return CHERRY_LEAVES;
+    }
+
+    @Override
+    protected Item getSapling() {
+        return new BlockCherrySapling().toItem();
+    }
+
+    /*这里写木质类型为BIRCH只是为了获取凋落物时的概率正确，并不代表真的就是白桦木*/
+    @Override
+    public WoodType getType() {
+        return WoodType.BIRCH;
     }
 
     @Since("1.4.0.0-PN")
