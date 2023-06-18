f14761bb5a2a7b4d2bd48dcf2de4dda3c3b8ecdb
超神的冰凉 <2801621420@qq.com>
2023-06-18 16:24:18 +0800

    修复炼药锅流体显示错误

--- src/main/java/cn/nukkit/blockproperty/value/CauldronLiquid.java
+++ src/main/java/cn/nukkit/blockproperty/value/CauldronLiquid.java
@@ -1,12 +1,14 @@
 package cn.nukkit.blockproperty.value;
 
 import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.PowerNukkitXDifference;
 import cn.nukkit.api.Since;
 
 @PowerNukkitOnly
 @Since("1.5.0.0-PN")
+@PowerNukkitXDifference(since = "1.20.0-r2", info = "WATER is the last one now")
 public enum CauldronLiquid {
-    @Since("1.5.0.0-PN") @PowerNukkitOnly WATER,
     @Since("1.5.0.0-PN") @PowerNukkitOnly LAVA,
     @Since("1.5.0.0-PN") @PowerNukkitOnly POWDER_SNOW,
+    @Since("1.5.0.0-PN") @PowerNukkitOnly WATER,
 }
