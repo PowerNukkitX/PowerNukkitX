c950a6ad5cc8be56df2058e9defd1631b4ed9c85
LT_Name <572413378@qq.com>
2023-07-01 16:18:47 +0800

    修复EmotePacket兼容问题

--- src/main/java/cn/nukkit/network/protocol/EmotePacket.java
+++ src/main/java/cn/nukkit/network/protocol/EmotePacket.java
@@ -10,9 +10,9 @@ public class EmotePacket extends DataPacket {
     @Since("1.3.0.0-PN")
     public long runtimeId;
     @Since("1.20.0-r1")
-    public String xuid;
+    public String xuid = "";
     @Since("1.20.0-r1")
-    public String platformId;
+    public String platformId = "";
     @Since("1.3.0.0-PN")
     public String emoteID;
     @Since("1.3.0.0-PN")
