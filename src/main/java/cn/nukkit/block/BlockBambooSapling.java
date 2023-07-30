7173c32906fa3e1852f683281b8819ce248ba573
Arkadiy <83061703+MEFRREEX@users.noreply.github.com>
2023-07-31 01:06:06 +0300

    Fix

--- src/main/java/cn/nukkit/block/BlockBambooSapling.java
+++ src/main/java/cn/nukkit/block/BlockBambooSapling.java
@@ -125,7 +125,7 @@ public class BlockBambooSapling extends BlockFlowable {
             }
 
             if (success) {
-                if (player.getGamemode().isSurvival()) {
+                if (player != null && player.getGamemode().isSurvival()) {
                     item.count--;
                 }
 
