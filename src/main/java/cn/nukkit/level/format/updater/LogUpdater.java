c0ecbf05ec18124ed6a7428e79b51589445d6c1c
Verox001 <verox@cimeyclust.com>
2023-11-25 17:49:24 +0100

    Added ChunkUpdaters for newest version: LogUpdater and FacingToCardinalDirectionUpdater (#1423)
    Added ChunkUpdaters for newest version: LogUpdater and FacingToCardinalUpdater
    Credits for FacingToCardinalUpdater goes to LT_Name

---
+++ src/main/java/cn/nukkit/level/format/updater/LogUpdater.java
@@ -0,0 +1,36 @@
+package cn.nukkit.level.format.updater;
+
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
+import cn.nukkit.block.BlockID;
+import cn.nukkit.blockstate.BlockState;
+import cn.nukkit.level.format.ChunkSection;
+
+public class LogUpdater implements Updater {
+    private final ChunkSection section;
+
+    public LogUpdater(ChunkSection section) {
+        this.section = section;
+    }
+
+    @Since("1.4.0.0-PN")
+    @PowerNukkitOnly
+    @Override
+    public boolean update(int offsetX, int offsetY, int offsetZ, int x, int y, int z, BlockState state) {
+        int blockId = state.getBlockId();
+        if (blockId != BlockID.LOG) {
+            return false;
+        }
+
+        int newLogID = getNewID(state.getExactIntStorage());
+        return section.setBlock(x, y, z, newLogID);
+    }
+
+    private int getNewID(int fromData) {
+        return switch (fromData) {
+            case 4, 5 -> // old block states for acacia and dark oak logs
+                    162; // new block id for acacia log and dark oak log
+            default -> fromData;
+        };
+    }
+}
