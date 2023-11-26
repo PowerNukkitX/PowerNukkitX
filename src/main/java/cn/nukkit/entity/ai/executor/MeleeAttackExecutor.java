b7167819a32adb5cae1236e89ca7ea93c10950ab
0ddlyoko <11074473+0ddlyoko@users.noreply.github.com>
2023-11-26 17:50:36 +0100

    Fix max sense not taken in action
    When an entity attacks another one and this entity is far away from the attacker, the attacker should stop attacking the entity.
    This commit fixes the issue by checking if the entity is far away from the attacker.

--- src/main/java/cn/nukkit/entity/ai/executor/MeleeAttackExecutor.java
+++ src/main/java/cn/nukkit/entity/ai/executor/MeleeAttackExecutor.java
@@ -101,6 +101,7 @@ public class MeleeAttackExecutor implements EntityControl, IBehaviorExecutor {
 
         //some check
         if (!target.isAlive()) return false;
+        else if (entity.distanceSquared(target) > maxSenseRangeSquared) return false;
         else if (target instanceof Player player) {
             if (player.isCreative() || player.isSpectator() || !player.isOnline() || !entity.level.getName().equals(player.level.getName())) {
                 return false;
