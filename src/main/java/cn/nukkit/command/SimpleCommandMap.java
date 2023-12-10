6c93532bcb3fa3dd4db4737c1bba94a70cdf5b63
JkqzDev <espulefjuan13@gmail.com>
2023-12-10 12:55:27 -0300

    Fix error

--- src/main/java/cn/nukkit/command/SimpleCommandMap.java
+++ src/main/java/cn/nukkit/command/SimpleCommandMap.java
@@ -63,7 +63,7 @@ public class SimpleCommandMap implements CommandMap {
         this.register("nukkit", new DayLockCommand("daylock"));
         this.register("nukkit", new ClearCommand("clear"));
         this.register("nukkit", new CloneCommand("clone"));
-        this.register("nukkit", new VersionCommand("version"));
+        //this.register("nukkit", new VersionCommand("version"));
         this.register("nukkit", new PluginsCommand("plugins"));
         this.register("nukkit", new SeedCommand("seed"));
         this.register("nukkit", new HelpCommand("help"));
@@ -89,7 +89,7 @@ public class SimpleCommandMap implements CommandMap {
         this.register("nukkit", new GiveCommand("give"));
         this.register("nukkit", new EffectCommand("effect"));
         this.register("nukkit", new EnchantCommand("enchant"));
-        this.register("nukkit", new ParticleCommand("particle"));
+        //this.register("nukkit", new ParticleCommand("particle"));
         this.register("nukkit", new GamemodeCommand("gamemode"));
         this.register("nukkit", new GameruleCommand("gamerule"));
         this.register("nukkit", new KillCommand("kill"));
@@ -97,11 +97,11 @@ public class SimpleCommandMap implements CommandMap {
         this.register("nukkit", new SetWorldSpawnCommand("setworldspawn"));
         this.register("nukkit", new TeleportCommand("tp"));
         this.register("nukkit", new TimeCommand("time"));
-        this.register("nukkit", new TitleCommand("title"));
+        //this.register("nukkit", new TitleCommand("title"));
         this.register("nukkit", new ReloadCommand("reload"));
         this.register("nukkit", new WeatherCommand("weather"));
         this.register("nukkit", new XpCommand("xp"));
-        this.register("nukkit", new SetBlockCommand("setblock"));
+        //this.register("nukkit", new SetBlockCommand("setblock"));
 
         this.register("nukkit", new StatusCommand("status"));
         this.register("nukkit", new GarbageCollectorCommand("gc"));
