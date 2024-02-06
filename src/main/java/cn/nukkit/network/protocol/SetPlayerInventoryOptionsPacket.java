8955741bec2b55526cf780e2fff51e313b0326b1
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2024-02-06 12:24:48 +0300

    fix: access to fields

--- src/main/java/cn/nukkit/network/protocol/SetPlayerInventoryOptionsPacket.java
+++ src/main/java/cn/nukkit/network/protocol/SetPlayerInventoryOptionsPacket.java
@@ -3,7 +3,11 @@ package cn.nukkit.network.protocol;
 import cn.nukkit.network.protocol.types.inventory.InventoryLayout;
 import cn.nukkit.network.protocol.types.inventory.InventoryTabLeft;
 import cn.nukkit.network.protocol.types.inventory.InventoryTabRight;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class SetPlayerInventoryOptionsPacket extends DataPacket {
 
     public static final int NETWORK_ID = ProtocolInfo.SET_PLAYER_INVENTORY_OPTIONS_PACKET;
@@ -21,11 +25,11 @@ public class SetPlayerInventoryOptionsPacket extends DataPacket {
 
     @Override
     public void decode() {
-        this.leftTab = InventoryTabLeft.VALUES[this.getVarInt()];
-        this.rightTab = InventoryTabRight.VALUES[this.getVarInt()];
-        this.filtering = this.getBoolean();
-        this.layout = InventoryLayout.VALUES[this.getVarInt()];
-        this.craftingLayout = InventoryLayout.VALUES[this.getVarInt()];
+        this.setLeftTab(InventoryTabLeft.VALUES[this.getVarInt()]);
+        this.setRightTab(InventoryTabRight.VALUES[this.getVarInt()]);
+        this.setFiltering(this.getBoolean());
+        this.setLayout(InventoryLayout.VALUES[this.getVarInt()]);
+        this.setCraftingLayout(InventoryLayout.VALUES[this.getVarInt()]);
     }
 
     @Override
