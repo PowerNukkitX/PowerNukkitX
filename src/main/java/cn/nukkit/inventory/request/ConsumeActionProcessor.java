4a674c207635898d83f895cdde844f49044577ff
Abstract <xavier04.martinez@gmail.com>
2024-03-25 15:18:00 -0600

    refactor(ConsumeActionProcessor): code cleanup
    Make the code cleaner and more understandable for other people.

--- src/main/java/cn/nukkit/inventory/request/ConsumeActionProcessor.java
+++ src/main/java/cn/nukkit/inventory/request/ConsumeActionProcessor.java
@@ -1,6 +1,8 @@
 package cn.nukkit.inventory.request;
 
 import cn.nukkit.Player;
+import cn.nukkit.inventory.Inventory;
+import cn.nukkit.item.Item;
 import cn.nukkit.network.protocol.types.itemstack.ContainerSlotType;
 import cn.nukkit.network.protocol.types.itemstack.request.action.ConsumeAction;
 import cn.nukkit.network.protocol.types.itemstack.request.action.ItemStackRequestActionType;
@@ -23,28 +25,34 @@ public class ConsumeActionProcessor implements ItemStackRequestActionProcessor<C
     @Override
     public ActionResponse handle(ConsumeAction action, Player player, ItemStackRequestContext context) {
         // We have validated the recipe in CraftRecipeActionProcessor, so here we can believe the client directly
-        ContainerSlotType container = action.getSource().getContainer();
-        var sourceContainer = NetworkMapping.getInventory(player, container);
-        var sourceStackNetworkId = action.getSource().getStackNetworkId();
-        var slot = sourceContainer.fromNetworkSlot(action.getSource().getSlot());
         var count = action.getCount();
         if (count == 0) {
             log.warn("cannot consume 0 items!");
+
             return context.error();
         }
-        var item = sourceContainer.getItem(slot);
-        if (validateStackNetworkId(item.getNetId(), sourceStackNetworkId)) {
+
+        Inventory sourceContainer = NetworkMapping.getInventory(player, action.getSource().getContainer());
+        int slot = sourceContainer.fromNetworkSlot(action.getSource().getSlot());
+        Item item = sourceContainer.getItem(slot);
+        if (validateStackNetworkId(item.getNetId(), action.getSource().getStackNetworkId())) {
             log.warn("mismatch stack network id!");
+
             return context.error();
         }
+
         if (item.isNull()) {
             log.warn("cannot consume an air!");
+
             return context.error();
         }
+
         if (item.getCount() < count) {
             log.warn("cannot consume more items than the current amount!");
+
             return context.error();
         }
+
         if (item.getCount() > count) {
             item.setCount(item.getCount() - count);
             sourceContainer.setItem(slot, item, false);
@@ -52,12 +60,20 @@ public class ConsumeActionProcessor implements ItemStackRequestActionProcessor<C
             sourceContainer.clear(slot, false);
             item = sourceContainer.getItem(slot);
         }
-        if (context.has(ENCH_RECIPE_KEY) && (Boolean) context.get(ENCH_RECIPE_KEY) && action.getSource().getContainer() == ContainerSlotType.ENCHANTING_INPUT) {
+
+        Boolean isEnchRecipe = context.get(ENCH_RECIPE_KEY);
+        if (isEnchRecipe != null && isEnchRecipe && action.getSource().getContainer() == ContainerSlotType.ENCHANTING_INPUT) {
             return null;
         }
+
+        ContainerSlotType containerSlotType = sourceContainer.getSlotType(slot);
+        if (containerSlotType == null) {
+            throw new IllegalStateException("Unknown slot type for slot " + slot + " in inventory " + sourceContainer.getClass().getSimpleName());
+        }
+
         return context.success(List.of(
                 new ItemStackResponseContainer(
-                        sourceContainer.getSlotType(slot),
+                        containerSlotType,
                         Lists.newArrayList(
                                 new ItemStackResponseSlot(
                                         sourceContainer.toNetworkSlot(slot),
