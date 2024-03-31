e4e78201c1da4b141aa4dc3fabdf25d68e86aa8d
CoolLoong <1542536763@qq.com>
2024-03-31 13:29:50 +0800

    fix: customblock break time

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -438,10 +438,9 @@ public class Player extends EntityHuman implements CommandSender, ChunkLoader, I
                 pk.data = 65535 / breakTick;
                 this.getLevel().addChunkPacket(this.breakingBlock.getFloorX() >> 4, this.breakingBlock.getFloorZ() >> 4, pk);
                 this.level.addParticle(new PunchBlockParticle(pos, block));
-                //miningTimeRequired * 1000-101这个算法最匹配原版计算速度，我们并不想任何方块破坏处理都由服务端执行，只处理自定义方块以绕过原版固定挖掘时间的限制
                 if (this.breakingBlock instanceof CustomBlock) {
                     var timeDiff = time - breakingBlockTime;
-                    blockBreakProgress += timeDiff / (miningTimeRequired * 1000 - 101);
+                    blockBreakProgress += timeDiff / (miningTimeRequired * 1000);
                     if (blockBreakProgress > 0.99) {
                         this.onBlockBreakAbort(pos, face);
                         this.onBlockBreakComplete(pos.asBlockVector3(), face);
