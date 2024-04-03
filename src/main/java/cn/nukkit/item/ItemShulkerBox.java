b43ab916f81d2631ea81431737701446275e0d91
CoolLoong <1542536763@qq.com>
2024-04-03 23:36:50 +0800

    fix: pick shulker in creative

--- src/main/java/cn/nukkit/item/ItemShulkerBox.java
+++ src/main/java/cn/nukkit/item/ItemShulkerBox.java
@@ -18,54 +18,70 @@ public class ItemShulkerBox extends Item {
 
     public void internalAdjust() {
         switch (getDamage()) {
-            case 0:
+            case 0->{
                 this.name = "White Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.WHITE_SHULKER_BOX));
-            case 1:
+            }
+            case 1->{
                 this.name = "Orange Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.ORANGE_SHULKER_BOX));
-            case 2:
+            }
+            case 2->{
                 this.name = "Magenta Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.MAGENTA_SHULKER_BOX));
-            case 3:
+            }
+            case 3->{
                 this.name = "Light Blue Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.LIGHT_BLUE_SHULKER_BOX));
-            case 4:
+            }
+            case 4->{
                 this.name = "Yellow Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.YELLOW_SHULKER_BOX));
-            case 5:
+            }
+            case 5->{
                 this.name = "Lime Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.LIME_SHULKER_BOX));
-            case 6:
+            }
+            case 6->{
                 this.name = "Pink Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.PINK_SHULKER_BOX));
-            case 7:
+            }
+            case 7->{
                 this.name = "Gray Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.GRAY_SHULKER_BOX));
-            case 8:
+            }
+            case 8->{
                 this.name = "Light Gray Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.LIGHT_GRAY_SHULKER_BOX));
-            case 9:
+            }
+            case 9->{
                 this.name = "Cyan Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.CYAN_SHULKER_BOX));
-            case 10:
+            }
+            case 10->{
                 this.name = "Purple Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.PURPLE_SHULKER_BOX));
-            case 11:
+            }
+            case 11->{
                 this.name = "Blue Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.BLUE_SHULKER_BOX));
-            case 12:
+            }
+            case 12->{
                 this.name = "Brown Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.BROWN_SHULKER_BOX));
-            case 13:
+            }
+            case 13->{
                 this.name = "Green Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.GREEN_SHULKER_BOX));
-            case 14:
+            }
+            case 14->{
                 this.name = "Red Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.RED_SHULKER_BOX));
-            case 15:
+            }
+            case 15->{
                 this.name = "Black Shulker Box";
                 setBlockUnsafe(Block.get(BlockID.BLACK_SHULKER_BOX));
+            }
         }
         this.meta = 0;
     }
