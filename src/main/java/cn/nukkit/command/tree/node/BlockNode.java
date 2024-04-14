54ec3fdbddf02aed8dac32fcdb44de726acc4dc8
CoolLoong <1542536763@qq.com>
2024-04-14 13:25:19 +0800

    fix: fill BlockNode for legacy block name

--- src/main/java/cn/nukkit/command/tree/node/BlockNode.java
+++ src/main/java/cn/nukkit/command/tree/node/BlockNode.java
@@ -1,12 +1,13 @@
 package cn.nukkit.command.tree.node;
 
 import cn.nukkit.block.Block;
+import cn.nukkit.command.data.CommandEnum;
 import cn.nukkit.registry.Registries;
 
 /**
  * 解析对应参数为{@link Block}值
  * <p>
- * 所有命令枚举{@link cn.nukkit.command.data.CommandEnum#ENUM_BLOCK ENUM_BLOCK}如果没有手动指定{@link IParamNode},则会默认使用这个解析
+ * 所有命令枚举{@link CommandEnum#ENUM_BLOCK ENUM_BLOCK}如果没有手动指定{@link IParamNode},则会默认使用这个解析
  */
 public class BlockNode extends ParamNode<Block> {
     @Override
@@ -14,9 +15,23 @@ public class BlockNode extends ParamNode<Block> {
         arg = arg.startsWith("minecraft:") ? arg : arg.contains(":") ? arg : "minecraft:" + arg;
         Block block = Registries.BLOCK.get(arg);
         if (block == null) {
+            arg = mappingLegacyBlock(arg);
+        }
+        block = Registries.BLOCK.get(arg);
+        if (block == null) {
             this.error();
             return;
         }
         this.value = block;
+    }
+
+    public String mappingLegacyBlock(String name) {
+        return switch (name) {
+            case "minecraft:stone_slab" -> "minecraft:stone_block_slab";
+            case "minecraft:stone_slab2" -> "minecraft:stone_block_slab2";
+            case "minecraft:stone_slab3" -> "minecraft:stone_block_slab3";
+            case "minecraft:stone_slab4" -> "minecraft:stone_block_slab4";
+            default -> name;
+        };
     }
 }
