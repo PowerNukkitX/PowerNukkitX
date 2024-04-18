6f1d590c3d558ee656d53b01b3f9f31e7df9b817
MEFRREEX <mefrreex@gmail.com>
2024-04-18 11:20:39 +0300

    fix: open and close sound

--- src/main/java/cn/nukkit/block/BlockIronDoor.java
+++ src/main/java/cn/nukkit/block/BlockIronDoor.java
@@ -5,10 +5,11 @@ import cn.nukkit.block.property.CommonBlockProperties;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemIronDoor;
 import cn.nukkit.item.ItemTool;
+import cn.nukkit.level.Sound;
 import cn.nukkit.math.BlockFace;
 import org.jetbrains.annotations.NotNull;
 
-public class BlockIronDoor extends BlockWoodenDoor {
+public class BlockIronDoor extends BlockDoor {
     public static final BlockProperties PROPERTIES = new BlockProperties(IRON_DOOR, CommonBlockProperties.DIRECTION, CommonBlockProperties.DOOR_HINGE_BIT, CommonBlockProperties.OPEN_BIT, CommonBlockProperties.UPPER_BLOCK_BIT);
 
     @Override
@@ -62,5 +63,15 @@ public class BlockIronDoor extends BlockWoodenDoor {
     @Override
     public boolean canHarvestWithHand() {
         return false;
+    }
+
+    @Override
+    public void playOpenSound() {
+        level.addSound(this, Sound.OPEN_IRON_DOOR);
+    }
+
+    @Override
+    public void playCloseSound() {
+        level.addSound(this, Sound.CLOSE_IRON_DOOR);
     }
 }
