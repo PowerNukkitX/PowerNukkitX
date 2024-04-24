861890f3c17f46b02f139ff2128eea4025c28c7d
CoolLoong <1542536763@qq.com>
2024-04-24 17:20:29 +0800

    build: upgrade to jdk21

--- build.gradle.kts
+++ build.gradle.kts
@@ -138,8 +138,8 @@ tasks.clean {
 tasks.compileJava {
     options.encoding = "UTF-8"
     options.compilerArgs.add("-Xpkginfo:always")
-    java.sourceCompatibility = JavaVersion.VERSION_17
-    java.targetCompatibility = JavaVersion.VERSION_17
+    java.sourceCompatibility = JavaVersion.VERSION_21
+    java.targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.test {
