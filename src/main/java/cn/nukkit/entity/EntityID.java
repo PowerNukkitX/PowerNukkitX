d3bde9a3b418852762c46add022e27372e00e633
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2024-04-25 13:49:50 +0300

    feat: armadillo(no AI)

--- src/main/java/cn/nukkit/entity/EntityID.java
+++ src/main/java/cn/nukkit/entity/EntityID.java
@@ -3,171 +3,9 @@ package cn.nukkit.entity;
 import java.lang.String;
 
 public interface EntityID {
-  String STRIDER = "minecraft:strider";
-
-  String TADPOLE = "minecraft:tadpole";
-
-  String PIGLIN_BRUTE = "minecraft:piglin_brute";
-
-  String FROG = "minecraft:frog";
-
-  String ZOGLIN = "minecraft:zoglin";
-
-  String PIGLIN = "minecraft:piglin";
-
-  String HOGLIN = "minecraft:hoglin";
-
-  String BEE = "minecraft:bee";
-
-  String FOX = "minecraft:fox";
-
-  String SNOW_GOLEM = "minecraft:snow_golem";
-
-  String PLAYER = "minecraft:player";
-
-  String AXOLOTL = "minecraft:axolotl";
-
-  String WANDERING_TRADER = "minecraft:wandering_trader";
-
-  String COW = "minecraft:cow";
-
-  String CAMEL = "minecraft:camel";
-
-  String ALLAY = "minecraft:allay";
-
-  String WARDEN = "minecraft:warden";
-
-  String TRIPOD_CAMERA = "minecraft:tripod_camera";
-
-  String NPC = "minecraft:npc";
-
-  String BALLOON = "minecraft:balloon";
-
-  String ICE_BOMB = "minecraft:ice_bomb";
-
-  String AGENT = "minecraft:agent";
-
-  String GLOW_SQUID = "minecraft:glow_squid";
-
-  String GOAT = "minecraft:goat";
-
-  String PILLAGER = "minecraft:pillager";
-
-  String VEX = "minecraft:vex";
-
-  String EVOCATION_ILLAGER = "minecraft:evocation_illager";
-
-  String EVOCATION_FANG = "minecraft:evocation_fang";
-
-  String FIREWORKS_ROCKET = "minecraft:fireworks_rocket";
-
-  String LINGERING_POTION = "minecraft:lingering_potion";
-
-  String AREA_EFFECT_CLOUD = "minecraft:area_effect_cloud";
-
-  String LLAMA_SPIT = "minecraft:llama_spit";
-
-  String SMALL_FIREBALL = "minecraft:small_fireball";
-
-  String LIGHTNING_BOLT = "minecraft:lightning_bolt";
-
-  String BOAT = "minecraft:boat";
-
-  String CHEST_BOAT = "minecraft:chest_boat";
-
-  String WITHER_SKULL_DANGEROUS = "minecraft:wither_skull_dangerous";
-
-  String WITHER_SKULL = "minecraft:wither_skull";
-
-  String LEASH_KNOT = "minecraft:leash_knot";
-
-  String ENDER_PEARL = "minecraft:ender_pearl";
-
-  String SPLASH_POTION = "minecraft:splash_potion";
-
-  String FIREBALL = "minecraft:fireball";
-
-  String THROWN_TRIDENT = "minecraft:thrown_trident";
-
-  String PAINTING = "minecraft:painting";
-
-  String EGG = "minecraft:egg";
-
-  String SNOWBALL = "minecraft:snowball";
-
-  String ARROW = "minecraft:arrow";
-
-  String DRAGON_FIREBALL = "minecraft:dragon_fireball";
-
-  String FISHING_HOOK = "minecraft:fishing_hook";
-
-  String SHULKER_BULLET = "minecraft:shulker_bullet";
-
-  String ENDER_CRYSTAL = "minecraft:ender_crystal";
-
-  String EYE_OF_ENDER_SIGNAL = "minecraft:eye_of_ender_signal";
-
-  String XP_ORB = "minecraft:xp_orb";
-
-  String XP_BOTTLE = "minecraft:xp_bottle";
-
-  String FALLING_BLOCK = "minecraft:falling_block";
-
-  String TNT = "minecraft:tnt";
-
-  String ITEM = "minecraft:item";
-
-  String ARMOR_STAND = "minecraft:armor_stand";
-
-  String COMMAND_BLOCK_MINECART = "minecraft:command_block_minecart";
-
-  String CHEST_MINECART = "minecraft:chest_minecart";
-
-  String TNT_MINECART = "minecraft:tnt_minecart";
-
-  String HOPPER_MINECART = "minecraft:hopper_minecart";
-
-  String ZOMBIE_HORSE = "minecraft:zombie_horse";
-
-  String DOLPHIN = "minecraft:dolphin";
-
-  String PUFFERFISH = "minecraft:pufferfish";
-
-  String GUARDIAN = "minecraft:guardian";
-
-  String MULE = "minecraft:mule";
-
-  String MINECART = "minecraft:minecart";
-
-  String COD = "minecraft:cod";
-
-  String ENDER_DRAGON = "minecraft:ender_dragon";
-
-  String DONKEY = "minecraft:donkey";
-
-  String TROPICALFISH = "minecraft:tropicalfish";
-
-  String WITHER = "minecraft:wither";
-
-  String SKELETON_HORSE = "minecraft:skeleton_horse";
-
-  String PANDA = "minecraft:panda";
-
-  String SHULKER = "minecraft:shulker";
-
-  String LLAMA = "minecraft:llama";
-
-  String HORSE = "minecraft:horse";
-
   String CHICKEN = "minecraft:chicken";
 
-  String PARROT = "minecraft:parrot";
-
-  String TRADER_LLAMA = "minecraft:trader_llama";
-
-  String SILVERFISH = "minecraft:silverfish";
-
-  String SNIFFER = "minecraft:sniffer";
+  String COW = "minecraft:cow";
 
   String PIG = "minecraft:pig";
 
@@ -177,15 +15,7 @@ public interface EntityID {
 
   String VILLAGER = "minecraft:villager";
 
-  String TURTLE = "minecraft:turtle";
-
-  String POLAR_BEAR = "minecraft:polar_bear";
-
-  String VILLAGER_V2 = "minecraft:villager_v2";
-
   String MOOSHROOM = "minecraft:mooshroom";
-
-  String CAT = "minecraft:cat";
 
   String SQUID = "minecraft:squid";
 
@@ -195,11 +25,27 @@ public interface EntityID {
 
   String IRON_GOLEM = "minecraft:iron_golem";
 
-  String SALMON = "minecraft:salmon";
-
-  String ELDER_GUARDIAN = "minecraft:elder_guardian";
+  String SNOW_GOLEM = "minecraft:snow_golem";
 
   String OCELOT = "minecraft:ocelot";
+
+  String HORSE = "minecraft:horse";
+
+  String DONKEY = "minecraft:donkey";
+
+  String MULE = "minecraft:mule";
+
+  String SKELETON_HORSE = "minecraft:skeleton_horse";
+
+  String ZOMBIE_HORSE = "minecraft:zombie_horse";
+
+  String POLAR_BEAR = "minecraft:polar_bear";
+
+  String LLAMA = "minecraft:llama";
+
+  String PARROT = "minecraft:parrot";
+
+  String DOLPHIN = "minecraft:dolphin";
 
   String ZOMBIE = "minecraft:zombie";
 
@@ -215,6 +61,8 @@ public interface EntityID {
 
   String ENDERMAN = "minecraft:enderman";
 
+  String SILVERFISH = "minecraft:silverfish";
+
   String CAVE_SPIDER = "minecraft:cave_spider";
 
   String GHAST = "minecraft:ghast";
@@ -225,25 +73,179 @@ public interface EntityID {
 
   String ZOMBIE_VILLAGER = "minecraft:zombie_villager";
 
-  String ZOMBIE_VILLAGER_V2 = "minecraft:zombie_villager_v2";
-
-  String VINDICATOR = "minecraft:vindicator";
-
   String WITCH = "minecraft:witch";
 
   String STRAY = "minecraft:stray";
 
   String HUSK = "minecraft:husk";
 
-  String DROWNED = "minecraft:drowned";
-
   String WITHER_SKELETON = "minecraft:wither_skeleton";
 
-  String ELDER_GUARDIAN_GHOST = "minecraft:elder_guardian_ghost";
+  String GUARDIAN = "minecraft:guardian";
+
+  String ELDER_GUARDIAN = "minecraft:elder_guardian";
+
+  String NPC = "minecraft:npc";
+
+  String WITHER = "minecraft:wither";
+
+  String ENDER_DRAGON = "minecraft:ender_dragon";
+
+  String SHULKER = "minecraft:shulker";
+
+  String ENDERMITE = "minecraft:endermite";
+
+  String AGENT = "minecraft:agent";
+
+  String VINDICATOR = "minecraft:vindicator";
 
   String PHANTOM = "minecraft:phantom";
 
   String RAVAGER = "minecraft:ravager";
 
-  String ENDERMITE = "minecraft:endermite";
+  String ARMOR_STAND = "minecraft:armor_stand";
+
+  String TRIPOD_CAMERA = "minecraft:tripod_camera";
+
+  String ITEM = "minecraft:item";
+
+  String TNT = "minecraft:tnt";
+
+  String FALLING_BLOCK = "minecraft:falling_block";
+
+  String XP_BOTTLE = "minecraft:xp_bottle";
+
+  String XP_ORB = "minecraft:xp_orb";
+
+  String EYE_OF_ENDER_SIGNAL = "minecraft:eye_of_ender_signal";
+
+  String ENDER_CRYSTAL = "minecraft:ender_crystal";
+
+  String FIREWORKS_ROCKET = "minecraft:fireworks_rocket";
+
+  String THROWN_TRIDENT = "minecraft:thrown_trident";
+
+  String TURTLE = "minecraft:turtle";
+
+  String CAT = "minecraft:cat";
+
+  String SHULKER_BULLET = "minecraft:shulker_bullet";
+
+  String FISHING_HOOK = "minecraft:fishing_hook";
+
+  String DRAGON_FIREBALL = "minecraft:dragon_fireball";
+
+  String ARROW = "minecraft:arrow";
+
+  String SNOWBALL = "minecraft:snowball";
+
+  String EGG = "minecraft:egg";
+
+  String PAINTING = "minecraft:painting";
+
+  String MINECART = "minecraft:minecart";
+
+  String FIREBALL = "minecraft:fireball";
+
+  String SPLASH_POTION = "minecraft:splash_potion";
+
+  String ENDER_PEARL = "minecraft:ender_pearl";
+
+  String LEASH_KNOT = "minecraft:leash_knot";
+
+  String WITHER_SKULL = "minecraft:wither_skull";
+
+  String BOAT = "minecraft:boat";
+
+  String WITHER_SKULL_DANGEROUS = "minecraft:wither_skull_dangerous";
+
+  String LIGHTNING_BOLT = "minecraft:lightning_bolt";
+
+  String SMALL_FIREBALL = "minecraft:small_fireball";
+
+  String AREA_EFFECT_CLOUD = "minecraft:area_effect_cloud";
+
+  String HOPPER_MINECART = "minecraft:hopper_minecart";
+
+  String TNT_MINECART = "minecraft:tnt_minecart";
+
+  String CHEST_MINECART = "minecraft:chest_minecart";
+
+  String COMMAND_BLOCK_MINECART = "minecraft:command_block_minecart";
+
+  String LINGERING_POTION = "minecraft:lingering_potion";
+
+  String LLAMA_SPIT = "minecraft:llama_spit";
+
+  String EVOCATION_FANG = "minecraft:evocation_fang";
+
+  String EVOCATION_ILLAGER = "minecraft:evocation_illager";
+
+  String VEX = "minecraft:vex";
+
+  String ICE_BOMB = "minecraft:ice_bomb";
+
+  String BALLOON = "minecraft:balloon";
+
+  String PUFFERFISH = "minecraft:pufferfish";
+
+  String SALMON = "minecraft:salmon";
+
+  String DROWNED = "minecraft:drowned";
+
+  String TROPICALFISH = "minecraft:tropicalfish";
+
+  String COD = "minecraft:cod";
+
+  String PANDA = "minecraft:panda";
+
+  String PILLAGER = "minecraft:pillager";
+
+  String VILLAGER_V2 = "minecraft:villager_v2";
+
+  String ZOMBIE_VILLAGER_V2 = "minecraft:zombie_villager_v2";
+
+  String WANDERING_TRADER = "minecraft:wandering_trader";
+
+  String ELDER_GUARDIAN_GHOST = "minecraft:elder_guardian_ghost";
+
+  String FOX = "minecraft:fox";
+
+  String BEE = "minecraft:bee";
+
+  String PIGLIN = "minecraft:piglin";
+
+  String HOGLIN = "minecraft:hoglin";
+
+  String STRIDER = "minecraft:strider";
+
+  String ZOGLIN = "minecraft:zoglin";
+
+  String PIGLIN_BRUTE = "minecraft:piglin_brute";
+
+  String GOAT = "minecraft:goat";
+
+  String GLOW_SQUID = "minecraft:glow_squid";
+
+  String AXOLOTL = "minecraft:axolotl";
+
+  String WARDEN = "minecraft:warden";
+
+  String FROG = "minecraft:frog";
+
+  String TADPOLE = "minecraft:tadpole";
+
+  String ALLAY = "minecraft:allay";
+
+  String CAMEL = "minecraft:camel";
+
+  String SNIFFER = "minecraft:sniffer";
+
+  String ARMADILLO = "minecraft:armadillo";
+
+  String TRADER_LLAMA = "minecraft:trader_llama";
+
+  String CHEST_BOAT = "minecraft:chest_boat";
+
+  String PLAYER = "minecraft:player";
 }
--- src/main/java/cn/nukkit/item/ItemArmadilloSpawnEgg.java
+++ src/main/java/cn/nukkit/item/ItemArmadilloSpawnEgg.java
@@ -1,7 +1,17 @@
 package cn.nukkit.item;
 
-public class ItemArmadilloSpawnEgg extends Item {
+public class ItemArmadilloSpawnEgg extends ItemSpawnEgg {
      public ItemArmadilloSpawnEgg() {
          super(ARMADILLO_SPAWN_EGG);
      }
+
+    @Override
+    public int getEntityNetworkId() {
+        return 142;
+    }
+
+    @Override
+    public void setDamage(int meta) {
+
+    }
 }
