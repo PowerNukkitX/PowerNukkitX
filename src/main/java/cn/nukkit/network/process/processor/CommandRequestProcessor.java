ce6a3da7628ae03f3568c0ebd6f7d707bb22c105
CoolLoong <1542536763@qq.com>
2024-05-30 01:13:19 +0800

    fix: anti crasher

--- src/main/java/cn/nukkit/network/process/processor/CommandRequestProcessor.java
+++ src/main/java/cn/nukkit/network/process/processor/CommandRequestProcessor.java
@@ -5,11 +5,21 @@ import cn.nukkit.event.player.PlayerCommandPreprocessEvent;
 import cn.nukkit.network.process.DataPacketProcessor;
 import cn.nukkit.network.protocol.CommandRequestPacket;
 import cn.nukkit.network.protocol.ProtocolInfo;
+import com.google.common.util.concurrent.RateLimiter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.TimeUnit;
+
 public class CommandRequestProcessor extends DataPacketProcessor<CommandRequestPacket> {
+    final RateLimiter rateLimiter = RateLimiter.create(500);
+
     @Override
     public void handle(@NotNull PlayerHandle playerHandle, @NotNull CommandRequestPacket pk) {
+        int length = pk.command.length();
+        if (!rateLimiter.tryAcquire(length, 300, TimeUnit.MILLISECONDS)) {
+            playerHandle.player.getSession().close("kick because hack");
+            return;
+        }
         if (!playerHandle.player.spawned || !playerHandle.player.isAlive()) {
             return;
         }
