81442e8550fb8372f400ecc1fef8de74c0f2c7e5
AzaleeX <azaleemcpe@gmail.com>
2024-06-28 10:09:33 +0200

    feat(add the latest discs not yet implemented in powernukkitx) (#1627)

--- src/main/java/cn/nukkit/item/ItemMusicDiscCreatorMusicBox.java
+++ src/main/java/cn/nukkit/item/ItemMusicDiscCreatorMusicBox.java
@@ -1,7 +1,12 @@
 package cn.nukkit.item;
 
-public class ItemMusicDiscCreatorMusicBox extends Item {
+public class ItemMusicDiscCreatorMusicBox extends ItemMusicDisc {
     public ItemMusicDiscCreatorMusicBox() {
         super(MUSIC_DISC_CREATOR_MUSIC_BOX);
+    }
+
+    @Override
+    public String getSoundId() {
+        return "record.creator_music_box";
     }
 }
--- src/main/java/cn/nukkit/item/ItemMusicDiscPrecipice.java
+++ src/main/java/cn/nukkit/item/ItemMusicDiscPrecipice.java
@@ -1,7 +1,12 @@
 package cn.nukkit.item;
 
-public class ItemMusicDiscPrecipice extends Item {
+public class ItemMusicDiscPrecipice extends ItemMusicDisc {
     public ItemMusicDiscPrecipice() {
         super(MUSIC_DISC_PRECIPICE);
+    }
+
+    @Override
+    public String getSoundId() {
+        return "record.precipice";
     }
 }
