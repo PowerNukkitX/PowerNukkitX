3eae6a8c8d6208125c0fecde47a89bf07b0b1eb0
AzaleeX <azaleemcpe@gmail.com>
2024-06-28 13:14:16 +0200

    feat(implementation of tem wind charge operation): attention repulsion doesn't work because i have no fu** idea how minecraft does it

--- src/main/java/cn/nukkit/item/ItemWindCharge.java
+++ src/main/java/cn/nukkit/item/ItemWindCharge.java
@@ -1,7 +1,33 @@
 package cn.nukkit.item;
 
-public class ItemWindCharge extends Item {
-     public ItemWindCharge() {
-         super(WIND_CHARGE);
-     }
+import static cn.nukkit.entity.EntityID.WIND_CHARGE_PROJECTILE;
+
+public class ItemWindCharge extends ProjectileItem {
+
+    public ItemWindCharge() {
+        this(0, 1);
+    }
+
+    public ItemWindCharge(Integer meta) {
+        this(meta, 1);
+    }
+
+    public ItemWindCharge(Integer meta, int count) {
+        super(WIND_CHARGE, 0, count, "Wind Charge");
+    }
+
+    @Override
+    public int getMaxStackSize() {
+        return 64;
+    }
+
+    @Override
+    public String getProjectileEntityType() {
+        return WIND_CHARGE_PROJECTILE;
+    }
+
+    @Override
+    public float getThrowForce() {
+        return 1.5f;
+    }
 }
