7166eb91b8a55e338766848c64275364a80ef944
AzaleeX <azaleemcpe@gmail.com>
2024-07-06 11:47:59 +0200

    merge on branch new-data (#1636)
    * feat(adapt block_tags.json and item_tags)
    
    * feat(add all new data files for migration):
    
    be careful, you need to change all the code to make it compatible, which is what you'll be doing in this new branch.

--- src/test/java/cn/nukkit/block/BlockPropertiesTest.java
+++ src/test/java/cn/nukkit/block/BlockPropertiesTest.java
@@ -7,6 +7,7 @@ import cn.nukkit.registry.BlockRegistry;
 import cn.nukkit.registry.Registries;
 import com.google.common.collect.Sets;
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.ObjectUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -15,23 +16,25 @@ import java.util.TreeMap;
 
 public class BlockPropertiesTest {
 
-
     @Test
     @SneakyThrows
     void BlockPaletteTest() {
         Registries.BLOCK.init();
         TreeMap<String, String> errors = new TreeMap<>();
+        int countedBlocks = 0; // Ajout pour compter les blocs traités
         try (var stream = new FileInputStream("src/main/resources/block_palette.nbt")) {
             CompoundTag nbt = NBTIO.readCompressed(stream);
             ListTag<CompoundTag> blocks = nbt.getList("blocks", CompoundTag.class);
             for (var b : blocks.getAll()) {
-                int i = b.getInt("network_id");
-                BlockState blockState = Registries.BLOCKSTATE.get(i);
+                ObjectUtils.Null blockState = null;
                 if (blockState == null) {
-                    errors.put(b.getString("name"), "palette not match vanilla,expected: " + i + " block: " + b.getString("name"));
+                    errors.put(b.getString("name"), "palette not match vanilla, expected:  block: " + b.getString("name"));
+                } else {
+                    countedBlocks++; // Incrémenter pour chaque bloc valide
                 }
             }
         }
+        System.out.println("Nombre de blocs comptés: " + countedBlocks); // Log pour débogage
         Assertions.assertEquals(BlockRegistry.skipBlockSet.size(), errors.size(), Sets.difference(errors.keySet(), BlockRegistry.skipBlockSet).toString());
     }
 }
