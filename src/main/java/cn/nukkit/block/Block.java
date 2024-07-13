3875b6f6ba9b8216f87d87236613f6ba7f8bfeaa
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-07-13 13:28:26 +0200

    Update Block.java

--- src/main/java/cn/nukkit/block/Block.java
+++ src/main/java/cn/nukkit/block/Block.java
@@ -54,7 +54,7 @@ public abstract class Block extends Position implements Metadatable, AxisAligned
 
     public static boolean isNotActivate(Player player) {
         if (player == null) {
-            return false;
+            return true;
         }
         Item itemInHand = player.getInventory().getItemInHand();
         return (player.isSneaking() || player.isFlySneaking()) && !(itemInHand.isTool() || itemInHand.isNull());
