82f5b446eb51b07c9bda3d1ffa857430222a86ae
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-09-14 17:18:31 +0200

    Fix stonecutter (#1681)
    * Update PlayerChunkManager.java
    
    The forEach caused some problems for some reason so I changed it to a for loop.
    
    * Update PlayerChunkManager.java
    
    * Update StonecutterInventory.java

--- src/main/java/cn/nukkit/inventory/StonecutterInventory.java
+++ src/main/java/cn/nukkit/inventory/StonecutterInventory.java
@@ -1,11 +1,16 @@
 package cn.nukkit.inventory;
 
 import cn.nukkit.Player;
+import cn.nukkit.Server;
 import cn.nukkit.block.BlockStonecutterBlock;
 import cn.nukkit.item.Item;
+import cn.nukkit.network.protocol.types.itemstack.ContainerSlotType;
+import cn.nukkit.recipe.Input;
+
+import java.util.Map;
 
 
-public class StonecutterInventory extends ContainerInventory implements CraftTypeInventory {
+public class StonecutterInventory extends ContainerInventory implements CraftTypeInventory, InputInventory {
 
 
     public StonecutterInventory(BlockStonecutterBlock blockStonecutterBlock) {
@@ -20,14 +25,33 @@ public class StonecutterInventory extends ContainerInventory implements CraftTyp
     @Override
     public void onClose(Player who) {
         super.onClose(who);
-
         Item[] drops = who.getInventory().addItem(this.getItem(0));
         for (Item drop : drops) {
             if (!who.dropItem(drop)) {
                 this.getHolder().getLevel().dropItem(this.getHolder().getVector3().add(0.5, 0.5, 0.5), drop);
             }
         }
-
         this.clear(0);
+    }
+
+    @Override
+    public boolean setItem(int index, Item item, boolean send) {
+        if(index == 3) {
+            slots.put(index, item);
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public Input getInput() {
+        return new Input(1, 1, new Item[][]{new Item[]{getItem(3)}});
+    }
+
+    @Override
+    public Map<Integer, ContainerSlotType> slotTypeMap() {
+        Map<Integer, ContainerSlotType> map = super.slotTypeMap();
+        map.put(3, ContainerSlotType.STONECUTTER_INPUT);
+        map.put(4, ContainerSlotType.STONECUTTER_RESULT);
+        return map;
     }
 }
