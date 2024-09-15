47c09812b90f4c300ed261198f1d46314e449d90
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-09-15 18:45:04 +0200

    Update ItemBrush.java

--- src/main/java/cn/nukkit/item/ItemBrush.java
+++ src/main/java/cn/nukkit/item/ItemBrush.java
@@ -3,7 +3,7 @@ package cn.nukkit.item;
 /**
  * todo feature
  */
-public class ItemBrush extends ItemTool {
+public class ItemBrush extends Item implements ItemDurable {
     public ItemBrush() {
         super(BRUSH);
     }
