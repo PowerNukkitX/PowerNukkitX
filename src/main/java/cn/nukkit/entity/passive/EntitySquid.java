4876df21490a7d45d16063358a2e222451bd4fd3
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-09-15 19:22:06 +0200

    Update EntitySquid.java

--- src/main/java/cn/nukkit/entity/passive/EntitySquid.java
+++ src/main/java/cn/nukkit/entity/passive/EntitySquid.java
@@ -1,11 +1,22 @@
 package cn.nukkit.entity.passive;
 
 import cn.nukkit.entity.EntitySwimmable;
+import cn.nukkit.entity.ai.behavior.Behavior;
+import cn.nukkit.entity.ai.behaviorgroup.BehaviorGroup;
+import cn.nukkit.entity.ai.behaviorgroup.IBehaviorGroup;
+import cn.nukkit.entity.ai.controller.DiveController;
+import cn.nukkit.entity.ai.controller.LookController;
+import cn.nukkit.entity.ai.controller.SpaceMoveController;
+import cn.nukkit.entity.ai.executor.SpaceRandomRoamExecutor;
+import cn.nukkit.entity.ai.route.finder.impl.SimpleSpaceAStarRouteFinder;
+import cn.nukkit.entity.ai.route.posevaluator.SwimmingPosEvaluator;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemID;
 import cn.nukkit.level.format.IChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
 
 /**
  * @author PikyCZ
@@ -18,6 +29,23 @@ public class EntitySquid extends EntityAnimal implements EntitySwimmable {
 
     public EntitySquid(IChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
+    }
+
+    @Override
+    public IBehaviorGroup requireBehaviorGroup() {
+        return new BehaviorGroup(
+                this.tickSpread,
+                Set.of(),
+                Set.of(
+                        new Behavior(
+                                new SpaceRandomRoamExecutor(0.36f, 12, 1, 80, false, -1, false, 10),
+                                entity -> true, 1)
+                ),
+                Set.of(),
+                Set.of(new SpaceMoveController(), new LookController(true, true), new DiveController()),
+                new SimpleSpaceAStarRouteFinder(new SwimmingPosEvaluator(), this),
+                this
+        );
     }
 
     @Override
