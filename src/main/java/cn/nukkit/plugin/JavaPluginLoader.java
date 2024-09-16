21b757b079604a9fde1cb5a7ef2fea8225a2c9f3
KeksDev <54219265+tunikakeks@users.noreply.github.com>
2024-09-16 19:50:53 +0200

    Re-added nukkit.yml

--- src/main/java/cn/nukkit/plugin/JavaPluginLoader.java
+++ src/main/java/cn/nukkit/plugin/JavaPluginLoader.java
@@ -87,9 +87,12 @@ public class JavaPluginLoader implements PluginLoader {
         try (JarFile jar = new JarFile(file)) {
             JarEntry entry = jar.getJarEntry("powernukkitx.yml");
             if (entry == null) {
-                entry = jar.getJarEntry("plugin.yml");
+                entry = jar.getJarEntry("nukkit.yml");
                 if (entry == null) {
-                    return null;
+                    entry = jar.getJarEntry("plugin.yml");
+                    if (entry == null) {
+                        return null;
+                    }
                 }
             }
             try (InputStream stream = jar.getInputStream(entry)) {
