f68f71340414a0583332bfe18c3124c55de7b755
AzaleeX <azaleemcpe@gmail.com>
2024-09-19 20:39:42 +0200

    feat: UsedByReflection.java

--- src/main/java/cn/nukkit/api/UsedByReflection.java
+++ src/main/java/cn/nukkit/api/UsedByReflection.java
@@ -1,8 +1,14 @@
 package cn.nukkit.api;
 
-import java.lang.annotation.Documented;
+import java.lang.annotation.*;
 
-
+/**
+ * Indicates that the annotated element is used by reflection.
+ * <p>
+ * This annotation is used to mark elements that are accessed or manipulated through reflection.
+ * It serves as a warning to developers that the annotated elements are intended to be used by reflection mechanisms.
+ * </p>
+ */
 @Documented
 public @interface UsedByReflection {
 }
