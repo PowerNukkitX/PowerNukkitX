d66e260c5e1fe2e7a0e79b37575bb447492c6852
AzaleeX <azaleemcpe@gmail.com>
2024-09-19 20:39:25 +0200

    feat: NonComputationAtomic.java

--- src/main/java/cn/nukkit/api/NonComputationAtomic.java
+++ src/main/java/cn/nukkit/api/NonComputationAtomic.java
@@ -3,7 +3,11 @@ package cn.nukkit.api;
 import java.lang.annotation.*;
 
 /**
- * Marks that the annotated element is not computation atomic, and its computeXXX methods may not be atomic.
+ * Indicates that the annotated element is not computation atomic.
+ * <p>
+ * This annotation is used to mark elements whose computeXXX methods may not be atomic.
+ * It serves as a warning to developers that the annotated methods or elements may not perform atomic computations.
+ * </p>
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.TYPE,
