8a5b55901fe0e992b28b7e0e3909ca1a0c347c75
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-09-22 12:51:02 +0200

    Revert "Revert "Craft item count"" (#1702)
    Revert "Revert "Craft item count" (#1701)"
    
    This reverts commit 31dfa0be5777f43f7f717cfbcb57755deb06aace.

--- src/main/java/cn/nukkit/event/inventory/CraftItemEvent.java
+++ src/main/java/cn/nukkit/event/inventory/CraftItemEvent.java
@@ -21,10 +21,13 @@ public class CraftItemEvent extends Event implements Cancellable {
 
     private final Player player;
 
-    public CraftItemEvent(Player player, Item[] input, Recipe recipe) {
+    private final int crafts;
+
+    public CraftItemEvent(Player player, Item[] input, Recipe recipe, int crafts) {
         this.player = player;
         this.input = input;
         this.recipe = recipe;
+        this.crafts = crafts;
     }
 
     public Item[] getInput() {
