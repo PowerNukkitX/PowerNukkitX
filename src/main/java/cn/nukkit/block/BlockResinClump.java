6946affed7a92fcba800aaef9018ceb113b859c0
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-12-04 23:10:20 +0100

    Fixed that resin Clumps could not be placed

--- src/main/java/cn/nukkit/block/BlockResinClump.java
+++ src/main/java/cn/nukkit/block/BlockResinClump.java
@@ -1,9 +1,11 @@
 package cn.nukkit.block;
 
 import cn.nukkit.block.property.CommonBlockProperties;
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemTool;
 import org.jetbrains.annotations.NotNull;
 
-public class BlockResinClump extends BlockFlowable {
+public class BlockResinClump extends BlockLichen {
     public static final BlockProperties PROPERTIES = new BlockProperties(RESIN_CLUMP, CommonBlockProperties.MULTI_FACE_DIRECTION_BITS);
 
     @Override
@@ -17,5 +19,20 @@ public class BlockResinClump extends BlockFlowable {
 
     public BlockResinClump(BlockState blockstate) {
         super(blockstate);
+    }
+
+    @Override
+    public boolean canHarvestWithHand() {
+        return true;
+    }
+
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_NONE;
+    }
+
+    @Override
+    public Item[] getDrops(Item item) {
+        return new Item[]{Item.get(Block.RESIN_CLUMP, 0, getGrowthSides().length)};
     }
 }
