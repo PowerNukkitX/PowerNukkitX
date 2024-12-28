06c4d97ea264556b7b9acae8ef38d657aab7f89e
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-12-28 17:44:47 +0100

    Implemented Drowned AI

--- src/main/java/cn/nukkit/entity/mob/EntityDrowned.java
+++ src/main/java/cn/nukkit/entity/mob/EntityDrowned.java
@@ -4,10 +4,27 @@ import cn.nukkit.Player;
 import cn.nukkit.entity.EntitySmite;
 import cn.nukkit.entity.EntitySwimmable;
 import cn.nukkit.entity.EntityWalkable;
+import cn.nukkit.entity.ai.behavior.Behavior;
+import cn.nukkit.entity.ai.behaviorgroup.BehaviorGroup;
+import cn.nukkit.entity.ai.behaviorgroup.IBehaviorGroup;
+import cn.nukkit.entity.ai.controller.LookController;
+import cn.nukkit.entity.ai.controller.WalkController;
+import cn.nukkit.entity.ai.evaluator.AttackCheckEvaluator;
+import cn.nukkit.entity.ai.evaluator.NearestCheckEvaluator;
+import cn.nukkit.entity.ai.executor.BowShootExecutor;
+import cn.nukkit.entity.ai.executor.FlatRandomRoamExecutor;
+import cn.nukkit.entity.ai.executor.MeleeAttackExecutor;
+import cn.nukkit.entity.ai.executor.TridentThrowExecutor;
+import cn.nukkit.entity.ai.memory.CoreMemoryTypes;
+import cn.nukkit.entity.ai.route.finder.impl.SimpleFlatAStarRouteFinder;
+import cn.nukkit.entity.ai.route.posevaluator.WalkingPosEvaluator;
+import cn.nukkit.entity.ai.sensor.NearestPlayerSensor;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.format.IChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
 
 /**
  * @author PetteriM1
@@ -23,7 +40,32 @@ public class EntityDrowned extends EntityMob implements EntitySwimmable, EntityW
         super(chunk, nbt);
     }
 
-    
+    @Override
+    public IBehaviorGroup requireBehaviorGroup() {
+        return new BehaviorGroup(
+                this.tickSpread,
+                Set.of(),
+                Set.of(
+                        new Behavior(new TridentThrowExecutor(CoreMemoryTypes.ATTACK_TARGET, 0.3f, 15, true, 30, 20), all(new AttackCheckEvaluator(), entity -> getItemInHand().getId().equals(Item.TRIDENT)), 5, 1),
+                        new Behavior(new TridentThrowExecutor(CoreMemoryTypes.NEAREST_PLAYER, 0.3f, 15, true, 30, 20), all(new NearestCheckEvaluator(), entity -> getItemInHand().getId().equals(Item.TRIDENT)), 4, 1),
+                        new Behavior(new MeleeAttackExecutor(CoreMemoryTypes.ATTACK_TARGET, 0.3f, 40, true, 30), new AttackCheckEvaluator(), 3, 1),
+                        new Behavior(new MeleeAttackExecutor(CoreMemoryTypes.NEAREST_PLAYER, 0.3f, 40, false, 30),
+                                entity -> {
+                                    if (entity.getMemoryStorage().isEmpty(CoreMemoryTypes.NEAREST_PLAYER)) {
+                                        return false;
+                                    } else {
+                                        Player player = entity.getMemoryStorage().get(CoreMemoryTypes.NEAREST_PLAYER);
+                                        return player.isSurvival() || player.isAdventure();
+                                    }
+                                }, 2, 1),
+                        new Behavior(new FlatRandomRoamExecutor(0.3f, 12, 100, false, -1, true, 10), (entity -> true), 1, 1)
+                ),
+                Set.of(new NearestPlayerSensor(40, 0, 20)),
+                Set.of(new WalkController(), new LookController(true, true)),
+                new SimpleFlatAStarRouteFinder(new WalkingPosEvaluator(), this),
+                this
+        );
+    }
 
     @Override
     protected void initEntity() {
