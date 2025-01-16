04d174098a6b2c0c601f910683626df036c75682
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2025-01-16 16:07:49 +0100

    fix: copy compoundtag for smithing upgrades

--- src/main/java/cn/nukkit/inventory/request/CraftRecipeActionProcessor.java
+++ src/main/java/cn/nukkit/inventory/request/CraftRecipeActionProcessor.java
@@ -202,10 +202,7 @@ public class CraftRecipeActionProcessor implements ItemStackRequestActionProcess
         match &= expectTemplate.match(template);
         if (match) {
             Item result = recipe.getResult().clone();
-            Enchantment[] enchantments = smithingInventory.getIngredient().getEnchantments();
-            if(enchantments.length != 0) {
-                result.addEnchantment();
-            }
+            result.setCompoundTag(ingredient.getOrCreateNamedTag().copy());
             player.getCreativeOutputInventory().setItem(result);
             return null;
         }
@@ -237,7 +234,7 @@ public class CraftRecipeActionProcessor implements ItemStackRequestActionProcess
             Item result = equipment.clone();
             CompoundTag trim = new CompoundTag().putString("Material", trimMaterial.materialId())
                     .putString("Pattern", trimPattern.patternId());
-            CompoundTag compound = result.getOrCreateNamedTag();
+            CompoundTag compound = ingredient.getOrCreateNamedTag().copy();
             compound.putCompound("Trim", trim);
             result.setNamedTag(compound);
             player.getCreativeOutputInventory().setItem(result);
