ea189c326558858edaaa8b4a35014cc7d074d9b1
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-01-21 21:59:36 +0100

    Fixed brewing recipes (#1795)

--- src/main/java/cn/nukkit/recipe/ContainerRecipe.java
+++ src/main/java/cn/nukkit/recipe/ContainerRecipe.java
@@ -1,6 +1,9 @@
 package cn.nukkit.recipe;
 
 import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemGunpowder;
+import cn.nukkit.item.ItemPotion;
+import cn.nukkit.item.ItemSplashPotion;
 import cn.nukkit.registry.RecipeRegistry;
 
 import java.util.List;
@@ -13,6 +16,16 @@ public class ContainerRecipe extends MixRecipe {
     public ContainerRecipe(String recipeId, Item input, Item ingredient, Item output) {
         super(recipeId == null ? RecipeRegistry.computeRecipeIdWithItem(List.of(output), List.of(input, ingredient), RecipeType.CONTAINER) : recipeId,
                 input, ingredient, output);
+    }
+
+    @Override
+    public boolean fastCheck(Item... items) {
+        if(items.length == 2) {
+            if(items[1] instanceof ItemPotion) {
+                return items[0].equals(getIngredient());
+            }
+        }
+        return false;
     }
 
     @Override
