8f2a39bfa6a4276e8c2287cfad1621fe30195a6c
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-02-01 02:12:14 +0100

    Made the vex returning to its illager

--- src/main/java/cn/nukkit/entity/mob/EntityVex.java
+++ src/main/java/cn/nukkit/entity/mob/EntityVex.java
@@ -161,6 +161,10 @@ public class EntityVex extends EntityMob implements EntityFlyable {
     public boolean onUpdate(int currentTick) {
         if(closed) return true;
         if(getIllager() != null) {
+            if(this.distanceSquared(illager) > 56.25d) {
+                setMoveTarget(illager);
+                setLookTarget(illager);
+            }
             if(ticksLived%20 == 0) {
                 if(ticksLived >= start_damage_timer*20) {
                     this.attack(new EntityDamageEvent(this, EntityDamageEvent.DamageCause.AGE, 1));
