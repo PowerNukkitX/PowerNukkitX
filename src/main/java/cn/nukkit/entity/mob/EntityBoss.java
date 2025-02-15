0795601d820446456af008f81779bad51573bc84
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-02-15 02:43:17 +0100

    Did some global boss stuff

--- src/main/java/cn/nukkit/entity/mob/EntityBoss.java
+++ src/main/java/cn/nukkit/entity/mob/EntityBoss.java
@@ -1,6 +1,8 @@
 package cn.nukkit.entity.mob;
 
+import cn.nukkit.Player;
 import cn.nukkit.Server;
+import cn.nukkit.block.Block;
 import cn.nukkit.level.Sound;
 import cn.nukkit.level.format.IChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
@@ -23,4 +25,39 @@ public abstract class EntityBoss extends EntityMob {
         pkBoss.healthPercent = health / getMaxHealth();
         Server.broadcastPacket(getViewers().values(), pkBoss);
     }
+
+    @Override
+    public void spawnTo(Player player) {
+        super.spawnTo(player);
+        if(player.locallyInitialized) {
+            addBossbar(player);
+        }
+    }
+
+    protected boolean canBreakBlock(Block block) {
+        return switch (block.getId()) {
+            case Block.BARRIER,
+                 Block.BEDROCK,
+                 Block.COMMAND_BLOCK,
+                 Block.CHAIN_COMMAND_BLOCK,
+                 Block.REPEATING_COMMAND_BLOCK,
+                 Block.CRYING_OBSIDIAN,
+                 Block.END_STONE,
+                 Block.END_PORTAL,
+                 Block.END_PORTAL_FRAME,
+                 Block.END_GATEWAY,
+                 Block.FIRE,
+                 Block.IRON_BARS,
+                 Block.JIGSAW,
+                 Block.OBSIDIAN,
+                 Block.REINFORCED_DEEPSLATE,
+                 Block.RESPAWN_ANCHOR,
+                 Block.SOUL_FIRE,
+                 Block.STRUCTURE_BLOCK -> false;
+            default -> true;
+        };
+    }
+
+    public abstract void addBossbar(Player player);
+
 }
