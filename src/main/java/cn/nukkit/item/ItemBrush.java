f56fd938eb657a6971ea8fba1194e10cd90164e9
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-02-16 02:53:16 +0100

    Fixed brush

--- src/main/java/cn/nukkit/item/ItemBrush.java
+++ src/main/java/cn/nukkit/item/ItemBrush.java
@@ -1,14 +1,5 @@
 package cn.nukkit.item;
 
-import cn.nukkit.Player;
-import cn.nukkit.block.Block;
-import cn.nukkit.entity.data.EntityFlag;
-import cn.nukkit.level.Level;
-import cn.nukkit.math.BlockFace;
-
-/**
- * todo feature
- */
 public class ItemBrush extends ItemTool {
     public ItemBrush() {
         super(BRUSH);
@@ -20,8 +11,7 @@ public class ItemBrush extends ItemTool {
     }
 
     @Override
-    public boolean onActivate(Level level, Player player, Block block, Block target, BlockFace face, double fx, double fy, double fz) {
-        player.setDataFlag(EntityFlag.USING_ITEM, true);
-        return super.onActivate(level, player, block, target, face, fx, fy, fz);
+    public int getMaxDurability() {
+        return 65;
     }
 }
