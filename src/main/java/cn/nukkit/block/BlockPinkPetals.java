e1bcea802f5231aba84a33b9af117bd163ef7c64
Omniac <64994204+OmniacDev@users.noreply.github.com>
2025-02-20 00:07:38 +1300

    Update isSupportValid for BlockPinkPetals

--- src/main/java/cn/nukkit/block/BlockPinkPetals.java
+++ src/main/java/cn/nukkit/block/BlockPinkPetals.java
@@ -56,7 +56,7 @@ public class BlockPinkPetals extends BlockFlowable {
 
     private static boolean isSupportValid(Block block) {
         return switch (block.getId()) {
-            case GRASS_BLOCK, DIRT, FARMLAND, PODZOL, DIRT_WITH_ROOTS, MOSS_BLOCK -> true;
+            case GRASS_BLOCK, DIRT, COARSE_DIRT, PODZOL, FARMLAND, MYCELIUM, DIRT_WITH_ROOTS, MOSS_BLOCK, PALE_MOSS_BLOCK -> true;
             default -> false;
         };
     }
