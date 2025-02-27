1990d20797e05094fff14ea1d6cb802d1280c778
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-02-27 17:14:26 +0100

    Update Player.java

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -4667,6 +4667,10 @@ public class Player extends EntityHuman implements CommandSender, ChunkLoader, I
         form.send(this);
     }
 
+    public void showFormWindow(Form form, int id) {
+        form.send(this, id);
+    }
+
     public Optional<Inventory> getTopWindow() {
         for (Entry<Inventory, Integer> entry : this.windows.entrySet()) {
             if (!this.permanentWindows.contains(entry.getValue())) {
