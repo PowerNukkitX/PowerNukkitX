bb0c83ab3c88bd5b0e59bb6137bf16a3b8ddca5c
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2025-03-03 13:14:02 +0100

    junit: forms

--- src/test/java/cn/nukkit/form/FormTest.java
+++ src/test/java/cn/nukkit/form/FormTest.java
@@ -30,29 +30,82 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @ExtendWith(GameMockExtension.class)
 public class FormTest {
     @Test
     void test_FormWindowCustom(TestPlayer player, TestPluginManager testPluginManager) {
         testPluginManager.resetAll();
-        ElementDropdown test1 = new ElementDropdown("test1", List.of("1", "2", "3"), 1);//default 2
-        ElementInput test2 = new ElementInput("test2", "placeholder", "defaultText");
-        ElementLabel test3 = new ElementLabel("test3");
-        ElementSlider test4 = new ElementSlider("test4", 0, 100, 1, 50);
-        ElementStepSlider test5 = new ElementStepSlider("test5", List.of("step1", "step2"), 1);//default step2
-        ElementToggle test6 = new ElementToggle("test6", true);
-        CustomForm test = new CustomForm("test")
-                .elements(ObjectArrayList.of(
+
+        ElementDropdown test1 = new ElementDropdown()
+                .text("text1")
+                .options(new ArrayList<>(List.of("1", "2", "3")))
+                .defaultOption(1);
+        Assertions.assertEquals("text1", test1.text());
+        Assertions.assertEquals(3, test1.options().size());
+        Assertions.assertEquals(1, test1.defaultOption());
+        Assertions.assertSame(test1, test1.addOption("4"));
+
+        ElementInput test2 = new ElementInput("test2", "placeholder", "defaultText")
+                .text("text2")
+                .placeholder("placeholder")
+                .defaultText("defaultText");
+        Assertions.assertEquals("text2", test2.text());
+        Assertions.assertEquals("placeholder", test2.placeholder());
+        Assertions.assertEquals("defaultText", test2.defaultText());
+
+        ElementLabel test3 = new ElementLabel()
+                .text("test3");
+        Assertions.assertEquals("text3", test3.text());
+
+        ElementSlider test4 = new ElementSlider()
+                .text("text4")
+                .min(0)
+                .max(100)
+                .step(1)
+                .defaultValue(50);
+        Assertions.assertEquals("text4", test4.text());
+        Assertions.assertEquals(0, test4.min());
+        Assertions.assertEquals(100, test4.max());
+        Assertions.assertEquals(1, test4.step());
+        Assertions.assertEquals(50, test4.defaultValue());
+
+        ElementStepSlider test5 = new ElementStepSlider()
+                .text("text5")
+                .steps(new ArrayList<>(List.of("step1", "step2")))
+                .defaultStep(1);
+        Assertions.assertEquals("text5", test5.text());
+        Assertions.assertEquals(2, test5.steps().size());
+        Assertions.assertEquals(1, test5.defaultStep());
+        Assertions.assertSame(test5, test5.addStep("step3"));
+
+        ElementToggle test6 = new ElementToggle()
+                .text("text6")
+                .defaultValue(true);
+        Assertions.assertEquals("text6", test6.text());
+        Assertions.assertTrue(test6.defaultValue());
+
+        CustomForm test = new CustomForm("test_CustomForm")
+                .elements(new ObjectArrayList<>(ObjectArrayList.of(
                         test1,
                         test2,
                         test3,
                         test4,
-                        test5,
-                        test6));
+                        test5)));
 
-        test.send(player, 1);
+        Assertions.assertSame(test, test.putMeta("test", true));
+
+        int currentElementSize = test.elements().size();
+        test.addElement(test6);
+        Assertions.assertEquals(currentElementSize + 1, test.elements().size());
+
+        CustomForm result1 = test.send(player, 1);
+        CustomForm result2 = test.send(player); // Won't send - player is already viewer
+        CustomForm result3 = test.sendUpdate(player);
+
         DataPacketManager dataPacketManager = player.getSession().getDataPacketManager();
         PlayerHandle playerHandle = new PlayerHandle(player);
 
@@ -66,16 +119,22 @@ public class FormTest {
                     @Override
                     public void handle(PlayerFormRespondedEvent event) {
                         CustomResponse response = (CustomResponse) event.getResponse();
+
                         ElementResponse dropdownResponse = response.getDropdownResponse(0);
                         Assertions.assertEquals("2", dropdownResponse.elementText());
+
                         String inputResponse = response.getInputResponse(1);
                         Assertions.assertEquals("input", inputResponse);
+
                         String labelResponse = response.getLabelResponse(2);
-                        Assertions.assertEquals("test3", labelResponse);
+                        Assertions.assertEquals("text3", labelResponse);
+
                         float sliderResponse = response.getSliderResponse(3);
                         Assertions.assertEquals(6, sliderResponse);
+
                         ElementResponse stepSliderResponse = response.getStepSliderResponse(4);
                         Assertions.assertEquals("step1", stepSliderResponse.elementText());
+
                         boolean toggleResponse = response.getToggleResponse(5);
                         Assertions.assertFalse(toggleResponse);
 
@@ -85,13 +144,32 @@ public class FormTest {
                         Int2ObjectOpenHashMap<Object> responses = response.getResponses();
                         Assertions.assertEquals(6, responses.size());
 
-                        Assertions.assertEquals("test", test.title());
+                        Assertions.assertEquals("test_CustomForm", test.title());
                         Assertions.assertEquals(test1, test.elements().toArray(Element[]::new)[0]);
                     }
                 }
         ));
 
+        AtomicBoolean submitCalled = new AtomicBoolean(false);
+        AtomicBoolean closeCalled = new AtomicBoolean(false);
+
+        CustomForm result4 = test.onSubmit((pl, response) -> submitCalled.set(true))
+                                 .onClose(pl -> closeCalled.set(true));
+
+        Assertions.assertAll(
+                () -> Assertions.assertSame(test, result1),
+                () -> Assertions.assertSame(test, result2),
+                () -> Assertions.assertSame(test, result3),
+                () -> Assertions.assertSame(test, result4)
+        );
+
         dataPacketManager.processPacket(playerHandle, modalFormResponsePacket);
+
+        Assertions.assertTrue(submitCalled.get(), "CustomForm: Submit consumer should be supplied");
+        Assertions.assertFalse(closeCalled.get(), "CustomForm: Close consumer shouldn't be supplied");
+
+        Assertions.assertTrue(test.getMeta("test", false), "CustomForm: Meta should contain variable 'test'");
+
         testPluginManager.resetAll();
     }
 
@@ -99,15 +177,45 @@ public class FormTest {
     @Test
     void test_FormWindowSimple(TestPlayer player, TestPluginManager testPluginManager) {
         testPluginManager.resetAll();
-        SimpleForm test = new SimpleForm("test_FormWindowSimple",
-                "123456"
-        );
 
-        test.addButton(new ElementButton("button1", ButtonImage.Type.PATH.of("textures/items/compass")))
-                .addButton("button2", ButtonImage.Type.URL.of("https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/94/Oak_Button_%28S%29_JE4.png"))
-                .addButton("button3");
+        SimpleForm constructorTest = new SimpleForm("test_SimpleForm");
+        Assertions.assertEquals("test_SimpleForm", constructorTest.title());
+        Assertions.assertEquals("", constructorTest.content());
 
-        test.send(player, 1);
+        SimpleForm res = constructorTest.title("");
+        Assertions.assertSame(constructorTest, res);
+
+        SimpleForm test = new SimpleForm("test_SimpleForm", "123456");
+
+        Assertions.assertSame(test, test.putMeta("test", true));
+
+        ElementButton test1 = new ElementButton("button1")
+                .image(ButtonImage.Type.PATH.of("textures/items/compass"));
+
+        ElementButton test2 = new ElementButton()
+                .text("button2");
+
+        Assertions.assertEquals("button2", test2.text());
+
+        AtomicBoolean button2Clicked = new AtomicBoolean(false);
+
+        test.addButton(test1)
+                .addButton(test2, pl -> button2Clicked.set(true))
+                .addButton("button3", ButtonImage.Type.URL.of("https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/94/Oak_Button_%28S%29_JE4.png"))
+                .addButton("button5")
+                .addButton("button5");
+
+        SimpleForm result1 = test.send(player, 1);
+        SimpleForm result2 = test.send(player); // Won't send - player is already viewer
+
+        ElementButton button4 = test.updateElement(3, new ElementButton("button4"));
+        Assertions.assertNotNull(button4);
+
+        int currentButtonSize = test.buttons().size();
+        test.removeElement(4);
+        Assertions.assertEquals(currentButtonSize - 1, test.buttons().size());
+
+        SimpleForm result3 = test.sendUpdate(player);
 
         DataPacketManager dataPacketManager = player.getSession().getDataPacketManager();
         PlayerHandle playerHandle = new PlayerHandle(player);
@@ -122,14 +230,16 @@ public class FormTest {
                     @Override
                     public void handle(PlayerFormRespondedEvent event) {
                         SimpleResponse response = ((SimpleResponse) event.getResponse());
+
                         ElementButton clickedButton = response.button();
-                        Assertions.assertEquals("button2", clickedButton.text());
                         int buttonId = response.buttonId();
+                        Assertions.assertEquals("button2", clickedButton.text());
                         Assertions.assertEquals(1, buttonId);
+
 
                         ElementButton[] buttons = test.buttons().keySet().toArray(ElementButton.EMPTY_LIST);
 
-                        Assertions.assertEquals("test_FormWindowSimple", test.title());
+                        Assertions.assertEquals("test_SimpleForm", test.title());
                         Assertions.assertEquals("button1", buttons[0].text());
                         Assertions.assertEquals("button3", buttons[2].text());
                         Assertions.assertEquals("textures/items/compass", buttons[0].image().data());
@@ -137,15 +247,59 @@ public class FormTest {
                 }
         ));
 
+        AtomicBoolean submitCalled = new AtomicBoolean(false);
+        AtomicBoolean closeCalled = new AtomicBoolean(false);
+
+        SimpleForm result4 = test.onSubmit((pl, response) -> submitCalled.set(true))
+                                 .onClose(pl -> closeCalled.set(true));
+
+        Assertions.assertAll(
+                () -> Assertions.assertSame(test, result1),
+                () -> Assertions.assertSame(test, result2),
+                () -> Assertions.assertSame(test, result3),
+                () -> Assertions.assertSame(test, result4)
+        );
+
         dataPacketManager.processPacket(playerHandle, modalFormResponsePacket);
+
+        Assertions.assertTrue(submitCalled.get(), "SimpleForm: Submit consumer should be supplied");
+        Assertions.assertFalse(closeCalled.get(), "SimpleForm: Close consumer shouldn't be supplied");
+        Assertions.assertTrue(button2Clicked.get(), "SimpleForm: Consumer of button2 should be supplied");
+
+        Assertions.assertTrue(test.getMeta("test", false), "SimpleForm: Meta should contain variable 'test'");
+
         testPluginManager.resetAll();
     }
 
     @Test
     void test_FormWindowModal(TestPlayer player, TestPluginManager testPluginManager) {
         testPluginManager.resetAll();
-        ModalForm test = new ModalForm("test_FormWindowModal").content("1028346237");
-        test.send(player, 1);
+
+        ModalForm constructorTest = new ModalForm("test_ModalForm");
+        Assertions.assertEquals("test_ModalForm", constructorTest.title());
+        Assertions.assertEquals("", constructorTest.content());
+
+        ModalForm res = constructorTest.title("");
+        Assertions.assertSame(constructorTest, res);
+
+        ModalForm test = new ModalForm("test_ModalForm", "1028346237");
+
+        Assertions.assertSame(test, test.putMeta("test", true));
+
+        AtomicBoolean yesCalled = new AtomicBoolean(false);
+        AtomicBoolean noCalled = new AtomicBoolean(false);
+
+        test.yes("yes", pl -> noCalled.set(true));
+        test.no("no", pl -> noCalled.set(true));
+
+        ModalForm result1 = test.send(player, 1);
+        ModalForm result2 = test.send(player); // Won't send - player is already viewer
+
+        ModalForm result3 = test.text("Yes!", "No!");
+        ModalForm result4 = test.onNo(pl -> {})
+                .onYes(pl -> yesCalled.set(true));
+
+        ModalForm result5 = test.sendUpdate(player);
 
         DataPacketManager dataPacketManager = player.getSession().getDataPacketManager();
         PlayerHandle playerHandle = new PlayerHandle(player);
@@ -165,13 +319,36 @@ public class FormTest {
                         boolean yes = response.yes();
                         Assertions.assertFalse(yes);
 
-                        Assertions.assertEquals("test_FormWindowModal", test.title());
+                        Assertions.assertEquals("test_ModalForm", test.title());
                         Assertions.assertEquals("1028346237", test.content());
                     }
                 }
         ));
 
+        AtomicBoolean submitCalled = new AtomicBoolean(false);
+        AtomicBoolean closeCalled = new AtomicBoolean(false);
+
+        ModalForm result6 = test.onSubmit((pl, response) -> submitCalled.set(true))
+                                .onClose(pl -> closeCalled.set(true));
+
+        Assertions.assertAll(
+                () -> Assertions.assertSame(test, result1),
+                () -> Assertions.assertSame(test, result2),
+                () -> Assertions.assertSame(test, result3),
+                () -> Assertions.assertSame(test, result4),
+                () -> Assertions.assertSame(test, result5),
+                () -> Assertions.assertSame(test, result6)
+        );
+
         dataPacketManager.processPacket(playerHandle, modalFormResponsePacket);
+
+        Assertions.assertTrue(submitCalled.get(), "ModalForm: Submit consumer should be supplied");
+        Assertions.assertFalse(closeCalled.get(), "ModalForm: Close consumer shouldn't be supplied");
+        Assertions.assertFalse(yesCalled.get(), "ModalForm: Yes consumer shouldn't be supplied");
+        Assertions.assertTrue(noCalled.get(), "ModalForm: No consumer should be supplied");
+
+        Assertions.assertTrue(test.getMeta("test", false), "ModalForm: Meta should contain variable 'test'");
+
         testPluginManager.resetAll();
     }
 }
