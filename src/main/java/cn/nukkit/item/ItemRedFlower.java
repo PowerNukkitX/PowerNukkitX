8c06d1a401de3019546790d49dd195a5436d820a
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-03-07 19:06:16 +0100

    Fixed that "red_flower" cannot be placed

--- src/main/java/cn/nukkit/item/ItemRedFlower.java
+++ src/main/java/cn/nukkit/item/ItemRedFlower.java
@@ -1,7 +1,12 @@
 package cn.nukkit.item;
 
+import cn.nukkit.block.Block;
+import cn.nukkit.block.BlockID;
+
 public class ItemRedFlower extends Item {
+
     public ItemRedFlower() {
         super(RED_FLOWER);
+        this.block = Block.get(BlockID.POPPY);
     }
 }
