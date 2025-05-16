58476a59e159e9c75aecaafc1a7885d390f1879d
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-05-16 12:21:45 +0200

    Made the chunk load ticking async

--- src/main/java/cn/nukkit/config/category/ChunkSettings.java
+++ src/main/java/cn/nukkit/config/category/ChunkSettings.java
@@ -26,4 +26,6 @@ public class ChunkSettings extends OkaeriConfig {
     boolean clearTickList = false;
     @Comment("pnx.settings.chunk.generationqueuesize")
     int generationQueueSize = 128;
+    @Comment("pnx.settings.chunk.tickOnLoad")
+    boolean tickOnLoad = true;
 }
