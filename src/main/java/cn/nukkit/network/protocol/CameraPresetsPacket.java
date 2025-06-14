d00ee168c8f7248ad56309b36297247c2df1f4e6
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-06-14 18:55:54 +0200

    Removed alignTargetAndCameraForward

--- src/main/java/cn/nukkit/network/protocol/CameraPresetsPacket.java
+++ src/main/java/cn/nukkit/network/protocol/CameraPresetsPacket.java
@@ -53,7 +53,6 @@ public class CameraPresetsPacket extends DataPacket {
         byteBuf.writeNotNull(preset.getYawLimitMax(), byteBuf::writeFloatLE);
         byteBuf.writeNotNull(preset.getListener(), (l) -> byteBuf.writeByte((byte) l.ordinal()));
         byteBuf.writeOptional(preset.getPlayEffect(), byteBuf::writeBoolean);
-        byteBuf.writeOptional(preset.getAlignTargetAndCameraForward(), byteBuf::writeBoolean);
         writeCameraPresetAimAssist(byteBuf, preset.getAimAssist());
         byteBuf.writeBoolean(false);
     }
