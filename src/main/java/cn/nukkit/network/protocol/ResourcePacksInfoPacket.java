24ff5e26fc5e7ff7d8533d75a797a8da96afd679
staticius <56194666+staticius@users.noreply.github.com>
2025-07-06 09:23:41 +0300

    Fix: Resource pack encoding & refactor comments

--- src/main/java/cn/nukkit/network/protocol/ResourcePacksInfoPacket.java
+++ src/main/java/cn/nukkit/network/protocol/ResourcePacksInfoPacket.java
@@ -33,9 +33,6 @@ public class ResourcePacksInfoPacket extends DataPacket {
      */
     public String worldTemplateVersion;
 
-    public boolean forceDisableVibrantVisuals;
-
-
     public ResourcePack[] resourcePackEntries = ResourcePack.EMPTY_ARRAY;
 
     @Override
