b558099806b2cff9a57a273384c57b9d1c603e98
Curse <anderson@lalumail.com>
2025-08-19 20:38:48 +0200

    Initial commit, added support for dynamicProperties on items

--- src/main/java/cn/nukkit/nbt/tag/FloatTag.java
+++ src/main/java/cn/nukkit/nbt/tag/FloatTag.java
@@ -14,6 +14,18 @@ public class FloatTag extends NumberTag<Float> {
         this.data = data;
     }
 
+    public FloatTag(String name) {
+        this.data = 0f;
+    }
+
+    public FloatTag(String name, float data) {
+        this.data = data;
+    }
+
+    public FloatTag(String name, double data) {
+        this.data = (float) data;
+    }
+
     @Override
     public Float getData() {
         return data;
