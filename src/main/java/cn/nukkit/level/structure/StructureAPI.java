493747d0bab1211064fa5848094b47fddbee85de
tunikakeks <keksdev9513@gmail.com>
2025-09-09 00:16:41 +0200

    structure caching (optional)

--- src/main/java/cn/nukkit/level/structure/StructureAPI.java
+++ src/main/java/cn/nukkit/level/structure/StructureAPI.java
@@ -8,14 +8,27 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.ByteOrder;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class StructureAPI {
+    private static Map<String, Structure> structureCache = new HashMap<>();
+
     public static Structure load(String name) {
+        if (structureCache.containsKey(name)) {
+            return structureCache.get(name);
+        }
+
         try (var stream = new FileInputStream(new File(Server.getInstance().structurePath + name + ".mcstructure"))) {
             CompoundTag root = NBTIO.read(stream, ByteOrder.LITTLE_ENDIAN);
 
-            return Structure.fromNbtAsync(root).join();
+            Structure structure = Structure.fromNbtAsync(root).join();
+
+            if(Server.getInstance().getSettings().gameplaySettings().cacheStructures())
+                structureCache.put(name, structure);
+
+            return structure;
         } catch (Exception e) {
             log.debug("Cannot load structure " + name);
             return null;
@@ -27,6 +40,9 @@ public class StructureAPI {
             File file = new File(Server.getInstance().structurePath + name + ".mcstructure");
             file.getParentFile().mkdirs();
             NBTIO.write(structure.toNBT(), file, ByteOrder.LITTLE_ENDIAN);
+
+            if(Server.getInstance().getSettings().gameplaySettings().cacheStructures())
+                structureCache.put(name, structure);
         } catch (Exception e) {
             log.error("Cannot save structure " + name);
             log.debug("Cannot save structure " + name, e);
@@ -38,6 +54,8 @@ public class StructureAPI {
     }
 
     public static boolean delete(String name) {
+        structureCache.remove(name);
+
         File file = new File(Server.getInstance().structurePath + name + ".mcstructure");
         if (file.exists()) {
             return file.delete();
