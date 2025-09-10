398c65003f13755cfb5af9b81821856847c02395
Curse <anderson@lalumail.com>
2025-09-10 17:47:04 +0200

    Initial Commit

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -2704,7 +2704,7 @@ public class Player extends EntityHuman implements CommandSender, ChunkLoader, I
 
             this.entityBaseTick(tickDiff);
 
-            if (this.getServer().getDifficulty() == 0 && this.level.getGameRules().getBoolean(GameRule.NATURAL_REGENERATION)) {
+            if (this.getServer().getDifficulty() == 0 || this.level.getGameRules().getBoolean(GameRule.NATURAL_REGENERATION)) {
                 if (this.getHealth() < this.getMaxHealth() && this.ticksLived % 20 == 0) {
                     this.heal(1);
                 }
