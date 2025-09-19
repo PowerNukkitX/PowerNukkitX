6ff6c8ddb3c8537d20069b6811170ff37ea1cc8e
KeksDev <54219265+tunikakeks@users.noreply.github.com>
2025-09-19 11:56:17 +0200

    Fix GZIP NBTIO output & add PNX Structure format (#2097)

--- src/main/java/cn/nukkit/level/structure/AbstractStructure.java
+++ src/main/java/cn/nukkit/level/structure/AbstractStructure.java
@@ -14,16 +14,26 @@ import java.util.concurrent.CompletableFuture;
 public abstract class AbstractStructure {
     protected static final BlockState STATE_AIR = BlockAir.PROPERTIES.getDefaultState();
     protected static final BlockState STATE_UNKNOWN = BlockUnknown.PROPERTIES.getDefaultState();
+    protected String name;
 
     public static AbstractStructure fromNbt(CompoundTag tag) {
         if(tag.contains("format_version")) return Structure.fromNbtAsync(tag).join();
-        if(tag.contains("DataVersion")) return JeStructure.fromNbt(tag);
-
+        if(tag.contains("PNX")) return PNXStructure.fromNbtAsync(tag).join();
+        if(tag.contains("DataVersion")) return JeStructure.fromNbtAsync(tag).join();
         return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public abstract CompoundTag toNBT();
     public abstract void place(Position pos, boolean includeEntities, BlockManager blockManager);
+    public abstract void preparePlace(Position pos, BlockManager blockManager);
     public abstract AbstractStructure rotate(StructureRotation rotation);
     public abstract AbstractStructure mirror(StructureMirror mirror);
 
