PowerNukkitX/PowerNukkitX

commit b124905faa34a9c1bb9fdc0d44736e67766cf96b

Protocol Changes for 1.21.110 (#2104)
* Beginning on R21U11 (WIP)

* more changes for R21U11

* improved deprecation details

* Final (required to be able to join) packet changes

* helper script

* add new blocks & items

* temp fix

* shelf, temporary blockstate file, itemupdater 1.21.110

* changes

* Fixed chain item translator

---------

Co-authored-by: tunikakeks <keksdev9513@gmail.com>
Co-authored-by: PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
Co-authored-by: LPMC Dev <admin@pycmc.eu>

src/main/java/cn/nukkit/inventory/HumanInventory.java
@@ -18,12 +18,15 @@ import cn.nukkit.network.protocol.InventorySlotPacket;
 import cn.nukkit.network.protocol.MobArmorEquipmentPacket;
 import cn.nukkit.network.protocol.MobEquipmentPacket;
 import cn.nukkit.network.protocol.PlayerArmorDamagePacket;
+import cn.nukkit.network.protocol.types.inventory.ArmorSlot;
+import cn.nukkit.network.protocol.types.inventory.ArmorSlotAndDamagePair;
 import cn.nukkit.network.protocol.types.inventory.FullContainerName;
 import cn.nukkit.network.protocol.types.itemstack.ContainerSlotType;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
+import org.checkerframework.checker.units.qual.A;
 import org.jetbrains.annotations.Range;
 
 import java.util.ArrayList;
@@ -506,10 +509,11 @@ public class HumanInventory extends BaseInventory {
                 for (int i = 0; i < 4; ++i) {
                     Item item = armor[i];
                     if (item.isNull()) {
-                        pk2.damage[i] = 0;
+                        pk2.getArmorSlotAndDamagePairs().add(
+                                new ArmorSlotAndDamagePair(ArmorSlot.from(i), (short) 0)
+                        );
                     } else {
-                        pk2.flags.add(PlayerArmorDamagePacket.PlayerArmorDamageFlag.values()[i]);
-                        pk2.damage[i] = item.getDamage();
+                        pk2.getArmorSlotAndDamagePairs().add(new ArmorSlotAndDamagePair(ArmorSlot.from(i), (short) item.getDamage()));
                     }
                 }
                 player.dataPacket(pk2);
@@ -592,10 +596,13 @@ public class HumanInventory extends BaseInventory {
                 PlayerArmorDamagePacket pk2 = new PlayerArmorDamagePacket();
                 Item item = armor[index];
                 if (item.isNull()) {
-                    pk2.damage[index] = 0;
+                    pk2.getArmorSlotAndDamagePairs().add(
+                            new ArmorSlotAndDamagePair(ArmorSlot.from(index), (short) 0)
+                    );
                 } else {
-                    pk2.flags.add(PlayerArmorDamagePacket.PlayerArmorDamageFlag.values()[index]);
-                    pk2.damage[index] = item.getDamage();
+                    pk2.getArmorSlotAndDamagePairs().add(
+                            new ArmorSlotAndDamagePair(ArmorSlot.from(index), (short) item.getDamage())
+                    );
                 }
                 player.dataPacket(pk2);
             } else {

commit 0821f6aaef0fdeea9737551cb9b6fc3b0814d991

fix: armor damage slot mapping (0-3 -> protocol IDs 0,2,4,6); resolves UI bar shift regression from #2104 (#2120)
Initial Commit

src/main/java/cn/nukkit/inventory/HumanInventory.java
@@ -26,7 +26,6 @@ import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
-import org.checkerframework.checker.units.qual.A;
 import org.jetbrains.annotations.Range;
 
 import java.util.ArrayList;
@@ -37,11 +36,6 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 0-8 物品栏<br>
- * 9-35 背包<br>
- * 36-39 盔甲栏<br>
- * 想获取副手库存请用{@link HumanOffHandInventory}<br>
- * <p>
  * 0-8 hotbar<br>
  * 9-35 inventory<br>
  * 36-39 Armor Inventory<br>
@@ -249,17 +243,17 @@ public class HumanInventory extends BaseInventory {
             }
         }
 
-        //使用FastUtils的IntArrayList提高性能
+        // Improve performance using IntArrayList of FastUtils
         IntList emptySlots = new IntArrayList(this.getSize());
 
         for (int i = 0; i < ARMORS_INDEX; ++i) {
-            //获取未克隆Item对象
+            // Get the uncloned Item object
             Item item = this.getUnclonedItem(i);
             if (item.isNull() || item.getCount() <= 0) {
                 emptySlots.add(i);
             }
 
-            //使用迭代器而不是新建一个ArrayList
+            // Use iterators instead of creating a new ArrayList
             for (Iterator<Item> iterator = itemSlots.iterator(); iterator.hasNext(); ) {
                 Item slot = iterator.next();
                 if (slot.equals(item)) {
@@ -268,7 +262,7 @@ public class HumanInventory extends BaseInventory {
                         int amount = Math.min(maxStackSize - item.getCount(), slot.getCount());
                         amount = Math.min(amount, this.getMaxStackSize());
                         if (amount > 0) {
-                            //在需要clone时再clone
+                            // Clone when needed
                             item = item.clone();
                             slot.setCount(slot.getCount() - amount);
                             item.setCount(item.getCount() + amount);
@@ -508,13 +502,8 @@ public class HumanInventory extends BaseInventory {
                 PlayerArmorDamagePacket pk2 = new PlayerArmorDamagePacket();
                 for (int i = 0; i < 4; ++i) {
                     Item item = armor[i];
-                    if (item.isNull()) {
-                        pk2.getArmorSlotAndDamagePairs().add(
-                                new ArmorSlotAndDamagePair(ArmorSlot.from(i), (short) 0)
-                        );
-                    } else {
-                        pk2.getArmorSlotAndDamagePairs().add(new ArmorSlotAndDamagePair(ArmorSlot.from(i), (short) item.getDamage()));
-                    }
+                    short dmg = item.isNull() ? (short) 0 : (short) item.getDamage();
+                    pk2.getArmorSlotAndDamagePairs().add(new ArmorSlotAndDamagePair(armorSlotOfIndex(i), dmg));
                 }
                 player.dataPacket(pk2);
             } else {
@@ -595,15 +584,8 @@ public class HumanInventory extends BaseInventory {
 
                 PlayerArmorDamagePacket pk2 = new PlayerArmorDamagePacket();
                 Item item = armor[index];
-                if (item.isNull()) {
-                    pk2.getArmorSlotAndDamagePairs().add(
-                            new ArmorSlotAndDamagePair(ArmorSlot.from(index), (short) 0)
-                    );
-                } else {
-                    pk2.getArmorSlotAndDamagePairs().add(
-                            new ArmorSlotAndDamagePair(ArmorSlot.from(index), (short) item.getDamage())
-                    );
-                }
+                short dmg = item.isNull() ? (short) 0 : (short) item.getDamage();
+                pk2.getArmorSlotAndDamagePairs().add(new ArmorSlotAndDamagePair(armorSlotOfIndex(index), dmg));
                 player.dataPacket(pk2);
             } else {
                 player.dataPacket(pk);
@@ -717,5 +699,9 @@ public class HumanInventory extends BaseInventory {
         if (who != holder) {
             super.onClose(who);
         }
+    }
+
+    private static ArmorSlot armorSlotOfIndex(int index) {
+        return ArmorSlot.from(index * 2);
     }
 }