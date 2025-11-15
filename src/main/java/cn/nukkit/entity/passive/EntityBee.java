670ec306c96130c9744111b71f925ea8efe1acfd
Curse <anderson@lalumail.com>
2025-11-15 21:37:09 +0100

    Initial Commit

--- src/main/java/cn/nukkit/entity/passive/EntityBee.java
+++ src/main/java/cn/nukkit/entity/passive/EntityBee.java
@@ -40,7 +40,7 @@ public class EntityBee extends EntityAnimal implements EntityFlyable {
     public static final EntityProperty[] PROPERTIES = new EntityProperty[]{
         new BooleanEntityProperty("minecraft:has_nectar", false, true)
     };
-    private final static String PROPERTY_HAS_NECTAR = "minecraft:has_nectar";
+    private static final String PROPERTY_HAS_NECTAR = "minecraft:has_nectar";
 
     @Override
     @NotNull public String getIdentifier() {
@@ -51,8 +51,43 @@ public class EntityBee extends EntityAnimal implements EntityFlyable {
 
     public int dieInTicks = -1;
 
+    private static final int NO_HOME = Integer.MIN_VALUE;
+    private int homeHiveX = NO_HOME;
+    private int homeHiveY = NO_HOME;
+    private int homeHiveZ = NO_HOME;
+
+    // Anti-stuck tracking for movement targets
+    private int stuckTicksOnTarget = 0;
+    private int lastTargetX = Integer.MIN_VALUE;
+    private int lastTargetY = Integer.MIN_VALUE;
+    private int lastTargetZ = Integer.MIN_VALUE;
+
     public EntityBee(IChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
+    }
+
+    public boolean hasHomeHive() {
+        return this.homeHiveY != NO_HOME;
+    }
+
+    public void setHomeHive(BlockEntityBeehive beehive) {
+        if (beehive == null || beehive.getBlock() == null) {
+            homeHiveX = homeHiveY = homeHiveZ = NO_HOME;
+            return;
+        }
+        Block block = beehive.getBlock();
+        this.homeHiveX = block.getFloorX();
+        this.homeHiveY = block.getFloorY();
+        this.homeHiveZ = block.getFloorZ();
+    }
+
+    public Block getHomeHiveBlock() {
+        if (!hasHomeHive() || this.level == null) return null;
+        Block block = this.level.getBlock(homeHiveX, homeHiveY, homeHiveZ);
+        if (!(block instanceof BlockBeehive)) {
+            return null;
+        }
+        return block;
     }
 
     @Override
@@ -61,16 +96,30 @@ public class EntityBee extends EntityAnimal implements EntityFlyable {
                 this.tickSpread,
                 Set.of(),
                 Set.of(
-                        new Behavior(new BeeAttackExecutor(CoreMemoryTypes.ATTACK_TARGET, 0.7f, 33, true, 20),
+                        new Behavior(
+                                new BeeAttackExecutor(CoreMemoryTypes.ATTACK_TARGET, 0.7f, 33, true, 20),
                                 all(
                                         new EntityCheckEvaluator(CoreMemoryTypes.ATTACK_TARGET),
                                         entity -> hasSting()
-                                ), 6, 1),
-                        new Behavior(new MoveToTargetExecutor(CoreMemoryTypes.NEAREST_BLOCK, 0.22f, true), new MemoryCheckNotEmptyEvaluator(CoreMemoryTypes.NEAREST_BLOCK), 4, 1),
-                        new Behavior(new SpaceRandomRoamExecutor(0.15f, 12, 100, 20, false, -1, true, 10), (entity -> true), 1, 1)
+                                ),
+                                6,
+                                1
+                        ),
+                        new Behavior(
+                                new MoveToTargetExecutor(CoreMemoryTypes.NEAREST_BLOCK, 0.22f, true),
+                                new MemoryCheckNotEmptyEvaluator(CoreMemoryTypes.NEAREST_BLOCK),
+                                4,
+                                1
+                        ),
+                        new Behavior(
+                                new SpaceRandomRoamExecutor(0.15f, 12, 100, 20, false, -1, true, 10),
+                                (entity -> true),
+                                1,
+                                1
+                        )
                 ),
                 Set.of(
-                        new MemorizedBlockSensor(20, 5, 20)
+                        new MemorizedBlockSensor(22, 6, 20)
                 ),
                 Set.of(new SpaceMoveController(), new LookController(true, true), new LiftController()),
                 new SimpleSpaceAStarRouteFinder(new FlyingPosEvaluator(), this),
@@ -85,7 +134,7 @@ public class EntityBee extends EntityAnimal implements EntityFlyable {
     public boolean hasNectar() {
         return this.getBooleanEntityProperty(PROPERTY_HAS_NECTAR);
     }
-    
+
     public void setNectar(boolean hasNectar) {
         this.setBooleanEntityProperty(PROPERTY_HAS_NECTAR, hasNectar);
     }
@@ -122,15 +171,15 @@ public class EntityBee extends EntityAnimal implements EntityFlyable {
 
     @Override
     public boolean attack(EntityDamageEvent source) {
-        if(source.getCause() == EntityDamageEvent.DamageCause.SUFFOCATION) {
-            if(ticksLived < 10) {
+        if (source.getCause() == EntityDamageEvent.DamageCause.SUFFOCATION) {
+            if (ticksLived < 10) {
                 source.setCancelled();
                 return false;
             }
         }
-        if(source instanceof EntityDamageByEntityEvent event) {
-            for(Entity entity : getLevel().getCollidingEntities(this.getBoundingBox().grow(4, 4, 4))) {
-                if(entity instanceof EntityBee bee && bee.hasSting()) {
+        if (source instanceof EntityDamageByEntityEvent event) {
+            for (Entity entity : getLevel().getCollidingEntities(this.getBoundingBox().grow(4, 4, 4))) {
+                if (entity instanceof EntityBee bee && bee.hasSting()) {
                     bee.setAngry(event.getDamager());
                 }
             }
@@ -140,33 +189,89 @@ public class EntityBee extends EntityAnimal implements EntityFlyable {
 
     @Override
     public boolean onUpdate(int currentTick) {
-        if(!hasSting() && isAlive()) {
+        if (!hasSting() && isAlive()) {
             dieInTicks--;
-            if(dieInTicks < 0) {
+            if (dieInTicks < 0) {
                 kill();
             }
         }
-        if(currentTick % 20 == 0 && hasSting()) {
-            getMemoryStorage().put(CoreMemoryTypes.LOOKING_BLOCK, shouldSearchBeehive() ? BlockBeehive.class : BlockFlower.class);
+
+        if (currentTick % 20 == 0 && hasSting()) {
+            boolean searchHive = shouldSearchBeehive();
+
+            // Search a hive and know home, steer towards it first
+            if (searchHive) {
+                Block homeHive = getHomeHiveBlock();
+                if (homeHive != null) {
+                    double dx = (homeHive.getFloorX() + 0.5) - this.x;
+                    double dy = (homeHive.getFloorY() + 0.5) - this.y;
+                    double dz = (homeHive.getFloorZ() + 0.5) - this.z;
+                    double distSq = dx * dx + dy * dy + dz * dz;
+
+                    // If home hive is too far, forget it so bee can re-home
+                    if (distSq > 128 * 128) {
+                        this.homeHiveX = this.homeHiveY = this.homeHiveZ = NO_HOME;
+                    } else {
+                        // Push home hive into NEAREST_BLOCK so MoveToTarget can start flying back
+                        this.getMemoryStorage().put(CoreMemoryTypes.NEAREST_BLOCK, homeHive);
+                    }
+                }
+            }
+
+            getMemoryStorage().put(CoreMemoryTypes.LOOKING_BLOCK, searchHive ? BlockBeehive.class : BlockFlower.class);
             Class<? extends Block> blockClass = this.getMemoryStorage().get(CoreMemoryTypes.LOOKING_BLOCK);
+            Block nearestBlock = this.getMemoryStorage().get(CoreMemoryTypes.NEAREST_BLOCK);
+
+            // Anti-stuck: if we chase the same target too long, clear it and let sensors pick another
+            if (nearestBlock != null && blockClass != null) {
+                int tx = nearestBlock.getFloorX();
+                int ty = nearestBlock.getFloorY();
+                int tz = nearestBlock.getFloorZ();
+
+                if (tx == lastTargetX && ty == lastTargetY && tz == lastTargetZ) {
+                    stuckTicksOnTarget += 20;
+                } else {
+                    lastTargetX = tx;
+                    lastTargetY = ty;
+                    lastTargetZ = tz;
+                    stuckTicksOnTarget = 0;
+                }
+
+                int timeoutTicks = blockClass.isAssignableFrom(BlockFlower.class) ? 200 : 400;
+                if (stuckTicksOnTarget > timeoutTicks) {
+                    this.getMemoryStorage().clear(CoreMemoryTypes.NEAREST_BLOCK);
+                    stuckTicksOnTarget = 0;
+                    lastTargetX = lastTargetY = lastTargetZ = Integer.MIN_VALUE;
+                }
+            } else {
+                stuckTicksOnTarget = 0;
+                lastTargetX = lastTargetY = lastTargetZ = Integer.MIN_VALUE;
+            }
+
+            if (blockClass == null) {
+                return super.onUpdate(currentTick);
+            }
+
             if (blockClass.isAssignableFrom(BlockFlower.class)) {
                 Arrays.stream(level.getCollisionBlocks(getBoundingBox().grow(1.5, 1.5, 1.5), false, true))
                         .filter(block -> block instanceof BlockFlower)
                         .findAny().ifPresent(flower -> {
-                            if(flower instanceof BlockWitherRose) {
+                            if (flower instanceof BlockWitherRose) {
                                 this.kill();
-                            } else if(stayAtFlower) {
+                            } else if (stayAtFlower) {
                                 this.setNectar(true);
                                 this.getLevel().addSound(this, Sound.MOB_BEE_POLLINATE);
                             }
                             stayAtFlower = !stayAtFlower;
                         });
-            } else if(blockClass.isAssignableFrom(BlockBeehive.class)) {
+            } else if (blockClass.isAssignableFrom(BlockBeehive.class)) {
                 Arrays.stream(level.getCollisionBlocks(getBoundingBox().grow(1.5, 1.5, 1.5), false, true))
                         .filter(block -> {
-                            if(block instanceof BlockBeehive hive) {
+                            if (block instanceof BlockBeehive hive) {
                                 BlockEntityBeehive hiveEntity = hive.getOrCreateBlockEntity();
-                                return !hiveEntity.isHoneyFull() && hiveEntity.getOccupantsCount() < 3;
+                                int occupants = hiveEntity.getOccupantsCount();
+                                boolean canEnter = occupants < 3;
+                                return canEnter;
                             }
                             return false;
                         })
@@ -183,14 +288,40 @@ public class EntityBee extends EntityAnimal implements EntityFlyable {
     protected void initEntity() {
         this.setMaxHealth(10);
         super.initEntity();
+
+        if (this.namedTag.contains("HomeHiveX")) {
+            this.homeHiveX = this.namedTag.getInt("HomeHiveX");
+            this.homeHiveY = this.namedTag.getInt("HomeHiveY");
+            this.homeHiveZ = this.namedTag.getInt("HomeHiveZ");
+        }
+    }
+
+    @Override
+    public void saveNBT() {
+        super.saveNBT();
+
+        if (hasHomeHive()) {
+            this.namedTag.putInt("HomeHiveX", homeHiveX);
+            this.namedTag.putInt("HomeHiveY", homeHiveY);
+            this.namedTag.putInt("HomeHiveZ", homeHiveZ);
+        } else {
+            this.namedTag.remove("HomeHiveX");
+            this.namedTag.remove("HomeHiveY");
+            this.namedTag.remove("HomeHiveZ");
+        }
     }
 
     public void nectarDelivered(BlockEntityBeehive blockEntityBeehive) {
         this.setNectar(false);
+        if (blockEntityBeehive != null) {
+            setHomeHive(blockEntityBeehive);
+        }
     }
 
     public void leftBeehive(BlockEntityBeehive blockEntityBeehive) {
-
+        if (blockEntityBeehive != null) {
+            setHomeHive(blockEntityBeehive);
+        }
     }
 
     public boolean shouldSearchBeehive() {
