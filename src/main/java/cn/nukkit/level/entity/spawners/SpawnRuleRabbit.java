50290fbc81dbf27906b2df11f590acbb38701695
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-11-20 14:53:09 +0100

    Fixed rabbit spawning (#2157)

--- src/main/java/cn/nukkit/level/entity/spawners/SpawnRuleRabbit.java
+++ src/main/java/cn/nukkit/level/entity/spawners/SpawnRuleRabbit.java
@@ -3,6 +3,7 @@ package cn.nukkit.level.entity.spawners;
 import cn.nukkit.block.BlockID;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.passive.EntityAnimal;
+import cn.nukkit.entity.passive.EntityRabbit;
 import cn.nukkit.level.entity.condition.*;
 import cn.nukkit.tags.BiomeTags;
 import cn.nukkit.tags.BlockTags;
@@ -14,8 +15,8 @@ public class SpawnRuleRabbit extends MultiSpawnRule {
         super(new Condition[]{
                 new ConditionInAir(),
                 new ConditionSpawnOnBlockFilter(Utils.concatArray(BlockTags.getBlockSet(BlockTags.GRASS).toArray(String[]::new),
-                                                                  BlockTags.getBlockSet(BlockTags.SAND).toArray(String[]::new),
-                                                                  new String[] {BlockID.SNOW, BlockID.SNOW_LAYER})),
+                        BlockTags.getBlockSet(BlockTags.SAND).toArray(String[]::new),
+                        new String[] {BlockID.SNOW, BlockID.SNOW_LAYER})),
                 new ConditionSpawnOnGround(),
         }, new SpawnRuleRabbitTaiga(), new SpawnRuleRabbitMeadow());
     }
@@ -28,10 +29,10 @@ public class SpawnRuleRabbit extends MultiSpawnRule {
                             new ConditionBiomeFilter(BiomeTags.FLOWER_FOREST, BiomeTags.GROVE, BiomeTags.SNOWY_SLOPES, BiomeTags.DESERT),
                             new ConditionAll(
                                     new ConditionBiomeFilter(BiomeTags.TAIGA),
-                                    new ConditionNot(new ConditionBiomeFilter(BiomeTags.MEGA)),
-                                    new ConditionPopulationControl(EntityAnimal.class, new int[]{3, 0, 0})
+                                    new ConditionNot(new ConditionBiomeFilter(BiomeTags.MEGA))
                             )
-                    )
+                    ),
+                    new ConditionPopulationControl(EntityAnimal.class, new int[]{3, 0, 0})
             );
         }
     }
