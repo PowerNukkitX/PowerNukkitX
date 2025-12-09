5520656dc23124f3b41410191c83ded5e0cb3ddd
tunikakeks <54219265+tunikakeks@users.noreply.github.com>
2025-12-09 18:41:54 +0100

    1.21.130 Support (#2180)
    * Protocol Update for 897
    
    * Updated custom blocks collision_box format to 1.21.130 and added support for multiple collision_box
    
    * Updated CustomBlockUtils and CollisionBox to match new collision_box format starting at 1.21.130
    
    * Added DOLPHIN_BOOST movement effect type.
    
    * Fixed message not set
    
    * Changed protocol version
    
    ---------
    
    Co-authored-by: Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
    Co-authored-by: Curse <anderson@lalumail.com>

--- src/main/java/cn/nukkit/network/protocol/TextPacket.java
+++ src/main/java/cn/nukkit/network/protocol/TextPacket.java
@@ -1,7 +1,10 @@
 package cn.nukkit.network.protocol;
 
 import cn.nukkit.network.connection.util.HandleByteBuf;
+import cn.nukkit.utils.OptionalValue;
 import io.netty.util.internal.EmptyArrays;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
 import lombok.*;
 
 /**
@@ -25,6 +28,8 @@ public class TextPacket extends DataPacket {
     public static final byte TYPE_ANNOUNCEMENT = 8;
     public static final byte TYPE_OBJECT = 9;
     public static final byte TYPE_OBJECT_WHISPER = 10;
+    public static final byte TYPE_OBJECT_ANNOUNCEMENT = 11;
+
 
     public byte type;
     public String source = "";
@@ -39,61 +44,85 @@ public class TextPacket extends DataPacket {
     public String filteredMessage = "";
     @Override
     public void decode(HandleByteBuf byteBuf) {
-        this.type = byteBuf.readByte();
-        this.isLocalized = byteBuf.readBoolean() || type == TYPE_TRANSLATION;
-        switch (type) {
-            case TYPE_CHAT:
-            case TYPE_WHISPER:
-            case TYPE_ANNOUNCEMENT:
-                this.source = byteBuf.readString();
-            case TYPE_RAW:
-            case TYPE_TIP:
-            case TYPE_SYSTEM:
-            case TYPE_OBJECT:
-            case TYPE_OBJECT_WHISPER:
+        this.isLocalized = byteBuf.readBoolean();
+        switch (byteBuf.readByte()) {
+            case 0 -> {
+                for (int i = 0; i < 6; i++) {
+                    byteBuf.readString();
+                }
+                this.type = (byte) byteBuf.readUnsignedByte();
                 this.message = byteBuf.readString();
-                break;
-
-            case TYPE_TRANSLATION:
-            case TYPE_POPUP:
-            case TYPE_JUKEBOX_POPUP:
+            }
+            case 1 -> {
+                for (int i = 0; i < 3; i++) {
+                    byteBuf.readString();
+                }
+                this.type = (byte) byteBuf.readUnsignedByte();
+                this.source = byteBuf.readString();
+                this.message = byteBuf.readString();
+            }
+            case 2 -> {
+                for (int i = 0; i < 3; i++) {
+                    byteBuf.readString();
+                }
+                this.type = (byte) byteBuf.readUnsignedByte();
                 this.message = byteBuf.readString();
                 this.parameters = byteBuf.readArray(String.class, HandleByteBuf::readString);
+            }
         }
         this.xboxUserId = byteBuf.readString();
         this.platformChatId = byteBuf.readString();
-        this.filteredMessage = byteBuf.readString();
+        this.filteredMessage = byteBuf.readOptional("", byteBuf::readString);
     }
 
     @Override
     public void encode(HandleByteBuf byteBuf) {
-        byteBuf.writeByte(this.type);
         byteBuf.writeBoolean(this.isLocalized || type == TYPE_TRANSLATION);
-        switch (this.type) {
-            case TYPE_CHAT:
-            case TYPE_WHISPER:
-            case TYPE_ANNOUNCEMENT:
-                byteBuf.writeString(this.source);
-            case TYPE_RAW:
-            case TYPE_TIP:
-            case TYPE_SYSTEM:
-            case TYPE_OBJECT:
-            case TYPE_OBJECT_WHISPER:
-                byteBuf.writeString(this.message);
-                break;
 
-            case TYPE_TRANSLATION:
-            case TYPE_POPUP:
-            case TYPE_JUKEBOX_POPUP:
+        switch (this.type) {
+            case TYPE_RAW,
+                 TYPE_TIP,
+                 TYPE_SYSTEM,
+                 TYPE_OBJECT,
+                 TYPE_OBJECT_WHISPER,
+                 TYPE_OBJECT_ANNOUNCEMENT -> {
+                byteBuf.writeByte(0);
+                byteBuf.writeString("raw");
+                byteBuf.writeString("tip");
+                byteBuf.writeString("systemMessage");
+                byteBuf.writeString("textObjectWhisper");
+                byteBuf.writeString("textObjectAnnouncement");
+                byteBuf.writeString("textObject");
+                byteBuf.writeByte(type);
                 byteBuf.writeString(this.message);
-                byteBuf.writeUnsignedVarInt(this.parameters.length);
-                for (String parameter : this.parameters) {
-                    byteBuf.writeString(parameter);
-                }
+            }
+            case TYPE_CHAT,
+                 TYPE_WHISPER,
+                 TYPE_ANNOUNCEMENT -> {
+                byteBuf.writeByte(1);
+                byteBuf.writeString("chat");
+                byteBuf.writeString("whisper");
+                byteBuf.writeString("announcement");
+                byteBuf.writeByte(type);
+                byteBuf.writeString(this.source);
+                byteBuf.writeString(this.source);
+            }
+
+            case TYPE_TRANSLATION,
+                 TYPE_POPUP,
+                 TYPE_JUKEBOX_POPUP -> {
+                byteBuf.writeByte(2);
+                byteBuf.writeString("translate");
+                byteBuf.writeString("popup");
+                byteBuf.writeString("jukeboxPopup");
+                byteBuf.writeByte(type);
+                byteBuf.writeString(this.message);
+                byteBuf.writeArray(this.parameters, byteBuf::writeString);
+            }
         }
         byteBuf.writeString(this.xboxUserId);
         byteBuf.writeString(this.platformChatId);
-        byteBuf.writeString(this.filteredMessage);
+        byteBuf.writeOptional(OptionalValue.of(filteredMessage), byteBuf::writeString);
     }
 
     @Override
