f2f77e79a24c19e46e16bb9309011769b3405305
xRookieFight <me@xrookiefight.com>
2026-01-05 20:33:30 +0300

    Added input permission support (#2253)
    * Added input permission support
    
    * Renamed lockComponentId unsigned var int to lockComponentData
    
    * Added javadoc comments
    
    * Github mobile...
    
    * Changed state and permission case-sensitive

--- src/main/java/cn/nukkit/network/protocol/UpdateClientInputLocksPacket.java
+++ src/main/java/cn/nukkit/network/protocol/UpdateClientInputLocksPacket.java
@@ -16,26 +16,26 @@ import java.util.Set;
 @AllArgsConstructor
 public class UpdateClientInputLocksPacket extends DataPacket {
 
-    private int lockComponentId;
+    private int lockComponentData;
     public Vector3f serverPosition;
 
     public void setFlags(Set<ClientInputLocksFlag> flags) {
-        this.lockComponentId = ClientInputLocksFlag.toBitSet(flags);
+        this.lockComponentData = ClientInputLocksFlag.toBitSet(flags);
     }
 
     public Set<ClientInputLocksFlag> getActiveFlags() {
-        return ClientInputLocksFlag.fromBitSet(this.lockComponentId);
+        return ClientInputLocksFlag.fromBitSet(this.lockComponentData);
     }
 
     @Override
     public void decode(HandleByteBuf byteBuf) {
-        this.lockComponentId = byteBuf.readVarInt();
+        this.lockComponentData = byteBuf.readUnsignedVarInt();
         this.serverPosition = byteBuf.readVector3f();
     }
 
     @Override
     public void encode(HandleByteBuf byteBuf) {
-        byteBuf.writeVarInt(lockComponentId);
+        byteBuf.writeUnsignedVarInt(lockComponentData);
         byteBuf.writeVector3f(serverPosition);
     }
 
