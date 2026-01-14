f4ce0d6975c6c3e0b77391ac177617ab58eca7ae
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2026-01-14 10:05:11 +0100

    improved terra performance by alot (#2272)
    Trees and other populators often span multiple chunks. Instead of generating entire chunks—which in turn may trigger the generation of additional chunks—we can now generate only the terrain. For example, when generating a tree, only the terrain of the overlapping chunks is generated. Those chunks will be populated only once they are actually loaded by a player.
    
    This will be applied to the default generator at some point too.

--- src/main/java/cn/nukkit/level/generator/terra/delegate/PNXServerWorld.java
+++ src/main/java/cn/nukkit/level/generator/terra/delegate/PNXServerWorld.java
@@ -1,8 +1,12 @@
 package cn.nukkit.level.generator.terra.delegate;
 
+import cn.nukkit.block.Block;
 import cn.nukkit.block.BlockID;
 import cn.nukkit.level.Level;
 import cn.nukkit.level.Position;
+import cn.nukkit.level.format.ChunkState;
+import cn.nukkit.level.format.IChunk;
+import cn.nukkit.level.generator.object.BlockManager;
 import cn.nukkit.level.generator.terra.PNXAdapter;
 import cn.nukkit.level.generator.terra.TerraGenerator;
 import cn.nukkit.math.BlockVector3;
@@ -18,42 +22,75 @@ import com.dfsek.terra.api.world.chunk.generation.ChunkGenerator;
 
 import java.util.Objects;
 
-public record PNXServerWorld(TerraGenerator generatorWrapper, Level level) implements ServerWorld {
+public class PNXServerWorld implements ServerWorld {
+
+    private final TerraGenerator generatorWrapper;
+    private final BlockManager level;
+
+    public PNXServerWorld(TerraGenerator generatorWrapper, Level level) {
+        this.generatorWrapper = generatorWrapper;
+        this.level = new BlockManager(level);
+    }
+
+    public TerraGenerator generatorWrapper() {
+        return generatorWrapper;
+    }
+
+    public Level level() {
+        return level.getLevel();
+    }
+
+    public void apply(IChunk chunk) {
+        for(Block b : level.getBlocks()) {
+            if(b.getChunk() != chunk) {
+                level.getChunk(b.getChunkX(), b.getChunkZ());
+            }
+        }
+        level.applySubChunkUpdate();
+    }
 
     @Override
-    public void setBlockState(int i, int i1, int i2, BlockState blockState, boolean b) {
+    public void setBlockState(int x, int y, int z, BlockState blockState, boolean b) {
         var innerBlockState = ((PNXBlockStateDelegate) blockState).innerBlockState();
+
         if (Objects.equals(innerBlockState.getIdentifier(), BlockID.KELP)) {
-            level.setBlockStateAt(i, i1, i2, innerBlockState);
-            level.setBlockStateAt(i, i1, i2, 1, PNXProtoChunk.water);
+            level.setBlockStateAt(x, y, z, innerBlockState);
+            level.setBlockStateAt(x, y, z, 1, PNXProtoChunk.water);
+            return;
+        }
+
+        var ob = level.getBlockAt(x, y, z);
+        if (Objects.equals(ob.getId(), BlockID.WATERLILY)
+                || Objects.equals(ob.getId(), BlockID.WATER)
+                || Objects.equals(ob.getId(), BlockID.FLOWING_WATER)) {
+
+            level.setBlockStateAt(x, y, z, innerBlockState);
+            level.setBlockStateAt(x, y, z, 1, PNXProtoChunk.water);
         } else {
-            var ob = level.getBlockStateAt(i, i1, i2);
-            if (Objects.equals(ob.getIdentifier(), BlockID.WATERLILY) ||
-                    Objects.equals(ob.getIdentifier(), BlockID.WATER) ||
-                    Objects.equals(ob.getIdentifier(), BlockID.FLOWING_WATER)) {
-                level.setBlockStateAt(i, i1, i2, innerBlockState);
-                level.setBlockStateAt(i, i1, i2, 1, PNXProtoChunk.water);
-            } else {
-                level.setBlockStateAt(i, i1, i2, innerBlockState);
-            }
+            level.setBlockStateAt(x, y, z, innerBlockState);
         }
     }
 
     @Override
-    public Entity spawnEntity(double v, double v1, double v2, EntityType entityType) {
+    public Entity spawnEntity(double x, double y, double z, EntityType entityType) {
         String identifier = (String) entityType.getHandle();
-        cn.nukkit.entity.Entity nukkitEntity = cn.nukkit.entity.Entity.createEntity(identifier, new Position(v, v1, v2, level));
+        cn.nukkit.entity.Entity nukkitEntity =
+                cn.nukkit.entity.Entity.createEntity(
+                        identifier,
+                        new Position(x, y, z, level.getLevel())
+                );
         return new PNXEntity(nukkitEntity, this);
     }
 
     @Override
-    public BlockState getBlockState(int i, int i1, int i2) {
-        return PNXAdapter.adapt(level.getBlockStateAt(i, i1, i2));
+    public BlockState getBlockState(int x, int y, int z) {
+        return PNXAdapter.adapt(level.getBlockAt(x, y, z).getBlockState());
     }
 
     @Override
-    public BlockEntity getBlockEntity(int i, int i1, int i2) {
-        cn.nukkit.blockentity.BlockEntity blockEntity = level.getBlockEntity(new BlockVector3(i, i1, i2));
+    public BlockEntity getBlockEntity(int x, int y, int z) {
+        cn.nukkit.blockentity.BlockEntity blockEntity =
+                level.getLevel().getBlockEntity(new BlockVector3(x, y, z));
         return null;
     }
 
@@ -89,11 +126,11 @@ public record PNXServerWorld(TerraGenerator generatorWrapper, Level level) imple
 
     @Override
     public Level getHandle() {
-        return level;
+        return level.getLevel();
     }
 
     @Override
-    public Chunk getChunkAt(int i, int i1) {
-        return new PNXChunkDelegate(this, level.getChunk(i, i1));
+    public Chunk getChunkAt(int x, int z) {
+        return new PNXChunkDelegate(this, level.getChunk(x, z));
     }
 }
