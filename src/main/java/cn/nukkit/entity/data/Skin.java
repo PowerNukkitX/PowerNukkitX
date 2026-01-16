7612c2294613b98ef3a69d7b3fb6d723a0dd1271
Sara <69171937+LadySara48@users.noreply.github.com>
2026-01-16 17:51:59 +0300

    Update Skin.java
    Cape files have a 64x32 image. Therefore, they should be checked using the calculation (64*32*4)

--- src/main/java/cn/nukkit/entity/data/Skin.java
+++ src/main/java/cn/nukkit/entity/data/Skin.java
@@ -201,7 +201,7 @@ public class Skin {
 
     public void setCapeData(byte[] capeData) {
         Objects.requireNonNull(capeData, "capeData");
-        Preconditions.checkArgument(capeData.length == SINGLE_SKIN_SIZE || capeData.length == 0, "Invalid legacy cape");
+        Preconditions.checkArgument(capeData.length == SKIN_64_32_SIZE || capeData.length == 0, "Invalid legacy cape");
         setCapeData(new SerializedImage(64, 32, capeData));
     }
 
