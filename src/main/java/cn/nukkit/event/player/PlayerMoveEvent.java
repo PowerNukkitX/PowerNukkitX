c1f30862d53fda69c0d39013dfabf96b536f5c2e
xRookieFight <me@xrookiefight.com>
2026-02-13 00:10:20 +0300

    Player movement improvements (#2316)
    * feat: Separate PlayerMoveEvent types
    
    * Made movement thresholds a setting
    
    * feat: Requested reviews
    
    ---------
    
    Co-authored-by: PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>

--- src/main/java/cn/nukkit/event/player/PlayerMoveEvent.java
+++ src/main/java/cn/nukkit/event/player/PlayerMoveEvent.java
@@ -4,8 +4,19 @@ import cn.nukkit.Player;
 import cn.nukkit.event.Cancellable;
 import cn.nukkit.event.HandlerList;
 import cn.nukkit.level.Location;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class PlayerMoveEvent extends PlayerEvent implements Cancellable {
+
+    public enum Type {
+        POSITION_CHANGE,
+        ROTATE,
+        ALL
+    }
+
     private static final HandlerList handlers = new HandlerList();
 
     public static HandlerList getHandlers() {
@@ -14,42 +25,23 @@ public class PlayerMoveEvent extends PlayerEvent implements Cancellable {
 
     private Location from;
     private Location to;
-
     private boolean resetBlocksAround;
+    private Type movementType;
 
     public PlayerMoveEvent(Player player, Location from, Location to) {
         this(player, from, to, true);
     }
 
     public PlayerMoveEvent(Player player, Location from, Location to, boolean resetBlocks) {
+        this(player, from, to, resetBlocks, Type.ALL);
+    }
+
+    public PlayerMoveEvent(Player player, Location from, Location to, boolean resetBlocks, Type movementType) {
         this.player = player;
         this.from = from;
         this.to = to;
         this.resetBlocksAround = resetBlocks;
-    }
-
-    public Location getFrom() {
-        return from;
-    }
-
-    public void setFrom(Location from) {
-        this.from = from;
-    }
-
-    public Location getTo() {
-        return to;
-    }
-
-    public void setTo(Location to) {
-        this.to = to;
-    }
-
-    public boolean isResetBlocksAround() {
-        return resetBlocksAround;
-    }
-
-    public void setResetBlocksAround(boolean value) {
-        this.resetBlocksAround = value;
+        this.movementType = movementType;
     }
 
     @Override
