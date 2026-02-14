1a8eb0c89b7b139f9100a9d2534e779bf31aee48
xRookieFight <me@xrookiefight.com>
2026-02-14 10:47:26 +0300

    Debug shape changes (#2326)
    * Debug shape changes
    
    * Copilot changes

--- src/main/java/cn/nukkit/network/protocol/types/debugshape/TextDebugShape.java
+++ src/main/java/cn/nukkit/network/protocol/types/debugshape/TextDebugShape.java
@@ -6,10 +6,11 @@ import cn.nukkit.network.protocol.types.ScriptDebugShapeType;
 import java.awt.*;
 
 public class TextDebugShape extends DebugShape {
-    public TextDebugShape(Vector3f location, Color color, String text) {
+    public TextDebugShape(Vector3f location, Color color, String text, long attachedToEntityId) {
         shapeType = ScriptDebugShapeType.TEXT;
         this.location = location;
         this.color = color;
+        this.attachedToEntityId = attachedToEntityId;
         this.text = text;
     }
 }
