26dab4ce99be93576e11da4afd220226bade2f9d
Kaooot <37877491+Kaooot@users.noreply.github.com>
2026-04-01 00:36:42 +0200

    Add VillageType enumeration

--- src/main/java/cn/nukkit/network/protocol/types/biome/VillageType.java
+++ src/main/java/cn/nukkit/network/protocol/types/biome/VillageType.java
@@ -1,4 +1,19 @@
 package cn.nukkit.network.protocol.types.biome;
 
 public enum VillageType {
+
+    DESERT,
+    ICE,
+    SAVANNA,
+    TAIGA,
+    DEFAULT;
+
+    private static final VillageType[] VALUES = values();
+
+    public static VillageType from(int ordinal) {
+        if (ordinal >= VALUES.length || ordinal < 0) {
+            throw new UnsupportedOperationException("Detected unknown VillageType ID: " + ordinal);
+        }
+        return VALUES[ordinal];
+    }
 }
