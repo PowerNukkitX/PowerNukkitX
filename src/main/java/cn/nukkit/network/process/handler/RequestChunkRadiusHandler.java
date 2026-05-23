4e4168c8448c497de3a0cfbe3f82cc1a410553ae
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2026-05-23 20:45:10 +0200

    Fixed max render distance

--- src/main/java/cn/nukkit/network/process/handler/RequestChunkRadiusHandler.java
+++ src/main/java/cn/nukkit/network/process/handler/RequestChunkRadiusHandler.java
@@ -13,7 +13,6 @@ import org.cloudburstmc.protocol.bedrock.packet.RequestChunkRadiusPacket;
 public class RequestChunkRadiusHandler implements PacketHandler<RequestChunkRadiusPacket> {
 
     private static final int MIN_CHUNK_RADIUS = 2;
-    private static final int MAX_CHUNK_RADIUS = 32;
 
     @Override
     public void handle(RequestChunkRadiusPacket packet, PlayerSessionHolder holder, Server server) {
@@ -21,7 +20,8 @@ public class RequestChunkRadiusHandler implements PacketHandler<RequestChunkRadi
             holder.disconnect(DisconnectFailReason.UNEXPECTED_PACKET);
             return;
         }
-        final int radius = Math.max(MIN_CHUNK_RADIUS, Math.min(packet.getChunkRadius(), MAX_CHUNK_RADIUS));
+        final int viewDistance = server.getSettings().gameplaySettings().viewDistance();
+        final int radius = Math.max(MIN_CHUNK_RADIUS, Math.min(packet.getChunkRadius(), viewDistance));
         holder.getPlayer().setViewDistance(radius);
     }
 }
