808a43c1f609ab5ea3ed587e9a8ceaf8e6886769
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2026-06-25 15:19:20 +0200

    fix: now using zombie_villager_v2 (#2646)
    * fix: now using zombie_villager_v2
    
    - Added a new event when entities transform into another
    - Vanillanized Zombie Villager
    
    * Deprecated Javadocs
    
    * fix: made villagers and zombie villagers persistent

--- src/main/java/cn/nukkit/entity/ai/executor/PiglinTransformExecutor.java
+++ src/main/java/cn/nukkit/entity/ai/executor/PiglinTransformExecutor.java
@@ -1,9 +1,11 @@
 package cn.nukkit.entity.ai.executor;
 
+import cn.nukkit.Server;
 import cn.nukkit.entity.EntityIntelligent;
 import cn.nukkit.entity.effect.Effect;
 import cn.nukkit.entity.effect.EffectType;
 import cn.nukkit.entity.mob.EntityZombiePigman;
+import cn.nukkit.event.entity.EntityTransformEvent;
 import cn.nukkit.inventory.Inventory;
 import cn.nukkit.level.Sound;
 import org.cloudburstmc.protocol.bedrock.data.actor.ActorFlags;
@@ -43,18 +45,22 @@ public class PiglinTransformExecutor implements EntityControl, IBehaviorExecutor
 
     private void transform(EntityIntelligent entity) {
         entity.saveNBT();
-        entity.close();
         EntityZombiePigman entityZombiePigman = new EntityZombiePigman(entity.getChunk(), entity.getNbt());
-        entityZombiePigman.setPosition(entity);
-        entityZombiePigman.setRotation(entity.yaw, entity.pitch);
-        entityZombiePigman.spawnToAll();
-        entityZombiePigman.level.addSound(entityZombiePigman, Sound.MOB_PIGLIN_CONVERTED_TO_ZOMBIFIED);
-        Inventory inventory = entityZombiePigman.getEquipmentInventory();
-        for(int i = 2; i < inventory.getSize(); i++) {
-            entityZombiePigman.level.dropItem(entityZombiePigman, inventory.getItem(i));
-            inventory.clear(i);
+        EntityTransformEvent event = new EntityTransformEvent(entity, entityZombiePigman);
+        Server.getInstance().getPluginManager().callEvent(event);
+        if(event.isCancelled()) {
+            entityZombiePigman.close();
+        } else {
+            entity.close();
+            entityZombiePigman.spawnToAll();
+            entityZombiePigman.level.addSound(entityZombiePigman, Sound.MOB_PIGLIN_CONVERTED_TO_ZOMBIFIED);
+            Inventory inventory = entityZombiePigman.getEquipmentInventory();
+            for (int i = 2; i < inventory.getSize(); i++) {
+                entityZombiePigman.level.dropItem(entityZombiePigman, inventory.getItem(i));
+                inventory.clear(i);
+            }
+            entityZombiePigman.addEffect(Effect.get(EffectType.NAUSEA).setDuration(15));
         }
-        entityZombiePigman.addEffect(Effect.get(EffectType.NAUSEA).setDuration(15));
     }
 
 }
