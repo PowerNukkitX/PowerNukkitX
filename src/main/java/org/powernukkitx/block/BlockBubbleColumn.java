63b2002bb90ba309315a6e6ff0b41afdd268b236
DrakzoSurYT <60445550+DrakzoSurYT@users.noreply.github.com>
2026-07-14 06:58:29 +0200

    yes change

--- src/main/java/org/powernukkitx/block/BlockBubbleColumn.java
+++ src/main/java/org/powernukkitx/block/BlockBubbleColumn.java
@@ -20,10 +20,10 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class BlockBubbleColumn extends BlockTransparent {
     public static final BlockProperties PROPERTIES = new BlockProperties(BUBBLE_COLUMN, CommonBlockProperties.DRAG_DOWN);
-    private static final double DOWNWARD_MIN_MOTION = -0.3;
+    private static final double DOWNWARD_MAX_MOTION = -0.3;
     private static final double DOWNWARD_ACCELERATION = 0.03;
     private static final double UPWARD_ACCELERATION = 0.08;
-    private static final double UPWARD_MAX_MOTION = 0.6;
+    private static final double UPWARD_MIN_MOTION = 0.6;
 
     @Override
     @NotNull public BlockProperties getProperties() {
@@ -110,17 +110,18 @@ public class BlockBubbleColumn extends BlockTransparent {
 
     @Override
     public void onEntityCollide(Entity entity) {
+        if (!entity.canBeMovedByCurrents()) return;
+
         if (up().isAir()) {
             spawnColumnParticles();
         }
 
         if (entity instanceof Player) return;
-        if (!(entity instanceof EntityItem) && !entity.canBeMovedByCurrents()) return;
 
         if (isDragDown()) {
-            entity.motionY = Math.max(DOWNWARD_MIN_MOTION, entity.motionY - DOWNWARD_ACCELERATION);
+            entity.motionY = Math.max(DOWNWARD_MAX_MOTION, entity.motionY - DOWNWARD_ACCELERATION);
         } else {
-            entity.motionY = Math.min(UPWARD_MAX_MOTION, entity.motionY + UPWARD_ACCELERATION);
+            entity.motionY = Math.min(UPWARD_MIN_MOTION, entity.motionY + UPWARD_ACCELERATION);
         }
 
         entity.motionChanged = true;
