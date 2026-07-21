427644c666bb742203d6f0517297a355e425dcb3
SemihAkin685 <semihakin681@gmail.com>
2026-07-22 02:08:42 +0300

    fix: restrict huge mushroom spawning to valid biomes

--- src/main/java/org/powernukkitx/level/generator/feature/decoration/HugeMushroomFeature.java
+++ src/main/java/org/powernukkitx/level/generator/feature/decoration/HugeMushroomFeature.java
@@ -3,7 +3,10 @@ package org.powernukkitx.level.generator.feature.decoration;
 import org.powernukkitx.level.generator.feature.ObjectGeneratorFeature;
 import org.powernukkitx.level.generator.object.ObjectBigMushroom;
 import org.powernukkitx.level.generator.object.ObjectGenerator;
+import org.powernukkitx.registry.Registries;
+import org.powernukkitx.tags.BiomeTags;
 import org.powernukkitx.utils.random.RandomSourceProvider;
+import org.cloudburstmc.protocol.bedrock.data.biome.BiomeDefinitionData;
 
 public class HugeMushroomFeature extends ObjectGeneratorFeature {
 
@@ -12,6 +15,13 @@ public class HugeMushroomFeature extends ObjectGeneratorFeature {
     @Override
     public ObjectGenerator getGenerator(RandomSourceProvider random) {
         return new ObjectBigMushroom();
+    }
+
+    @Override
+    public boolean canSpawnHere(BiomeDefinitionData definition) {
+        return Registries.BIOME.containsTag(BiomeTags.ROOFED, definition)
+                || Registries.BIOME.containsTag(BiomeTags.SWAMP, definition)
+                || Registries.BIOME.containsTag(BiomeTags.MOOSHROOM_ISLAND, definition);
     }
 
     @Override
