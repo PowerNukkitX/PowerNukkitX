eef32b9f103ff7f55930fb44ea75338f6302d4cf
DrakzoSurYT <60445550+DrakzoSurYT@users.noreply.github.com>
2026-07-24 01:07:58 +0200

    use short

--- src/main/java/org/powernukkitx/item/customitem/CustomItemDefinition.java
+++ src/main/java/org/powernukkitx/item/customitem/CustomItemDefinition.java
@@ -1086,7 +1086,7 @@ public record CustomItemDefinition(String identifier, CompoundTag nbt) implement
             if (this.damage == null) return;
 
             itemProps.putInt("damage", this.damage);
-            components.putShort("minecraft:damage", this.damage);
+            components.putCompound("minecraft:damage", new CompoundTag().putShort("value", this.damage));
         }
 
         private void writeFood(CompoundTag components, CompoundTag itemProps) {
