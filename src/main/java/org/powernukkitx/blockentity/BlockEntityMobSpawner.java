a60bb821a8f55df5f93bd57c01faeeb0e67457f4
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2026-07-26 22:19:59 +0200

    fix: deadlock when lectern tries to update block in uninitiated chunk (#2875)
    * fix: deadlock by lectern updating blocks in unloaded chunks while initiating
    
    note: I could not reproduce the deadlock in the first place, but it could actually happen.
    
    * cleanup: initBlockEntity

--- src/main/java/org/powernukkitx/blockentity/BlockEntityMobSpawner.java
+++ src/main/java/org/powernukkitx/blockentity/BlockEntityMobSpawner.java
@@ -62,6 +62,13 @@ public class BlockEntityMobSpawner extends BlockEntitySpawnable {
 
     @Override
     protected void initBlockEntity() {
+        super.initBlockEntity();
+        this.scheduleUpdate();
+    }
+
+    @Override
+    public void loadNBT() {
+        super.loadNBT();
         if (!this.nbt.contains(TAG_SPAWN_RANGE) || !(this.nbt.get(TAG_SPAWN_RANGE) instanceof ShortTag)) {
             this.nbt.putShort(TAG_SPAWN_RANGE, (short) SPAWN_RANGE);
         }
@@ -98,10 +105,6 @@ public class BlockEntityMobSpawner extends BlockEntitySpawnable {
         this.requiredPlayerRange = nbtMap.getShort(TAG_REQUIRED_PLAYER_RANGE);
         this.minSpawnCount = nbtMap.getShort(TAG_MINIMUM_SPAWN_COUNT);
         this.maxSpawnCount = nbtMap.getShort(TAG_MAXIMUM_SPAWN_COUNT);
-
-        this.scheduleUpdate();
-        super.initBlockEntity();
-        this.level.getScheduler().scheduleTask(this::spawnToAll);
     }
 
     @Override
