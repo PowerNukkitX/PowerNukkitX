c020d8f4057e0df158cd18605ed6d88b255da1f7
DrakzoSurYT <60445550+DrakzoSurYT@users.noreply.github.com>
2026-07-26 12:15:21 +0200

    use item helper ; delete UnknownItem and better usage

--- src/main/java/org/powernukkitx/item/ItemUnknown.java
+++ src/main/java/org/powernukkitx/item/ItemUnknown.java
@@ -2,20 +2,14 @@ package org.powernukkitx.item;
 
 public class ItemUnknown extends Item {
 
-    private final String originalId;
-
     public ItemUnknown(String originalId, int meta, int count, byte[] tags) {
         super(originalId, meta, count);
-        this.originalId = originalId;
         if (tags != null && tags.length > 0) {
             this.setNbtBytes(tags);
         }
     }
 
-    public String getOriginalId() {
-        return originalId;
-    }
-
+    @Override
     public int getMaxStackSize() {
         return 64;
     }
