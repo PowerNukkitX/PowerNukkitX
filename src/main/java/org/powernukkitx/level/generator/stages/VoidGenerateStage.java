fb3492a1818f3ecd9f4b8a8410c6934472dd7165
Wyze3306 <remy.grisollet@gmail.com>
2026-08-03 17:42:48 +0200

    feat: add void world generator
    Register a "void" generator producing empty chunks, for worlds whose
    content comes entirely from saved chunks or pasted structures.

---
+++ src/main/java/org/powernukkitx/level/generator/stages/VoidGenerateStage.java
@@ -0,0 +1,26 @@
+package org.powernukkitx.level.generator.stages;
+
+import org.powernukkitx.level.format.ChunkState;
+import org.powernukkitx.level.format.IChunk;
+import org.powernukkitx.level.generator.ChunkGenerateContext;
+import org.powernukkitx.level.generator.GenerateStage;
+
+public class VoidGenerateStage extends GenerateStage {
+    public static final String NAME = "void_generate";
+
+    @Override
+    public String name() {
+        return NAME;
+    }
+
+    @Override
+    public void apply(ChunkGenerateContext context) {
+        IChunk chunk = context.getChunk();
+        for (int x = 0; x < 16; x++) {
+            for (int z = 0; z < 16; z++) {
+                chunk.setHeightMap(x, z, 0);
+            }
+        }
+        chunk.setChunkState(ChunkState.POPULATED);
+    }
+}
