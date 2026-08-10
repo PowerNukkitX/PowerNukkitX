a36648fc00656b5f5c567f008dde46d3c1d2806f
Kanelucky <tuankietdoan46@gmail.com>
2026-08-10 16:23:17 +0700

    Update RouteTree.java

--- src/main/java/org/powernukkitx/command/route/RouteTree.java
+++ src/main/java/org/powernukkitx/command/route/RouteTree.java
@@ -74,7 +74,12 @@ public class RouteTree {
 
         RouteNode current = match(root, args, 0, context);
         if (current == null) {
-            sender.sendMessage(new TranslationContainer("commands.generic.usage", command.getUsage()));
+            String usage = command.getUsage();
+            if (usage == null || usage.isBlank()) {
+                sender.sendMessage(new TranslationContainer("commands.generic.syntax", "", "", ""));
+            } else {
+                sender.sendMessage(new TranslationContainer("commands.generic.usage", command.getUsage()));
+            }
             return CommandResult.fail();
         }
 
